531924cfccc3527b2b62a9e04b3ec7f0491ddd45
Yang Zhou <zhouyang@elastos.org>
2018-03-20 21:53:37 +0800

    adjust auxBlock byte order and reverse auxBlock hash bytes

--- ELA/core/auxpow/auxpow.go
+++ ELA/core/auxpow/auxpow.go
@@ -51,13 +51,25 @@ func (ap *AuxPow) Serialize(w io.Writer) error {
 		return err
 	}
 
-	idx := uint32(ap.AuxMerkleIndex)
+	count := uint64(len(ap.ParCoinBaseMerkle))
+	err = serialization.WriteVarUint(w, count)
+	if err != nil {
+		return err
+	}
+
+	for _, pcbm := range ap.ParCoinBaseMerkle {
+		_, err = pcbm.Serialize(w)
+		if err != nil {
+			return err
+		}
+	}
+	idx := uint32(ap.ParMerkleIndex)
 	err = serialization.WriteUint32(w, idx)
 	if err != nil {
 		return err
 	}
 
-	count := uint64(len(ap.AuxMerkleBranch))
+	count = uint64(len(ap.AuxMerkleBranch))
 	err = serialization.WriteVarUint(w, count)
 	if err != nil {
 		return err
@@ -70,23 +82,10 @@ func (ap *AuxPow) Serialize(w io.Writer) error {
 		}
 	}
 
-	idx = uint32(ap.ParMerkleIndex)
+	idx = uint32(ap.AuxMerkleIndex)
 	err = serialization.WriteUint32(w, idx)
 	if err != nil {
 		return err
-	}
-
-	count = uint64(len(ap.ParCoinBaseMerkle))
-	err = serialization.WriteVarUint(w, count)
-	if err != nil {
-		return err
-	}
-
-	for _, pcbm := range ap.ParCoinBaseMerkle {
-		_, err = pcbm.Serialize(w)
-		if err != nil {
-			return err
-		}
 	}
 
 	err = ap.ParBlockHeader.Serialize(w)
@@ -107,13 +106,29 @@ func (ap *AuxPow) Deserialize(r io.Reader) error {
 		return err
 	}
 
+	count, err := serialization.ReadVarUint(r, 0)
+	if err != nil {
+		return err
+	}
+
+	ap.ParCoinBaseMerkle = make([]Uint256, count)
+	for i := uint64(0); i < count; i++ {
+		temp := Uint256{}
+		err = temp.Deserialize(r)
+		if err != nil {
+			return err
+		}
+		ap.ParCoinBaseMerkle[i] = temp
+
+	}
+
 	temp, err := serialization.ReadUint32(r)
 	if err != nil {
 		return err
 	}
-	ap.AuxMerkleIndex = int(temp)
+	ap.ParMerkleIndex = int(temp)
 
-	count, err := serialization.ReadVarUint(r, 0)
+	count, err = serialization.ReadVarUint(r, 0)
 	if err != nil {
 		return err
 	}
@@ -132,23 +147,7 @@ func (ap *AuxPow) Deserialize(r io.Reader) error {
 	if err != nil {
 		return err
 	}
-	ap.ParMerkleIndex = int(temp)
-
-	count, err = serialization.ReadVarUint(r, 0)
-	if err != nil {
-		return err
-	}
-
-	ap.ParCoinBaseMerkle = make([]Uint256, count)
-	for i := uint64(0); i < count; i++ {
-		temp := Uint256{}
-		err = temp.Deserialize(r)
-		if err != nil {
-			return err
-		}
-		ap.ParCoinBaseMerkle[i] = temp
-
-	}
+	ap.AuxMerkleIndex = int(temp)
 
 	err = ap.ParBlockHeader.Deserialize(r)
 	if err != nil {
@@ -163,7 +162,9 @@ func (ap *AuxPow) Check(hashAuxBlock Uint256, chainId int) bool {
 		return false
 	}
 
-	auxRootHash := CheckMerkleBranch(hashAuxBlock, ap.AuxMerkleBranch, ap.AuxMerkleIndex)
+	hashAuxBlockBytes := hashAuxBlock.ToArrayReverse()
+	hashAuxBlockReverse, _ := Uint256ParseFromBytes(hashAuxBlockBytes)
+	auxRootHash := CheckMerkleBranch(hashAuxBlockReverse, ap.AuxMerkleBranch, ap.AuxMerkleIndex)
 
 	script := ap.ParCoinbaseTx.TxIn[0].SignatureScript
 	scriptStr := hex.EncodeToString(script)
