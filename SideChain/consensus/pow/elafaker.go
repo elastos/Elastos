96204c63be63f8520a0a5b3d316e6bd2597f3b7b
Yang Zhou <zhouyang@elastos.org>
2018-03-22 00:19:12 +0800

    add elafaker

---
+++ SideChain/consensus/pow/elafaker.go
@@ -0,0 +1,40 @@
+package pow
+
+import (
+	"time"
+
+	. "Elastos.ELA.SideChain/common"
+	"Elastos.ELA.SideChain/core/auxpow"
+)
+
+func getSideMiningTx(msgBlockHash Uint256) *auxpow.ElaTx {
+	s := auxpow.SideMiningPayload{
+		SideBlockHash: msgBlockHash,
+	}
+	sideMiningTx := auxpow.NewSideMiningTx(s, 0)
+
+	return sideMiningTx
+}
+
+func generateSideAuxPow(msgBlockHash Uint256) *auxpow.SideAuxPow {
+	sideAuxMerkleBranch := make([]Uint256, 0)
+	sideAuxMerkleIndex := uint32(0)
+	sideAuxBlockTx := getSideMiningTx(msgBlockHash)
+	elaBlockHeader := auxpow.ElaBlockHeader{
+		Version:          0x7fffffff,
+		PrevBlockHash:    Uint256{},
+		TransactionsRoot: sideAuxBlockTx.Hash(),
+		Timestamp:        uint32(time.Now().Unix()),
+		Bits:             0, // do not care about parent block diff
+		Nonce:            0, // to be solved
+		Height:           0,
+	}
+	auxPow := auxpow.NewSideAuxPow(
+		sideAuxMerkleBranch,
+		sideAuxMerkleIndex,
+		*sideAuxBlockTx,
+		elaBlockHeader,
+	)
+
+	return auxPow
+}
