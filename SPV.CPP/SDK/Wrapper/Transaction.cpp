8087d2cf78cf703bf7357cb24075a9b4f74353df
HeroPoon <heroopan@gmail.com>
2018-04-17 10:30:15 +0800

    fix compile error on ubuntu.

--- SPV.CPP/SDK/Wrapper/Transaction.cpp
+++ SPV.CPP/SDK/Wrapper/Transaction.cpp
@@ -2,10 +2,12 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <BRTransaction.h>
-#include "Transaction.h"
+#include "PreCompiled.h"
+
+#include <cstring>
 
 #include "BRTransaction.h"
+#include "Transaction.h"
 
 namespace Elastos {
 	namespace SDK {
