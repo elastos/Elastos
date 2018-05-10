3e1efc4ad47cde66a1e2f4b5815afff7054668bc
jiangzehua <jiangzehua@elastos.org>
2018-05-10 18:15:02 +0800

    Fix bug to deal with withdraw transaction correctly

--- ELA.CR.ClaimDPosNode/core/payloadwithdrawasset.go
+++ ELA.CR.ClaimDPosNode/core/payloadwithdrawasset.go
@@ -24,10 +24,10 @@ func (t *PayloadWithdrawAsset) Serialize(w io.Writer, version byte) error {
 		return errors.New("[WithdrawAsset], BlockHeight serialize failed.")
 	}
 	if err := common.WriteVarString(w, t.GenesisBlockAddress); err != nil {
-		return errors.New("[WithdrawAsset], BlockHeight serialize failed.")
+		return errors.New("[WithdrawAsset], GenesisBlockAddress serialize failed.")
 	}
 	if err := common.WriteVarString(w, t.SideChainTransactionHash); err != nil {
-		return errors.New("[WithdrawAsset], BlockHeight serialize failed.")
+		return errors.New("[WithdrawAsset], SideChainTransactionHash serialize failed.")
 	}
 
 	return nil
@@ -40,11 +40,11 @@ func (t *PayloadWithdrawAsset) Deserialize(r io.Reader, version byte) error {
 	}
 	address, err := common.ReadVarString(r)
 	if err != nil {
-		return errors.New("[WithdrawAsset], BlockHeight deserialize failed.")
+		return errors.New("[WithdrawAsset], GenesisBlockAddress deserialize failed.")
 	}
 	hash, err := common.ReadVarString(r)
 	if err != nil {
-		return errors.New("[WithdrawAsset], BlockHeight deserialize failed.")
+		return errors.New("[WithdrawAsset], SideChainTransactionHash deserialize failed.")
 	}
 
 	t.BlockHeight = height
