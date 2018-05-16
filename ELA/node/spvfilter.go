ba611f8af3e86835c15e4065d0a35175bfdb19aa
AlexPan <ppgod@live.cn>
2018-05-16 20:19:59 +0800

    node can sync blockchain using new EIP001 protocol

--- ELA/node/spvfilter.go
+++ ELA/node/spvfilter.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/elastos/Elastos.ELA/protocol"
+	"github.com/elastos/Elastos.ELA.Utility/p2p"
 )
 
 // SPV client will connect node through the specific spv port
@@ -13,24 +14,23 @@ func FilterMessage(node protocol.Noder, msgType string) error {
 	if node.LocalPort() == protocol.SPVPort {
 		// Only cased message types can go through
 		switch msgType {
-		case "version":
-		case "verack":
-		case "getaddr":
-		case "addr":
-			return fmt.Errorf("message addr can not go through SPV port")
-		case "filterload":
-		case "getblocks":
-		case "getdata":
-		case "ping":
-		case "pong":
-		case "tx":
+		case p2p.CmdVersion:
+		case p2p.CmdVerAck:
+		case p2p.CmdGetAddr:
+		case p2p.CmdPing:
+		case p2p.CmdPong:
+		case p2p.CmdFilterLoad:
+		case p2p.CmdGetBlocks:
+		case p2p.CmdGetData:
+		case p2p.CmdTx:
+		case p2p.CmdMemPool:
 		default:
 			return fmt.Errorf("unsupport messsage from SPV port %d, type: %s", node.Port(), msgType)
 		}
 	} else {
 		// Node not using SPV port can not send filterload message
 		switch msgType {
-		case "filterload":
+		case p2p.CmdFilterLoad:
 			return fmt.Errorf("SPV messsage from non SPV port %d, type: %s", node.Port(), msgType)
 		}
 	}
