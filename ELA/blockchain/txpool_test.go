dcc214d60a3c047446b93c8f47b8fd16ee7b3fdd
Yang Zhou <zhouyang@elastos.org>
2018-05-24 22:44:14 +0800

    add clean sidechain tx pool func

--- ELA/blockchain/txpool_test.go
+++ ELA/blockchain/txpool_test.go
@@ -48,3 +48,69 @@ func TestTxPool_VerifyDuplicateSidechainTx(t *testing.T) {
 		t.Error("Should find the duplicate sidechain tx")
 	}
 }
+
+func TestTxPool_CleanSidechainTx(t *testing.T) {
+	// 1. Generate some withdraw transactions
+	txn1 := new(core.Transaction)
+	txn1.TxType = core.WithdrawAsset
+	txn1.Payload = &core.PayloadWithdrawAsset{
+		BlockHeight:         100,
+		GenesisBlockAddress: "eb7adb1fea0dd6185b09a43bdcd4924bb22bff7151f0b1b4e08699840ab1384b",
+		SideChainTransactionHash: []string{
+			"300db7783393a6f60533c1223108445df57de4fb4842f84f55d07df57caa0c7d",
+			"d6c2cb8345a8fe4af0d103cc4e40dbb0654bb169a85bb8cc57923d0c72f3658f",
+		},
+	}
+
+	txn2 := new(core.Transaction)
+	txn2.TxType = core.WithdrawAsset
+	txn2.Payload = &core.PayloadWithdrawAsset{
+		BlockHeight:         100,
+		GenesisBlockAddress: "eb7adb1fea0dd6185b09a43bdcd4924bb22bff7151f0b1b4e08699840ab1384b",
+		SideChainTransactionHash: []string{
+			"326218253e6feaa21e3521eff27418b942a5fbd45347505f3e5aca0463baffe2",
+		},
+	}
+
+	txn3 := new(core.Transaction)
+	txn3.TxType = core.WithdrawAsset
+	txn3.Payload = &core.PayloadWithdrawAsset{
+		BlockHeight:         100,
+		GenesisBlockAddress: "eb7adb1fea0dd6185b09a43bdcd4924bb22bff7151f0b1b4e08699840ab1384b",
+		SideChainTransactionHash: []string{
+			"645b614eaaa0a1bfd7015d88f3c1343048343924fc105e403b735ba754caa8db",
+			"9dcad6d4ec2851bf522ddd301c7567caf98554a82a0bcce866de80b503909642",
+		},
+	}
+	txns := []*core.Transaction{txn1, txn2, txn3}
+
+	// 2. Add to sidechain txs pool
+	for _, txn := range txns {
+		witPayload := txn.Payload.(*core.PayloadWithdrawAsset)
+		for _, hash := range witPayload.SideChainTransactionHash {
+			success := txPool.addSidechainTx(hash)
+			if !success {
+				t.Error("Add to sidechain tx pool failed")
+			}
+		}
+	}
+
+	// Verify sidechain tx pool state
+	for _, txn := range txns {
+		err := txPool.verifyDuplicateSidechainTx(txn)
+		if err == nil {
+			t.Error("Should find the duplicate sidechain tx")
+		}
+	}
+
+	// 3. Run cleanSidechainTx
+	txPool.cleanSidechainTx(txns)
+
+	// Verify sidechian tx pool state
+	for _, txn := range txns {
+		err := txPool.verifyDuplicateSidechainTx(txn)
+		if err != nil {
+			t.Error("Should not find the duplicate sidechain tx")
+		}
+	}
+}
