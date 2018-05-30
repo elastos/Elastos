2b221ff7a8137731151ad84d156ea39828d7d047
Yan Minzhi <yanmingzhi@elastos.org>
2018-05-30 19:59:08 +0800

    Complete PayloadRegisterIdentification.

--- SideChain.Release/core/payloadregisteridentification.go
+++ SideChain.Release/core/payloadregisteridentification.go
@@ -1,20 +1,60 @@
 package core
 
-import "io"
+import (
+	"bytes"
+	"errors"
+	"io"
+
+	"github.com/elastos/Elastos.ELA.Utility/common"
+)
 
 const RegisterIdentificationVersion = 0x00
 
-type PayloadRegisterIdentification struct{}
+type PayloadRegisterIdentification struct {
+	ID       string
+	Path     string
+	DataHash common.Uint256
+	Proof    []byte
+	Sign     []byte
+}
 
 func (a *PayloadRegisterIdentification) Data(version byte) []byte {
-	//TODO: implement TransferAsset.Data()
-	return []byte{0}
+	buf := new(bytes.Buffer)
+	a.Serialize(buf, RecordPayloadVersion)
+	return buf.Bytes()
 }
 
 func (a *PayloadRegisterIdentification) Serialize(w io.Writer, version byte) error {
+
+	if err := common.WriteVarString(w, a.ID); err != nil {
+		return errors.New("[RegisterIdentification], ID serialize failed.")
+	}
+
+	if err := common.WriteVarString(w, a.Path); err != nil {
+		return errors.New("[RegisterIdentification], path serialize failed.")
+	}
+
+	if err := common.WriteElements(w, a.DataHash, a.Proof, a.Sign); err != nil {
+		return errors.New("[RegisterIdentification], others serialize failed.")
+	}
 	return nil
 }
 
 func (a *PayloadRegisterIdentification) Deserialize(r io.Reader, version byte) error {
+
+	var err error
+	a.ID, err = common.ReadVarString(r)
+	if err != nil {
+		return errors.New("[RegisterIdentification], ID deserialize failed.")
+	}
+	a.Path, err = common.ReadVarString(r)
+	if err != nil {
+		return errors.New("[RegisterIdentification], path deserialize failed.")
+	}
+
+	if err := common.ReadElements(r, &a.DataHash, &a.Proof, &a.Sign); err != nil {
+		return errors.New("[RegisterIdentification], others deserialize failed.")
+	}
+
 	return nil
 }
