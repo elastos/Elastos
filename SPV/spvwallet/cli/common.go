e429725a10b514872b87f8a181a16c17849c1b30
hejianhui <hejianhui@elastos.org>
2018-06-09 14:58:39 +0800

    re-initialized glide.yaml to solve dependencies problem

--- SPV/spvwallet/cli/common.go
+++ SPV/spvwallet/cli/common.go
@@ -12,7 +12,7 @@ import (
 	"github.com/elastos/Elastos.ELA.SPV/spvwallet/db"
 
 	"github.com/elastos/Elastos.ELA.Utility/common"
-	"github.com//howeyc/gopass"
+	"github.com/howeyc/gopass"
 	"github.com/urfave/cli"
 )
 
