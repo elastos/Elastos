elastos/Elastos.ELA.SideChain

use transaction in core folder instead of ELA transaction

blockchain/txvalidator.go
@@ -6,15 +6,15 @@ import (
 	"math"
 
 	"github.com/elastos/Elastos.ELA.SideChain/config"
+	"github.com/elastos/Elastos.ELA.SideChain/core"
 	. "github.com/elastos/Elastos.ELA.SideChain/errors"
 	"github.com/elastos/Elastos.ELA.SideChain/log"
 
 	. "github.com/elastos/Elastos.ELA.Utility/common"
-	ela "github.com/elastos/Elastos.ELA/core"
 )
 
 // CheckTransactionSanity verifys received single transaction
-func CheckTransactionSanity(txn *ela.Transaction) ErrCode {
+func CheckTransactionSanity(txn *core.Transaction) ErrCode {
 
 	if err := CheckTransactionSize(txn); err != nil {
 		log.Warn("[CheckTransactionSize],", err)
@@ -55,7 +55,7 @@ func CheckTransactionSanity(txn *ela.Transaction) ErrCode {
 }
 
 // CheckTransactionContext verifys a transaction with history transaction in ledger
-func CheckTransactionContext(txn *ela.Transaction) ErrCode {
+func CheckTransactionContext(txn *core.Transaction) ErrCode {
 	// check if duplicated with transaction in ledger
 	if exist := DefaultLedger.Store.IsTxHashDuplicate(txn.Hash()); exist {
 		log.Info("[CheckTransactionContext] duplicate transaction check faild.")
@@ -119,7 +119,7 @@ func CheckTransactionContext(txn *ela.Transaction) ErrCode {
 }
 
 //validate the transaction of duplicate UTXO input
-func CheckTransactionInput(txn *ela.Transaction) error {
+func CheckTransactionInput(txn *core.Transaction) error {
 	if txn.IsCoinBaseTx() {
 		if len(txn.Inputs) != 1 {
 			return errors.New("coinbase must has only one input")
@@ -155,7 +155,7 @@ func CheckTransactionInput(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckTransactionOutput(txn *ela.Transaction) error {
+func CheckTransactionOutput(txn *core.Transaction) error {
 	if txn.IsCoinBaseTx() {
 		if len(txn.Outputs) < 2 {
 			return errors.New("coinbase output is not enough, at least 2")
@@ -202,7 +202,7 @@ func CheckTransactionOutput(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckTransactionUTXOLock(txn *ela.Transaction) error {
+func CheckTransactionUTXOLock(txn *core.Transaction) error {
 	if txn.IsCoinBaseTx() {
 		return nil
 	}
@@ -229,7 +229,7 @@ func CheckTransactionUTXOLock(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckTransactionSize(txn *ela.Transaction) error {
+func CheckTransactionSize(txn *core.Transaction) error {
 	size := txn.GetSize()
 	if size <= 0 || size > config.Parameters.MaxBlockSize {
 		return errors.New(fmt.Sprintf("Invalid transaction size: %d bytes", size))
@@ -238,11 +238,11 @@ func CheckTransactionSize(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckAssetPrecision(txn *ela.Transaction) error {
+func CheckAssetPrecision(txn *core.Transaction) error {
 	if len(txn.Outputs) == 0 {
 		return nil
 	}
-	assetOutputs := make(map[Uint256][]*ela.Output, len(txn.Outputs))
+	assetOutputs := make(map[Uint256][]*core.Output, len(txn.Outputs))
 
 	for _, v := range txn.Outputs {
 		assetOutputs[v.AssetID] = append(assetOutputs[v.AssetID], v)
@@ -262,7 +262,7 @@ func CheckAssetPrecision(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckTransactionBalance(txn *ela.Transaction) error {
+func CheckTransactionBalance(txn *core.Transaction) error {
 	// TODO: check coinbase balance 30%-70%
 	for _, v := range txn.Outputs {
 		if v.Value <= Fixed64(0) {
@@ -282,12 +282,12 @@ func CheckTransactionBalance(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckAttributeProgram(txn *ela.Transaction) error {
+func CheckAttributeProgram(txn *core.Transaction) error {
 	//TODO: implement CheckAttributeProgram
 	return nil
 }
 
-func CheckTransactionSignature(txn *ela.Transaction) error {
+func CheckTransactionSignature(txn *core.Transaction) error {
 	return VerifySignature(txn)
 }
 
@@ -295,23 +295,23 @@ func checkAmountPrecise(amount Fixed64, precision byte) bool {
 	return amount.IntValue()%int64(math.Pow(10, 8-float64(precision))) != 0
 }
 
-func CheckTransactionPayload(txn *ela.Transaction) error {
+func CheckTransactionPayload(txn *core.Transaction) error {
 
 	switch pld := txn.Payload.(type) {
-	case *ela.PayloadRegisterAsset:
-		if pld.Asset.Precision < ela.MinPrecision || pld.Asset.Precision > ela.MaxPrecision {
+	case *core.PayloadRegisterAsset:
+		if pld.Asset.Precision < core.MinPrecision || pld.Asset.Precision > core.MaxPrecision {
 			return errors.New("Invalide asset Precision.")
 		}
 		if checkAmountPrecise(pld.Amount, pld.Asset.Precision) {
 			return errors.New("Invalide asset value,out of precise.")
 		}
-	case *ela.PayloadTransferAsset:
-	case *ela.PayloadRecord:
-	case *ela.PayloadCoinBase:
-	case *ela.PayloadSideMining:
-	case *ela.PayloadWithdrawAsset:
-	case *ela.PayloadIssueToken:
-	case *ela.PayloadTransferCrossChainAsset:
+	case *core.PayloadTransferAsset:
+	case *core.PayloadRecord:
+	case *core.PayloadCoinBase:
+	case *core.PayloadSideMining:
+	case *core.PayloadWithdrawAsset:
+	case *core.PayloadIssueToken:
+	case *core.PayloadTransferCrossChainAsset:
 	default:
 		return errors.New("[txValidator],invalidate transaction payload type.")
 	}

blockchain/validation.go
@@ -3,27 +3,19 @@ package blockchain
 import (
 	"bytes"
 	"errors"
+
+	"github.com/elastos/Elastos.ELA.SideChain/core"
 	"github.com/elastos/Elastos.ELA.SideChain/mainchain"
 	"github.com/elastos/Elastos.ELA.SideChain/spv"
 	"github.com/elastos/Elastos.ELA.SideChain/vm"
+	"github.com/elastos/Elastos.ELA.SideChain/vm/interfaces"
 
 	. "github.com/elastos/Elastos.ELA.Utility/common"
 	"github.com/elastos/Elastos.ELA.Utility/crypto"
-	ela "github.com/elastos/Elastos.ELA/core"
 )
 
-type TxContainer struct {
-	tx *ela.Transaction
-}
-
-func (tc *TxContainer) GetData() []byte {
-	buf := new(bytes.Buffer)
-	tc.tx.SerializeUnsigned(buf)
-	return buf.Bytes()
-}
-
-func VerifySignature(tx *ela.Transaction) error {
-	if tx.TxType == ela.IssueToken {
+func VerifySignature(tx *core.Transaction) error {
+	if tx.TxType == core.IssueToken {
 		if err := spv.VerifyTransaction(tx); err != nil {
 			return errors.New("Issue token transaction validate failed.")
 		}
@@ -35,9 +27,11 @@ func VerifySignature(tx *ela.Transaction) error {
 		return err
 	}
 
-	programs := tx.Programs
-	Length := len(hashes)
-	if Length != len(programs) {
+	return RunPrograms(tx, hashes, tx.Programs)
+}
+
+func RunPrograms(data interfaces.IDataContainer, hashes []Uint168, programs []*core.Program) error {
+	if len(hashes) != len(programs) {
 		return errors.New("The number of data hashes is different with number of programs.")
 	}
 
@@ -51,7 +45,7 @@ func VerifySignature(tx *ela.Transaction) error {
 			return errors.New("The data hashes is different with corresponding program code.")
 		}
 		//execute program on VM
-		se := vm.NewExecutionEngine(&TxContainer{tx: tx}, new(vm.CryptoECDsa), vm.MAXSTEPS, nil, nil)
+		se := vm.NewExecutionEngine(data, new(vm.CryptoECDsa), vm.MAXSTEPS, nil, nil)
 		se.LoadScript(programs[i].Code, false)
 		se.LoadScript(programs[i].Parameter, true)
 		se.Execute()
@@ -73,7 +67,7 @@ func VerifySignature(tx *ela.Transaction) error {
 	return nil
 }
 
-func GetTxProgramHashes(tx *ela.Transaction) ([]Uint168, error) {
+func GetTxProgramHashes(tx *core.Transaction) ([]Uint168, error) {
 	if tx == nil {
 		return nil, errors.New("[Transaction],GetProgramHashes transaction is nil.")
 	}
@@ -89,7 +83,7 @@ func GetTxProgramHashes(tx *ela.Transaction) ([]Uint168, error) {
 		hashes = append(hashes, programHash)
 	}
 	for _, attribute := range tx.Attributes {
-		if attribute.Usage == ela.Script {
+		if attribute.Usage == core.Script {
 			dataHash, err := Uint168FromBytes(attribute.Data)
 			if err != nil {
 				return nil, errors.New("[Transaction], GetProgramHashes err.")
@@ -110,12 +104,12 @@ func GetTxProgramHashes(tx *ela.Transaction) ([]Uint168, error) {
 	return uniqueHashes, nil
 }
 
-func checkCrossChainTransaction(txn *ela.Transaction) error {
+func checkCrossChainTransaction(txn *core.Transaction) error {
 	if !txn.IsIssueTokenTx() {
 		return nil
 	}
 
-	depositPayload, ok := txn.Payload.(*ela.PayloadIssueToken)
+	depositPayload, ok := txn.Payload.(*core.PayloadIssueToken)
 	if !ok {
 		return errors.New("Invalid payload type.")
 	}
@@ -128,7 +122,7 @@ func checkCrossChainTransaction(txn *ela.Transaction) error {
 		mainchain.DbCache = dbCache
 	}
 
-	mainChainTransaction := new(ela.Transaction)
+	mainChainTransaction := new(core.Transaction)
 	reader := bytes.NewReader(depositPayload.MainChainTransaction)
 	if err := mainChainTransaction.Deserialize(reader); err != nil {
 		return errors.New("PayloadIssueToken mainChainTransaction deserialize failed")

spv/spv_module.go
@@ -6,10 +6,11 @@ import (
 	"math/rand"
 
 	"github.com/elastos/Elastos.ELA.SideChain/config"
+	"github.com/elastos/Elastos.ELA.SideChain/core"
+	"github.com/elastos/Elastos.ELA.SideChain/log"
 
 	"github.com/elastos/Elastos.ELA.SPV/interface"
 	spvlog "github.com/elastos/Elastos.ELA.SPV/log"
-	"github.com/elastos/Elastos.ELA.SideChain/log"
 	. "github.com/elastos/Elastos.ELA/bloom"
 	ela "github.com/elastos/Elastos.ELA/core"
 )
@@ -33,12 +34,12 @@ func SpvInit() error {
 	return nil
 }
 
-func VerifyTransaction(tx *ela.Transaction) error {
+func VerifyTransaction(tx *core.Transaction) error {
 	proof := new(MerkleProof)
 	mainChainTransaction := new(ela.Transaction)
 
 	switch object := tx.Payload.(type) {
-	case *ela.PayloadIssueToken:
+	case *core.PayloadIssueToken:
 		reader := bytes.NewReader(object.MerkleProof)
 		if err := proof.Deserialize(reader); err != nil {
 			return errors.New("IssueToken payload deserialize failed")

elastos/Elastos

SideChain/blockchain/txvalidator.go
@@ -6,15 +6,15 @@ import (
 	"math"
 
 	"github.com/elastos/Elastos.ELA.SideChain/config"
+	"github.com/elastos/Elastos.ELA.SideChain/core"
 	. "github.com/elastos/Elastos.ELA.SideChain/errors"
 	"github.com/elastos/Elastos.ELA.SideChain/log"
 
 	. "github.com/elastos/Elastos.ELA.Utility/common"
-	ela "github.com/elastos/Elastos.ELA/core"
 )
 
 // CheckTransactionSanity verifys received single transaction
-func CheckTransactionSanity(txn *ela.Transaction) ErrCode {
+func CheckTransactionSanity(txn *core.Transaction) ErrCode {
 
 	if err := CheckTransactionSize(txn); err != nil {
 		log.Warn("[CheckTransactionSize],", err)
@@ -55,7 +55,7 @@ func CheckTransactionSanity(txn *ela.Transaction) ErrCode {
 }
 
 // CheckTransactionContext verifys a transaction with history transaction in ledger
-func CheckTransactionContext(txn *ela.Transaction) ErrCode {
+func CheckTransactionContext(txn *core.Transaction) ErrCode {
 	// check if duplicated with transaction in ledger
 	if exist := DefaultLedger.Store.IsTxHashDuplicate(txn.Hash()); exist {
 		log.Info("[CheckTransactionContext] duplicate transaction check faild.")
@@ -119,7 +119,7 @@ func CheckTransactionContext(txn *ela.Transaction) ErrCode {
 }
 
 //validate the transaction of duplicate UTXO input
-func CheckTransactionInput(txn *ela.Transaction) error {
+func CheckTransactionInput(txn *core.Transaction) error {
 	if txn.IsCoinBaseTx() {
 		if len(txn.Inputs) != 1 {
 			return errors.New("coinbase must has only one input")
@@ -155,7 +155,7 @@ func CheckTransactionInput(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckTransactionOutput(txn *ela.Transaction) error {
+func CheckTransactionOutput(txn *core.Transaction) error {
 	if txn.IsCoinBaseTx() {
 		if len(txn.Outputs) < 2 {
 			return errors.New("coinbase output is not enough, at least 2")
@@ -202,7 +202,7 @@ func CheckTransactionOutput(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckTransactionUTXOLock(txn *ela.Transaction) error {
+func CheckTransactionUTXOLock(txn *core.Transaction) error {
 	if txn.IsCoinBaseTx() {
 		return nil
 	}
@@ -229,7 +229,7 @@ func CheckTransactionUTXOLock(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckTransactionSize(txn *ela.Transaction) error {
+func CheckTransactionSize(txn *core.Transaction) error {
 	size := txn.GetSize()
 	if size <= 0 || size > config.Parameters.MaxBlockSize {
 		return errors.New(fmt.Sprintf("Invalid transaction size: %d bytes", size))
@@ -238,11 +238,11 @@ func CheckTransactionSize(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckAssetPrecision(txn *ela.Transaction) error {
+func CheckAssetPrecision(txn *core.Transaction) error {
 	if len(txn.Outputs) == 0 {
 		return nil
 	}
-	assetOutputs := make(map[Uint256][]*ela.Output, len(txn.Outputs))
+	assetOutputs := make(map[Uint256][]*core.Output, len(txn.Outputs))
 
 	for _, v := range txn.Outputs {
 		assetOutputs[v.AssetID] = append(assetOutputs[v.AssetID], v)
@@ -262,7 +262,7 @@ func CheckAssetPrecision(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckTransactionBalance(txn *ela.Transaction) error {
+func CheckTransactionBalance(txn *core.Transaction) error {
 	// TODO: check coinbase balance 30%-70%
 	for _, v := range txn.Outputs {
 		if v.Value <= Fixed64(0) {
@@ -282,12 +282,12 @@ func CheckTransactionBalance(txn *ela.Transaction) error {
 	return nil
 }
 
-func CheckAttributeProgram(txn *ela.Transaction) error {
+func CheckAttributeProgram(txn *core.Transaction) error {
 	//TODO: implement CheckAttributeProgram
 	return nil
 }
 
-func CheckTransactionSignature(txn *ela.Transaction) error {
+func CheckTransactionSignature(txn *core.Transaction) error {
 	return VerifySignature(txn)
 }
 
@@ -295,23 +295,23 @@ func checkAmountPrecise(amount Fixed64, precision byte) bool {
 	return amount.IntValue()%int64(math.Pow(10, 8-float64(precision))) != 0
 }
 
-func CheckTransactionPayload(txn *ela.Transaction) error {
+func CheckTransactionPayload(txn *core.Transaction) error {
 
 	switch pld := txn.Payload.(type) {
-	case *ela.PayloadRegisterAsset:
-		if pld.Asset.Precision < ela.MinPrecision || pld.Asset.Precision > ela.MaxPrecision {
+	case *core.PayloadRegisterAsset:
+		if pld.Asset.Precision < core.MinPrecision || pld.Asset.Precision > core.MaxPrecision {
 			return errors.New("Invalide asset Precision.")
 		}
 		if checkAmountPrecise(pld.Amount, pld.Asset.Precision) {
 			return errors.New("Invalide asset value,out of precise.")
 		}
-	case *ela.PayloadTransferAsset:
-	case *ela.PayloadRecord:
-	case *ela.PayloadCoinBase:
-	case *ela.PayloadSideMining:
-	case *ela.PayloadWithdrawFromSideChain:
-	case *ela.PayloadRechargeToSideChain:
-	case *ela.PayloadTransferCrossChainAsset:
+	case *core.PayloadTransferAsset:
+	case *core.PayloadRecord:
+	case *core.PayloadCoinBase:
+	case *core.PayloadSideMining:
+	case *core.PayloadWithdrawFromSideChain:
+	case *core.PayloadRechargeToSideChain:
+	case *core.PayloadTransferCrossChainAsset:
 	default:
 		return errors.New("[txValidator],invalidate transaction payload type.")
 	}

SideChain/blockchain/validation.go
@@ -3,27 +3,19 @@ package blockchain
 import (
 	"bytes"
 	"errors"
+
+	"github.com/elastos/Elastos.ELA.SideChain/core"
 	"github.com/elastos/Elastos.ELA.SideChain/mainchain"
 	"github.com/elastos/Elastos.ELA.SideChain/spv"
 	"github.com/elastos/Elastos.ELA.SideChain/vm"
+	"github.com/elastos/Elastos.ELA.SideChain/vm/interfaces"
 
 	. "github.com/elastos/Elastos.ELA.Utility/common"
 	"github.com/elastos/Elastos.ELA.Utility/crypto"
-	ela "github.com/elastos/Elastos.ELA/core"
 )
 
-type TxContainer struct {
-	tx *ela.Transaction
-}
-
-func (tc *TxContainer) GetData() []byte {
-	buf := new(bytes.Buffer)
-	tc.tx.SerializeUnsigned(buf)
-	return buf.Bytes()
-}
-
-func VerifySignature(tx *ela.Transaction) error {
-	if tx.TxType == ela.RechargeToSideChain {
+func VerifySignature(tx *core.Transaction) error {
+	if tx.TxType == core.RechargeToSideChain {
 		if err := spv.VerifyTransaction(tx); err != nil {
 			return err
 		}
@@ -35,9 +27,11 @@ func VerifySignature(tx *ela.Transaction) error {
 		return err
 	}
 
-	programs := tx.Programs
-	Length := len(hashes)
-	if Length != len(programs) {
+	return RunPrograms(tx, hashes, tx.Programs)
+}
+
+func RunPrograms(data interfaces.IDataContainer, hashes []Uint168, programs []*core.Program) error {
+	if len(hashes) != len(programs) {
 		return errors.New("The number of data hashes is different with number of programs.")
 	}
 
@@ -51,7 +45,7 @@ func VerifySignature(tx *ela.Transaction) error {
 			return errors.New("The data hashes is different with corresponding program code.")
 		}
 		//execute program on VM
-		se := vm.NewExecutionEngine(&TxContainer{tx: tx}, new(vm.CryptoECDsa), vm.MAXSTEPS, nil, nil)
+		se := vm.NewExecutionEngine(data, new(vm.CryptoECDsa), vm.MAXSTEPS, nil, nil)
 		se.LoadScript(programs[i].Code, false)
 		se.LoadScript(programs[i].Parameter, true)
 		se.Execute()
@@ -73,7 +67,7 @@ func VerifySignature(tx *ela.Transaction) error {
 	return nil
 }
 
-func GetTxProgramHashes(tx *ela.Transaction) ([]Uint168, error) {
+func GetTxProgramHashes(tx *core.Transaction) ([]Uint168, error) {
 	if tx == nil {
 		return nil, errors.New("[Transaction],GetProgramHashes transaction is nil.")
 	}
@@ -89,7 +83,7 @@ func GetTxProgramHashes(tx *ela.Transaction) ([]Uint168, error) {
 		hashes = append(hashes, programHash)
 	}
 	for _, attribute := range tx.Attributes {
-		if attribute.Usage == ela.Script {
+		if attribute.Usage == core.Script {
 			dataHash, err := Uint168FromBytes(attribute.Data)
 			if err != nil {
 				return nil, errors.New("[Transaction], GetProgramHashes err.")
@@ -110,12 +104,12 @@ func GetTxProgramHashes(tx *ela.Transaction) ([]Uint168, error) {
 	return uniqueHashes, nil
 }
 
-func checkCrossChainTransaction(txn *ela.Transaction) error {
+func checkCrossChainTransaction(txn *core.Transaction) error {
 	if !txn.IsRechargeToSideChainTx() {
 		return nil
 	}
 
-	depositPayload, ok := txn.Payload.(*ela.PayloadRechargeToSideChain)
+	depositPayload, ok := txn.Payload.(*core.PayloadRechargeToSideChain)
 	if !ok {
 		return errors.New("Invalid payload type.")
 	}
@@ -128,7 +122,7 @@ func checkCrossChainTransaction(txn *ela.Transaction) error {
 		mainchain.DbCache = dbCache
 	}
 
-	mainChainTransaction := new(ela.Transaction)
+	mainChainTransaction := new(core.Transaction)
 	reader := bytes.NewReader(depositPayload.MainChainTransaction)
 	if err := mainChainTransaction.Deserialize(reader); err != nil {
 		return errors.New("PayloadRechargeToSideChain mainChainTransaction deserialize failed")

SideChain/spv/spv_module.go
@@ -8,6 +8,8 @@ import (
 	"os"
 
 	"github.com/elastos/Elastos.ELA.SideChain/config"
+
+	"github.com/elastos/Elastos.ELA.SideChain/core"
 	"github.com/elastos/Elastos.ELA.SideChain/log"
 
 	"github.com/elastos/Elastos.ELA.SPV/interface"
@@ -44,12 +46,12 @@ func SpvInit() error {
 	return nil
 }
 
-func VerifyTransaction(tx *ela.Transaction) error {
+func VerifyTransaction(tx *core.Transaction) error {
 	proof := new(MerkleProof)
 	mainChainTransaction := new(ela.Transaction)
 
 	switch object := tx.Payload.(type) {
-	case *ela.PayloadRechargeToSideChain:
+	case *core.PayloadRechargeToSideChain:
 		reader := bytes.NewReader(object.MerkleProof)
 		if err := proof.Deserialize(reader); err != nil {
 			return errors.New("RechargeToSideChain payload deserialize failed")