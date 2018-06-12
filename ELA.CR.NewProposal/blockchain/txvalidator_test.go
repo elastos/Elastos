a9fe7c305471f968a4b5b1f2dc6da63cde3c4c23
AlexPan <ppgod@live.cn>
2018-06-12 15:55:55 +0800

    fix pow mining block verify get Reward to foundation in coinbase < 30% error issue

--- ELA.CR.NewProposal/blockchain/txvalidator_test.go
+++ ELA.CR.NewProposal/blockchain/txvalidator_test.go
@@ -164,10 +164,28 @@ func TestCheckTransactionOutput(t *testing.T) {
 	err = CheckTransactionOutput(core.CheckTxOut, tx)
 	assert.EqualError(t, err, "asset ID in coinbase is invalid")
 
-	// reward to foundation in coinbase < 30%
+	// reward to foundation in coinbase = 30%
+	totalReward := GetBlockRewardAmount(0)
+	t.Logf("Block reward amount %s", totalReward.String())
+	foundationReward := common.Fixed64(float64(totalReward)*0.3)
+	t.Logf("Foundation reward amount %s", foundationReward.String())
+	minerReward := totalReward - foundationReward
+	t.Logf("Miner reward amount %s", minerReward.String())
 	tx.Outputs = []*core.Output{
-		{AssetID: DefaultLedger.Blockchain.AssetID, ProgramHash: FoundationAddress, Value: common.Fixed64(29 * ELA)},
-		{AssetID: DefaultLedger.Blockchain.AssetID, ProgramHash: common.Uint168{}, Value: common.Fixed64(71 * ELA)},
+		{AssetID: DefaultLedger.Blockchain.AssetID, ProgramHash: FoundationAddress, Value: foundationReward},
+		{AssetID: DefaultLedger.Blockchain.AssetID, ProgramHash: common.Uint168{}, Value: minerReward},
+	}
+	err = CheckTransactionOutput(core.CheckTxOut, tx)
+	assert.NoError(t, err)
+
+	// reward to foundation in coinbase < 30%
+	foundationReward = common.Fixed64(float64(totalReward)*0.2999999)
+	t.Logf("Foundation reward amount %s", foundationReward.String())
+	minerReward = totalReward - foundationReward
+	t.Logf("Miner reward amount %s", minerReward.String())
+	tx.Outputs = []*core.Output{
+		{AssetID: DefaultLedger.Blockchain.AssetID, ProgramHash: FoundationAddress, Value: foundationReward},
+		{AssetID: DefaultLedger.Blockchain.AssetID, ProgramHash: common.Uint168{}, Value: minerReward},
 	}
 	err = CheckTransactionOutput(core.CheckTxOut, tx)
 	assert.EqualError(t, err, "Reward to foundation in coinbase < 30%")
