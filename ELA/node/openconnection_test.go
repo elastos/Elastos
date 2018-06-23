afd33cb6bb9bc57fead9e7ffbae8b01f77541223
AlexPan <ppgod@live.cn>
2018-06-23 19:21:01 +0800

    enable InvTypeTx message to support SPV wallet

--- ELA/node/openconnection_test.go
+++ ELA/node/openconnection_test.go
@@ -5,7 +5,10 @@ import (
 
 	"github.com/elastos/Elastos.ELA/config"
 
+	"github.com/elastos/Elastos.ELA.Utility/common"
 	"github.com/elastos/Elastos.ELA.Utility/p2p"
+	"github.com/elastos/Elastos.ELA.Utility/p2p/msg"
+	"github.com/elastos/Elastos.ELA.Utility/p2p/msg/v0"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,7 +17,7 @@ This test is for the p2p connections come from the extra net.
 There are several rules in open connections:
 1. will be marked as from extra net
 2. can not send addr message
-3. can not send inventory message
+3. can not send inventory type block message
 4. can not send block message
 5. can not send notfound message
 6. can not send reject message
@@ -26,7 +29,30 @@ func TestOpenConnectionInit(t *testing.T) {
 	initLocalNode(t)
 }
 
-func TestInvalidMessages(t *testing.T) {
+func TestV0Messages(t *testing.T) {
+	handler := newTestHandlerV0(t, config.Parameters.NodeOpenPort)
+
+	// check if the node is marked as extra node
+	assert.Equal(t, true, handler.that.IsFromExtraNet())
+
+	// addr message should be rejected
+	err := handler.Write(newMessage(p2p.CmdAddr))
+	assert.EqualError(t, err, "[MsgHelper] make message failed unsupported messsage type [addr] from extra node")
+
+	// inventory message type block should be rejected
+	err = handler.Write(v0.NewInv([]*common.Uint256{&common.EmptyHash}))
+	assert.EqualError(t, err, "receive inv message from extra node")
+
+	// block message should be rejected
+	err = handler.Write(newMessage(p2p.CmdBlock))
+	assert.EqualError(t, err, "[MsgHelper] make message failed unsupported messsage type [block] from extra node")
+
+	// notfound message should be rejected
+	err = handler.Write(newMessage(p2p.CmdNotFound))
+	assert.EqualError(t, err, "[MsgHelper] make message failed unsupported messsage type [notfound] from extra node")
+}
+
+func TestEIP001Messages(t *testing.T) {
 	handler := newTestHandlerEIP001(t, config.Parameters.NodeOpenPort)
 
 	// check if the node is marked as extra node
@@ -36,9 +62,11 @@ func TestInvalidMessages(t *testing.T) {
 	err := handler.Write(newMessage(p2p.CmdAddr))
 	assert.EqualError(t, err, "[MsgHelper] make message failed unsupported messsage type [addr] from extra node")
 
-	// inventory message should be rejected
-	err = handler.Write(newMessage(p2p.CmdInv))
-	assert.EqualError(t, err, "[MsgHelper] make message failed unsupported messsage type [inv] from extra node")
+	// inventory message type block should be rejected
+	inv := msg.NewInventory()
+	inv.AddInvVect(msg.NewInvVect(msg.InvTypeBlock, &common.EmptyHash))
+	err = handler.Write(inv)
+	assert.EqualError(t, err, "receive InvTypeBlock from extra node")
 
 	// block message should be rejected
 	err = handler.Write(newMessage(p2p.CmdBlock))
