eedb2c1f849b63329749f50a9a8cfcb5cfc8cff1
Yan Minzhi <yanmingzhi@elastos.org>
2018-08-09 17:47:59 +0800

    Modify content of register identification payload.

--- SideChain/core/payloadregisteridentification.go
+++ SideChain/core/payloadregisteridentification.go
@@ -10,12 +10,16 @@ import (
 
 const RegisterIdentificationVersion = 0x00
 
-type PayloadRegisterIdentification struct {
-	ID       string
+type RegisterIdentificationContent struct {
 	Path     string
 	DataHash common.Uint256
 	Proof    string
+}
+
+type PayloadRegisterIdentification struct {
+	ID       string
 	Sign     []byte
+	Contents []RegisterIdentificationContent
 }
 
 func (a *PayloadRegisterIdentification) Data(version byte) []byte {
@@ -30,21 +34,29 @@ func (a *PayloadRegisterIdentification) Serialize(w io.Writer, version byte) err
 		return errors.New("[RegisterIdentification], ID serialize failed.")
 	}
 
-	if err := common.WriteVarString(w, a.Path); err != nil {
-		return errors.New("[RegisterIdentification], path serialize failed.")
-	}
-
-	if err := common.WriteElement(w, a.DataHash); err != nil {
-		return errors.New("[RegisterIdentification], DataHash serialize failed.")
-	}
-
-	if err := common.WriteVarString(w, a.Proof); err != nil {
-		return errors.New("[RegisterIdentification], Proof serialize failed.")
-	}
-
 	if err := common.WriteElement(w, a.Sign); err != nil {
 		return errors.New("[RegisterIdentification], Sign serialize failed.")
 	}
+
+	if err := common.WriteVarUint(w, uint64(len(a.Contents))); err != nil {
+		return errors.New("[RegisterIdentification], Content size serialize failed.")
+	}
+
+	for _, content := range a.Contents {
+
+		if err := common.WriteVarString(w, content.Path); err != nil {
+			return errors.New("[RegisterIdentification], path serialize failed.")
+		}
+
+		if err := common.WriteElement(w, content.DataHash); err != nil {
+			return errors.New("[RegisterIdentification], DataHash serialize failed.")
+		}
+
+		if err := common.WriteVarString(w, content.Proof); err != nil {
+			return errors.New("[RegisterIdentification], Proof serialize failed.")
+		}
+	}
+
 	return nil
 }
 
@@ -56,22 +68,33 @@ func (a *PayloadRegisterIdentification) Deserialize(r io.Reader, version byte) e
 		return errors.New("[RegisterIdentification], ID deserialize failed.")
 	}
 
-	a.Path, err = common.ReadVarString(r)
-	if err != nil {
-		return errors.New("[RegisterIdentification], path deserialize failed.")
-	}
-
-	if err := common.ReadElement(r, &a.DataHash); err != nil {
-		return errors.New("[RegisterIdentification], DataHash deserialize failed.")
-	}
-
-	a.Proof, err = common.ReadVarString(r)
-	if err != nil {
-		return errors.New("[RegisterIdentification], Proof deserialize failed.")
-	}
-
 	if err := common.ReadElement(r, &a.Sign); err != nil {
 		return errors.New("[RegisterIdentification], Sign deserialize failed.")
+	}
+
+	size, err := common.ReadVarUint(r, 0)
+	if err != nil {
+		return errors.New("[RegisterIdentification], Content size deserialize failed.")
+	}
+
+	a.Contents = make([]RegisterIdentificationContent, size)
+	for i := uint64(0); i < size; i++ {
+		content := RegisterIdentificationContent{}
+
+		content.Path, err = common.ReadVarString(r)
+		if err != nil {
+			return errors.New("[RegisterIdentification], path deserialize failed.")
+		}
+
+		if err := common.ReadElement(r, &content.DataHash); err != nil {
+			return errors.New("[RegisterIdentification], DataHash deserialize failed.")
+		}
+
+		content.Proof, err = common.ReadVarString(r)
+		if err != nil {
+			return errors.New("[RegisterIdentification], Proof deserialize failed.")
+		}
+		a.Contents[i] = content
 	}
 
 	return nil
