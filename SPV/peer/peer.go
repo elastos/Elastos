0245b26a7229214c5d8d95d80677d8575284d4cc
AlexPan <ppgod@live.cn>
2018-09-08 13:34:14 +0800

    initial peer package implement

--- SPV/peer/peer.go
+++ SPV/peer/peer.go
@@ -1,15 +1,17 @@
-package sdk
+package peer
 
 import (
+	"container/list"
+	"sync"
 	"time"
 
-	"github.com/elastos/Elastos.ELA.SPV/log"
-	"github.com/elastos/Elastos.ELA.SPV/net"
+	"github.com/elastos/Elastos.ELA.SPV/util"
 
 	"github.com/elastos/Elastos.ELA.Utility/common"
 	"github.com/elastos/Elastos.ELA.Utility/p2p"
 	"github.com/elastos/Elastos.ELA.Utility/p2p/msg"
-	"github.com/elastos/Elastos.ELA.Utility/p2p/rw"
+	"github.com/elastos/Elastos.ELA.Utility/p2p/peer"
+	"github.com/elastos/Elastos.ELA/bloom"
 	"github.com/elastos/Elastos.ELA/core"
 )
 
@@ -23,193 +25,123 @@ const (
 	// stalling.  The deadlines are adjusted for callback running times and
 	// only checked on each stall tick interval.
 	stallResponseTimeout = 15 * time.Second
+
+	// outputBufferSize is the number of elements the output channels use.
+	outputBufferSize = 50
 )
 
-func (p *SPVPeer) EnqueueBlock(header *msg.MerkleBlock, txIds []*common.Uint256) *block {
-	hash := header.Header.(*core.Header).Hash()
-
-	block := newBlock(header)
-
-	// No transactions to download, just finish it
-	if len(txIds) == 0 {
-		return block
-	}
-
-	// Download transactions of this block
-	getData := msg.NewGetData()
-	for _, txId := range txIds {
-		block.txQueue[*txId] = struct{}{}
-		getData.AddInvVect(msg.NewInvVect(msg.InvTypeTx, txId))
-	}
-	// Stall message
-	p.stallControl <- getData
-
-	p.blocksQueue[hash] = block
-
-	return nil
-}
-
-func (p *SPVPeer) EnqueueTx(txId common.Uint256) {
-	p.txsQueue[txId] = struct{}{}
-}
-
-func (p *SPVPeer) DequeueTx(tx *core.Transaction) (download interface{}, finished bool) {
-	// Get tx id
-	txId := tx.Hash()
-
-	// Check downloading block first
-	if blockId, ok := p.blockTxs[txId]; ok {
-		// Dequeue block tx
-		block := p.blocksQueue[blockId]
-		block.txs = append(block.txs, tx)
-		delete(block.txQueue, txId)
-
-		return block, len(block.txQueue) == 0
-	}
-
-	// Dequeue from downloading tx
-	_, ok := p.txsQueue[txId]
-	if !ok {
-		return tx, false
-	}
-	delete(p.txsQueue, txId)
-
-	return tx, true
-}
-
-type block struct {
-	*msg.MerkleBlock
-	txQueue map[common.Uint256]struct{}
-	txs     []*core.Transaction
-}
-
-func newBlock(header *msg.MerkleBlock) *block {
-	return &block{
-		MerkleBlock: header,
-		txQueue:     make(map[common.Uint256]struct{}),
-	}
-}
-
-type SPVPeerConfig struct {
-	// LocalHeight is invoked when peer queue a ping or pong message
-	LocalHeight func() uint32
-
+// Config is the struct to hold configuration options useful to Peer.
+type Config struct {
 	// After send a blocks request message, this inventory message
 	// will return with a bunch of block hashes, then you can use them
 	// to request all the blocks by send data requests.
-	OnInventory func(*SPVPeer, *msg.Inventory) error
+	OnInv func(*Peer, *msg.Inv)
 
-	// After sent a data request with invType BLOCK, a merkleblock message will return through this method.
-	// To make this work, you must register a filterload message to the connected peer first,
-	// then this client will be known as a SPV client. To create a bloom filter and get the
-	// filterload message, you will use the method in SDK bloom sdk.NewBloomFilter()
-	// merkleblock includes a block header, transaction hashes in merkle proof format.
-	// Which transaction hashes will be in the merkleblock is depends on the addresses and outpoints
-	// you've added into the bloom filter before you send a filterload message with this bloom filter.
-	// You will use these transaction hashes to request transactions by sending data request message
-	// with invType TRANSACTION
-	OnMerkleBlock func(*SPVPeer, *msg.MerkleBlock) error
+	// This method will be invoked when a merkleblock and transactions
+	// within it has been downloaded.
+	OnBlock func(*Peer, *util.Block)
 
 	// After sent a data request with invType TRANSACTION, a txn message will return through this method.
 	// these transactions are matched to the bloom filter you have sent with the filterload message.
-	OnTx func(*SPVPeer, *msg.Tx) error
+	OnTx func(*Peer, *core.Transaction)
 
 	// If the BLOCK or TRANSACTION requested by the data request message can not be found,
 	// notfound message with requested data hash will return through this method.
-	OnNotFound func(*SPVPeer, *msg.NotFound) error
+	OnNotFound func(*Peer, *msg.NotFound)
 
 	// If the submitted transaction was rejected, this message will return.
-	OnReject func(*SPVPeer, *msg.Reject) error
+	OnReject func(*Peer, *msg.Reject)
 }
 
-type SPVPeer struct {
-	*net.Peer
+// outMsg is used to house a message to be sent along with a channel to signal
+// when the message has been sent (or won't be sent due to things such as
+// shutdown)
+type outMsg struct {
+	msg      p2p.Message
+	doneChan chan<- struct{}
+}
 
-	blockQueue  chan common.Uint256
-	blocksQueue map[common.Uint256]*block
-	blockTxs    map[common.Uint256]common.Uint256
-	txsQueue    map[common.Uint256]struct{}
-	receivedTxs int
-	fPositives  int
+type Peer struct {
+	*peer.Peer
+	cfg Config
+
+	prevGetBlocksMtx   sync.Mutex
+	prevGetBlocksBegin *common.Uint256
+	prevGetBlocksStop  *common.Uint256
 
 	stallControl chan p2p.Message
+	blockQueue   chan p2p.Message
+	outputQueue  chan outMsg
+	queueQuit    chan struct{}
 }
 
-func NewSPVPeer(peer *net.Peer, config SPVPeerConfig) *SPVPeer {
-	spvPeer := &SPVPeer{
+func NewPeer(peer *peer.Peer, cfg *Config) *Peer {
+	p := Peer{
 		Peer:         peer,
-		blockQueue:   make(chan common.Uint256, p2p.MaxBlocksPerMsg),
-		blocksQueue:  make(map[common.Uint256]*block),
-		blockTxs:     make(map[common.Uint256]common.Uint256),
-		txsQueue:     make(map[common.Uint256]struct{}),
+		cfg:          *cfg,
 		stallControl: make(chan p2p.Message, 1),
+		blockQueue:   make(chan p2p.Message, 1),
+		outputQueue:  make(chan outMsg, outputBufferSize),
+		queueQuit:    make(chan struct{}),
 	}
+	peer.AddMessageFunc(p.handleMessage)
 
-	msgConfig := rw.Config{
-		ProtocolVersion: p2p.EIP001Version,
-		MakeTx:          func() *msg.Tx { return msg.NewTx(new(core.Transaction)) },
-		MakeBlock:       func() *msg.Block { return msg.NewBlock(new(core.Block)) },
-		MakeMerkleBlock: func() *msg.MerkleBlock { return msg.NewMerkleBlock(new(core.Header)) },
-	}
+	go p.stallHandler()
+	go p.blockHandler()
 
-	spvPeer.SetMessageConfig(msgConfig)
-
-	peerConfig := net.PeerConfig{
-		PingNonce: config.LocalHeight,
-
-		PongNonce: config.LocalHeight,
-
-		OnPing: func(peer *net.Peer, ping *msg.Ping) {
-			peer.SetHeight(ping.Nonce)
-		},
-
-		OnPong: func(peer *net.Peer, pong *msg.Pong) {
-			peer.SetHeight(pong.Nonce)
-		},
-
-		HandleMessage: func(peer *net.Peer, message p2p.Message) {
-			// Notify stall control
-			spvPeer.stallControl <- message
-
-			switch m := message.(type) {
-			case *msg.Inventory:
-				config.OnInventory(spvPeer, m)
-
-			case *msg.MerkleBlock:
-				config.OnMerkleBlock(spvPeer, m)
-
-			case *msg.Tx:
-				config.OnTx(spvPeer, m)
-
-			case *msg.NotFound:
-				config.OnNotFound(spvPeer, m)
-
-			case *msg.Reject:
-				config.OnReject(spvPeer, m)
-			}
-		},
-	}
-
-	spvPeer.SetPeerConfig(peerConfig)
-
-	go spvPeer.stallHandler()
-
-	return spvPeer
+	go func() {
+		// We have waited on queueQuit and thus we can be sure
+		// that we will not miss anything sent on sendQueue.
+		<-p.queueQuit
+		p.CleanupSendQueue()
+	}()
+	return &p
 }
 
-func (p *SPVPeer) stallHandler() {
+func (p *Peer) sendMessage(msg outMsg) {
+	p.stallControl <- msg.msg
+	p.SendMessage(msg.msg, msg.doneChan)
+}
+
+func (p *Peer) handleMessage(peer *peer.Peer, message p2p.Message) {
+	// Notify stall control
+	p.stallControl <- message
+
+	switch m := message.(type) {
+	case *msg.Inv:
+		p.cfg.OnInv(p, m)
+
+	case *msg.MerkleBlock:
+		p.blockQueue <- m
+
+	case *msg.Tx:
+		p.blockQueue <- m
+
+	case *msg.NotFound:
+		p.cfg.OnNotFound(p, m)
+
+	case *msg.Reject:
+		p.cfg.OnReject(p, m)
+	}
+}
+
+func (p *Peer) stallHandler() {
+	// lastActive tracks the last active sync message.
+	var lastActive time.Time
+
+	// pendingResponses tracks the expected responses.
+	pendingResponses := make(map[string]struct{})
+
 	// stallTicker is used to periodically check pending responses that have
 	// exceeded the expected deadline and disconnect the peer due to stalling.
 	stallTicker := time.NewTicker(stallTickInterval)
 	defer stallTicker.Stop()
 
-	// pendingResponses tracks the expected responses.
-	pendingResponses := make(map[string]struct{})
-
-	// lastActive tracks the last active sync message.
-	var lastActive time.Time
-
-	for p.Connected() {
+	// ioStopped is used to detect when both the input and output handler
+	// goroutines are done.
+	var ioStopped bool
+out:
+	for {
 		select {
 		case ctrMsg := <-p.stallControl:
 			// update last active time
@@ -220,7 +152,7 @@ func (p *SPVPeer) stallHandler() {
 				// Add expected response
 				pendingResponses[p2p.CmdInv] = struct{}{}
 
-			case *msg.Inventory:
+			case *msg.Inv:
 				// Remove inventory from expected response map.
 				delete(pendingResponses, p2p.CmdInv)
 
@@ -236,7 +168,7 @@ func (p *SPVPeer) stallHandler() {
 
 			case *msg.Tx:
 				// Remove received transaction from expected response map.
-				delete(pendingResponses, message.Transaction.(*core.Transaction).Hash().String())
+				delete(pendingResponses, message.Serializable.(*core.Transaction).Hash().String())
 
 			case *msg.NotFound:
 				// NotFound should not received from sync peer
@@ -257,6 +189,22 @@ func (p *SPVPeer) stallHandler() {
 
 			log.Debugf("peer %v appears to be stalled or misbehaving, response timeout -- disconnecting", p)
 			p.Disconnect()
+
+		case <-p.InQuit():
+			// The stall handler can exit once both the input and
+			// output handler goroutines are done.
+			if ioStopped {
+				break out
+			}
+			ioStopped = true
+
+		case <-p.OutQuit():
+			// The stall handler can exit once both the input and
+			// output handler goroutines are done.
+			if ioStopped {
+				break out
+			}
+			ioStopped = true
 		}
 	}
 
@@ -273,30 +221,253 @@ cleanup:
 	log.Tracef("Peer stall handler done for %v", p)
 }
 
-// Add message to output queue and wait until message sent
-func (p *SPVPeer) SendMessage(message p2p.Message) {
-	doneChan := make(chan struct{})
-	p.queueMessage(message, doneChan)
-	<-doneChan
-}
+// blockHandler handles the downloading of merkleblock and the transactions
+// within it.  We handle a merkleblock and it's transactions as one block.
+// We do not notify this new block until the downloading has been completed.
+func (p *Peer) blockHandler() {
+	// Data caches for the downloading block.
+	var header *util.Header
+	var pendingTxs map[common.Uint256]struct{}
+	var txs []*util.Tx
 
-// Add a message into output queue
-func (p *SPVPeer) QueueMessage(message p2p.Message) {
-	p.queueMessage(message, nil)
-}
+	// NotifyOnBlock message and clear cached data.
+	notifyBlock := func() {
+		// Notify OnBlock.
+		p.cfg.OnBlock(p, &util.Block{
+			Header:       header,
+			Transactions: txs,
+		})
 
-func (p *SPVPeer) queueMessage(message p2p.Message, doneChan chan struct{}) {
-	switch message.(type) {
-	case *msg.GetBlocks, *msg.GetData:
-		p.stallControl <- message
+		// Clear cached data.
+		header = nil
+		pendingTxs = nil
+		txs = nil
 	}
-	p.Peer.QueueMessage(message, doneChan)
+
+out:
+	for {
+		select {
+		case bmsg := <-p.blockQueue:
+			switch m := bmsg.(type) {
+			case *msg.MerkleBlock:
+				// If header is not nil, the previous block download was not
+				// finished, that means the peer is misbehaving, disconnect it.
+				if header != nil {
+					log.Debugf("peer %v send us new block before previous"+
+						" block download finished -- disconnecting", p)
+					p.Disconnect()
+					continue
+				}
+
+				// Check block
+				txIds, err := bloom.CheckMerkleBlock(*m)
+				if err != nil {
+					log.Debugf("peer %v send us invalid merkleblock -- disconnecting", p)
+					p.Disconnect()
+					continue
+				}
+
+				// No transaction included in this block, so just notify block
+				// downloading completed.
+				if len(txIds) == 0 {
+					notifyBlock()
+					continue
+				}
+
+				// Set current downloading block
+				header = &util.Header{
+					Header: m.Header.(*core.Header),
+					NumTxs: m.Transactions,
+					Hashes: m.Hashes,
+					Flags:  m.Flags,
+				}
+
+				// Save pending transactions to cache.
+				pendingTxs = make(map[common.Uint256]struct{})
+				for _, txId := range txIds {
+					pendingTxs[*txId] = struct{}{}
+				}
+
+				// Initiate transactions cache.
+				txs = make([]*util.Tx, 0, len(pendingTxs))
+
+			case *msg.Tx:
+				// Not in block downloading mode, just notify new transaction.
+				tx := m.Serializable.(*core.Transaction)
+				if header == nil {
+					p.cfg.OnTx(p, tx)
+					continue
+				}
+
+				txId := tx.Hash()
+
+				// When downloading block, received transactions can only by
+				// those within the block.
+				if _, ok := pendingTxs[txId]; !ok {
+					log.Debugf("peer %v send us invalid transaction -- disconnecting", p)
+					p.Disconnect()
+					continue
+				}
+
+				// Save downloaded transaction to cache.
+				txs = append(txs, &util.Tx{
+					Transaction: tx,
+					Height:      header.Height,
+				})
+
+				// Remove transaction from pending list.
+				delete(pendingTxs, txId)
+
+				// Block download completed, notify OnBlock.
+				if len(pendingTxs) == 0 {
+					notifyBlock()
+				}
+			}
+
+		case <-p.Quit():
+			break out
+		}
+	}
+
+	// Drain any wait channels before going away so there is nothing left
+	// waiting on this goroutine.
+cleanup:
+	for {
+		select {
+		case <-p.blockQueue:
+		default:
+			break cleanup
+		}
+	}
+	log.Tracef("Peer block handler done for %v", p)
 }
 
-func (p *SPVPeer) GetFalsePositiveRate() float32 {
-	return float32(p.fPositives) / float32(p.receivedTxs)
+// queueHandler handles the queuing of outgoing data for the peer. This runs as
+// a muxer for various sources of input so we can ensure that server and peer
+// handlers will not block on us sending a message.  That data is then passed on
+// to outHandler to be actually written.
+func (p *Peer) queueHandler() {
+	pendingMsgs := list.New()
+
+	// We keep the waiting flag so that we know if we have a message queued
+	// to the outHandler or not.  We could use the presence of a head of
+	// the list for this but then we have rather racy concerns about whether
+	// it has gotten it at cleanup time - and thus who sends on the
+	// message's done channel.  To avoid such confusion we keep a different
+	// flag and pendingMsgs only contains messages that we have not yet
+	// passed to outHandler.
+	waiting := false
+
+	// To avoid duplication below.
+	queuePacket := func(msg outMsg, list *list.List, waiting bool) bool {
+		if !waiting {
+			p.sendMessage(msg)
+		} else {
+			list.PushBack(msg)
+		}
+		// we are always waiting now.
+		return true
+	}
+out:
+	for {
+		select {
+		case msg := <-p.outputQueue:
+			waiting = queuePacket(msg, pendingMsgs, waiting)
+
+			// This channel is notified when a message has been sent across
+			// the network socket.
+		case <-p.SendDoneQueue():
+			// No longer waiting if there are no more messages
+			// in the pending messages queue.
+			next := pendingMsgs.Front()
+			if next == nil {
+				waiting = false
+				continue
+			}
+
+			// Notify the outHandler about the next item to
+			// asynchronously send.
+			val := pendingMsgs.Remove(next)
+			p.sendMessage(val.(outMsg))
+
+		case <-p.Quit():
+			break out
+		}
+	}
+
+	// Drain any wait channels before we go away so we don't leave something
+	// waiting for us.
+	for e := pendingMsgs.Front(); e != nil; e = pendingMsgs.Front() {
+		val := pendingMsgs.Remove(e)
+		msg := val.(outMsg)
+		if msg.doneChan != nil {
+			msg.doneChan <- struct{}{}
+		}
+	}
+cleanup:
+	for {
+		select {
+		case msg := <-p.outputQueue:
+			if msg.doneChan != nil {
+				msg.doneChan <- struct{}{}
+			}
+		default:
+			break cleanup
+		}
+	}
+	close(p.queueQuit)
+	log.Tracef("Peer queue handler done for %s", p)
 }
 
-func (p *SPVPeer) ResetFalsePositives() {
-	p.fPositives, p.receivedTxs = 0, 0
+// PushGetBlocksMsg sends a getblocks message for the provided block locator
+// and stop hash.  It will ignore back-to-back duplicate requests.
+//
+// This function is safe for concurrent access.
+func (p *Peer) PushGetBlocksMsg(locator []*common.Uint256, stopHash *common.Uint256) error {
+	// Extract the begin hash from the block locator, if one was specified,
+	// to use for filtering duplicate getblocks requests.
+	var beginHash *common.Uint256
+	if len(locator) > 0 {
+		beginHash = locator[0]
+	}
+
+	// Filter duplicate getblocks requests.
+	p.prevGetBlocksMtx.Lock()
+	isDuplicate := p.prevGetBlocksStop != nil && p.prevGetBlocksBegin != nil &&
+		beginHash != nil && stopHash.IsEqual(*p.prevGetBlocksStop) &&
+		beginHash.IsEqual(*p.prevGetBlocksBegin)
+	p.prevGetBlocksMtx.Unlock()
+
+	if isDuplicate {
+		log.Tracef("Filtering duplicate [getblocks] with begin "+
+			"hash %v, stop hash %v", beginHash, stopHash)
+		return nil
+	}
+
+	// Construct the getblocks request and queue it to be sent.
+	msg := msg.NewGetBlocks(locator, *stopHash)
+	p.QueueMessage(msg, nil)
+
+	// Update the previous getblocks request information for filtering
+	// duplicates.
+	p.prevGetBlocksMtx.Lock()
+	p.prevGetBlocksBegin = beginHash
+	p.prevGetBlocksStop = stopHash
+	p.prevGetBlocksMtx.Unlock()
+	return nil
+}
+
+func (p *Peer) QueueMessage(msg p2p.Message, doneChan chan<- struct{}) {
+	// Avoid risk of deadlock if goroutine already exited.  The goroutine
+	// we will be sending to hangs around until it knows for a fact that
+	// it is marked as disconnected and *then* it drains the channels.
+	if !p.Connected() {
+		if doneChan != nil {
+			go func() {
+				doneChan <- struct{}{}
+			}()
+		}
+		return
+	}
+	p.outputQueue <- outMsg{msg: msg, doneChan: doneChan}
 }
