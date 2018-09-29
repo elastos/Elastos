56d8f1f6f29963e743dcd1a5eee4ec4c5a2124f9
AlexPan <ppgod@live.cn>
2018-09-29 17:15:43 +0800

    add pact package to place protocol constants

---
+++ SideChain/pact/protocol.go
@@ -0,0 +1,14 @@
+package pact
+
+import "github.com/elastos/Elastos.ELA.Utility/p2p"
+
+// Release version numbers
+const (
+	// The Release_0.0.1 version number of side chain
+	Release001Version = 1
+)
+
+// Services
+const (
+	OpenService p2p.ServiceFlag = 1 << 2
+)
