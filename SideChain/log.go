f35af9ac8ac7bbf9e3a77e89279ff931261991a2
AlexPan <ppgod@live.cn>
2018-09-29 13:55:06 +0800

    add more log outputs

--- SideChain/log.go
+++ SideChain/log.go
@@ -6,11 +6,15 @@ import (
 	"github.com/elastos/Elastos.ELA.SideChain/logger"
 	"github.com/elastos/Elastos.ELA.SideChain/mempool"
 	"github.com/elastos/Elastos.ELA.SideChain/netsync"
-	"github.com/elastos/Elastos.ELA.SideChain/peer"
 	"github.com/elastos/Elastos.ELA.SideChain/pow"
 	"github.com/elastos/Elastos.ELA.SideChain/servers"
 	"github.com/elastos/Elastos.ELA.SideChain/servers/httpjsonrpc"
 	"github.com/elastos/Elastos.ELA.SideChain/servers/httprestful"
+
+	"github.com/elastos/Elastos.ELA.Utility/p2p/addrmgr"
+	"github.com/elastos/Elastos.ELA.Utility/p2p/connmgr"
+	"github.com/elastos/Elastos.ELA.Utility/p2p/peer"
+	p2pserver "github.com/elastos/Elastos.ELA.Utility/p2p/server"
 )
 
 const LogPath = "./logs/"
@@ -26,6 +30,8 @@ var (
 		config.Parameters.MaxLogsSize,
 	)
 
+	admrlog = elalog.Logger("ADMR")
+	cmgrlog = elalog.Logger("CMGR")
 	bcdblog = elalog.Logger("BCDB")
 	txmplog = elalog.Logger("TXMP")
 	synclog = elalog.Logger("SYNC")
@@ -33,17 +39,22 @@ var (
 	minrlog = elalog.Logger("MINR")
 	srvrlog = elalog.Logger("SRVR")
 	httplog = elalog.Logger("HTTP")
+	rpcslog = elalog.Logger("RPCS")
+	restlog = elalog.Logger("REST")
 	eladlog = elalog.Logger("ELAD")
 )
 
 // The default amount of logging is none.
 func init() {
+	addrmgr.UseLogger(admrlog)
+	connmgr.UseLogger(cmgrlog)
 	blockchain.UseLogger(bcdblog)
 	mempool.UseLogger(txmplog)
 	netsync.UseLogger(synclog)
 	peer.UseLogger(peerlog)
+	p2pserver.UseLogger(srvrlog)
 	pow.UseLogger(minrlog)
 	servers.UseLogger(httplog)
-	httpjsonrpc.UseLogger(httplog)
-	httprestful.UseLogger(httplog)
+	httpjsonrpc.UseLogger(rpcslog)
+	httprestful.UseLogger(restlog)
 }
