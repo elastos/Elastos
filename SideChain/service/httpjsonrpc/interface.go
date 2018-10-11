293c70cadc9914723c30389e4ffa7246dc0c83bd
AlexPan <ppgod@live.cn>
2018-10-11 10:11:04 +0800

    rename and code formatting

--- SideChain/service/httpjsonrpc/interface.go
+++ SideChain/service/httpjsonrpc/interface.go
@@ -3,7 +3,7 @@ package httpjsonrpc
 import "github.com/elastos/Elastos.ELA.SideChain/service"
 
 type Server interface {
-	RegisterAction(name string, action service.Handler, params... string)
+	RegisterAction(name string, action service.Handler, params ...string)
 	Start() error
 	Stop() error
 }
