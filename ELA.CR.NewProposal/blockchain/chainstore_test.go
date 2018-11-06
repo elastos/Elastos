d4dcc09bc71a99f463cf53d5f430b2621e629668
BochengZhang <bocheng0000@gmail.com>
2018-11-06 14:03:33 +0800

    Modify IDx to Idx and keep the interface is the same as before

--- ELA.CR.NewProposal/blockchain/chainstore_test.go
+++ ELA.CR.NewProposal/blockchain/chainstore_test.go
@@ -23,7 +23,7 @@ func newTestChainStore() (*ChainStore, error) {
 		IStore:             st,
 		headerIndex:        map[uint32]common.Uint256{},
 		headerCache:        map[common.Uint256]*ela.Header{},
-		headerIDx:          list.New(),
+		headerIdx:          list.New(),
 		currentBlockHeight: 0,
 		storedHeaderCount:  0,
 		taskCh:             make(chan persistTask, TaskChanCap),
