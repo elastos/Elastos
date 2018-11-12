098f2d6cb15d5f002b9543f80293e7905c6e635a
AlexPan <ppgod@live.cn>
2018-11-12 11:09:49 +0800

    remove duplicated method compactToBig() and useless file main.go log.go

--- SideChain.Release/blockchain/blockchain.go
+++ SideChain.Release/blockchain/blockchain.go
@@ -386,37 +386,9 @@ func NewBlockNode(header *types.Header, hash *Uint256) *BlockNode {
 	return &node
 }
 
-func compactToBig(compact uint32) *big.Int {
-	// Extract the mantissa, sign bit, and exponent.
-	mantissa := compact & 0x007fffff
-	isNegative := compact&0x00800000 != 0
-	exponent := uint(compact >> 24)
-
-	// Since the base for the exponent is 256, the exponent can be treated
-	// as the number of bytes to represent the full 256-bit number.  So,
-	// treat the exponent as the number of bytes and shift the mantissa
-	// right or left accordingly.  This is equivalent to:
-	// N = mantissa * 256^(exponent-3)
-	var bn *big.Int
-	if exponent <= 3 {
-		mantissa >>= 8 * (3 - exponent)
-		bn = big.NewInt(int64(mantissa))
-	} else {
-		bn = big.NewInt(int64(mantissa))
-		bn.Lsh(bn, 8*(exponent-3))
-	}
-
-	// Make it negative if the sign bit is set.
-	if isNegative {
-		bn = bn.Neg(bn)
-	}
-
-	return bn
-}
-
 // (1 << 256) / (difficultyNum + 1)
 func CalcWork(bits uint32) *big.Int {
-	difficultyNum := compactToBig(bits)
+	difficultyNum := CompactToBig(bits)
 	if difficultyNum.Sign() <= 0 {
 		return big.NewInt(0)
 	}
