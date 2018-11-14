6e39eb57d6290915bbb38652f688fa05763bf7f6
jacky.li <jacky.li@accenture.com>
2018-11-14 19:38:22 +0800

    update

---
+++ CR.Elastos-Unity-Carrier/ios/RNCarrier/RNCarrier/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RNCarrier
+//
+//  Created by jacky.li on 2018/11/14.
+//  Copyright © 2018 elastos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
