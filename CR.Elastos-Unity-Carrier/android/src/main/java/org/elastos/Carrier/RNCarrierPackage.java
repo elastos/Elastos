ff99a52088b18f5f8f8e6f8f298a40da691e4290
jacky.li <jacky.li@accenture.com>
2018-11-16 10:19:21 +0800

    update

--- CR.Elastos-Unity-Carrier/android/src/main/java/org/elastos/Carrier/RNCarrierPackage.java
+++ CR.Elastos-Unity-Carrier/android/src/main/java/org/elastos/Carrier/RNCarrierPackage.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.List;
 
 
-public class CarrierPlugin implements ReactPackage {
+public class RNCarrierPackage implements ReactPackage {
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
