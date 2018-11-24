b323571eae558336b348e57ad27a72a36fd338f6
Yan Minzhi <yanmingzhi@elastos.org>
2018-11-24 10:38:40 +0800

    Get producers by height version.

--- ELA.CR.NewProposal/blockchain/blockhistory/blockversionv0.go
+++ ELA.CR.NewProposal/blockchain/blockhistory/blockversionv0.go
@@ -1,6 +1,13 @@
 package blockhistory
 
-import "github.com/elastos/Elastos.ELA/blockchain"
+import (
+	"errors"
+
+	"github.com/elastos/Elastos.ELA/blockchain"
+	"github.com/elastos/Elastos.ELA/config"
+
+	"github.com/elastos/Elastos.ELA.Utility/common"
+)
 
 type BlockVersionV0 struct {
 	blockchain.BlockVersionMain
@@ -8,4 +15,21 @@ type BlockVersionV0 struct {
 
 func (b *BlockVersionV0) GetVersion() uint32 {
 	return 0
+}
+
+func (b *BlockVersionV0) GetProducersDesc() ([][]byte, error) {
+	if len(config.Parameters.Arbiters) == 0 {
+		return nil, errors.New("arbiters not configured")
+	}
+
+	arbitersByte := make([][]byte, 0)
+	for _, arbiter := range config.Parameters.Arbiters {
+		arbiterByte, err := common.HexStringToBytes(arbiter)
+		if err != nil {
+			return nil, err
+		}
+		arbitersByte = append(arbitersByte, arbiterByte)
+	}
+
+	return arbitersByte, nil
 }
--- ELA.CR.NewProposal/blockchain/blockversion.go
+++ ELA.CR.NewProposal/blockchain/blockversion.go
@@ -1,14 +1,38 @@
 package blockchain
 
+import (
+	"errors"
+
+	"github.com/elastos/Elastos.ELA/config"
+
+	"github.com/elastos/Elastos.ELA.Utility/common"
+)
+
 type BlockVersion interface {
 	GetVersion() uint32
+	GetProducersDesc() ([][]byte, error)
 }
 
 type BlockVersionMain struct {
-
 }
 
 func (b *BlockVersionMain) GetVersion() uint32 {
 	return 1
 }
 
+func (b *BlockVersionMain) GetProducersDesc() ([][]byte, error) {
+	producersInfo := DefaultLedger.Store.GetRegisteredProducers()
+	if uint32(len(producersInfo)) < config.Parameters.ArbiterConfiguration.ArbitratorsCount {
+		return nil, errors.New("producers count less than min arbitrators count.")
+	}
+
+	result := make([][]byte, 0)
+	for i := uint32(0); i < uint32(len(producersInfo)); i++ {
+		arbiterByte, err := common.HexStringToBytes(producersInfo[i].PublicKey)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, arbiterByte)
+	}
+	return result, nil
+}
