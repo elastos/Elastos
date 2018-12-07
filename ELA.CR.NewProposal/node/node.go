75542018a8d7076cf4d5787c5936b02e9f3da9ca
hejianhui <hejianhui@elastos.org>
2018-12-07 10:45:30 +0800

    set last active time for ping message as well

--- ELA.CR.NewProposal/node/node.go
+++ ELA.CR.NewProposal/node/node.go
@@ -182,7 +182,7 @@ func (node *node) nodeHeartBeat() {
 		log.Info("node heart beat")
 		for _, peer := range node.GetNeighborNodes() {
 			if time.Now().Sub(peer.GetLastActive()) > 10 * time.Minute {
-				log.Warn("does not get pong message for 10 minutes.")
+				log.Warn("does not update last active time for 10 minutes.")
 				peer.Disconnect()
 			}
 		}
