c0c4be1725aaa8edb699a9a8c4cc766bab3bddcb
tempzc <tempzc@126.com>
2018-12-13 12:09:39 +0800

    change to refer to ks_sample.js and ctrt_sample.js

--- SideChain.ETH/oracle/common.js
+++ SideChain.ETH/oracle/common.js
@@ -2,9 +2,9 @@
 
 const Web3 = require("web3");
 const web3 = new Web3("http://127.0.0.1:6666");
-const ks = require("./ks");
+const ks = require("./ks_sample");
 const acc = web3.eth.accounts.decrypt(ks.kstore, ks.kpass);
-const ctrt = require("./ctrt");
+const ctrt = require("./ctrt_sample");
 const contract = new web3.eth.Contract(ctrt.abi);
 contract.options.address = ctrt.address;
 const payloadReceived = {name: null, inputs: null, signature: null};
