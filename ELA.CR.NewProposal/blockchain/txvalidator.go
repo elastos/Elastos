c01eefa0f685b53330f3d731dc7b7f9437cb2f01
jiangzehua <jiangzehua@elastos.org>
2018-12-18 11:07:58 +0800

    change default arbitrators to main chain arbitrators

--- ELA.CR.NewProposal/blockchain/txvalidator.go
+++ ELA.CR.NewProposal/blockchain/txvalidator.go
@@ -484,8 +484,16 @@ func CheckAttributeProgram(blockHeight uint32, tx *Transaction) error {
 				return fmt.Errorf("invalid standard program code %x", program.Code)
 			}
 		case contract.MultiSig:
-			// TODO: implement the multi signature code checking
-			return fmt.Errorf("invalid multi signature program code, %x", program.Code)
+			publicKeys, err := ParseMultisigScript(program.Code)
+			if err != nil {
+				return fmt.Errorf("invalid multi signature program code, %x", program.Code)
+			}
+			for _, pk := range publicKeys {
+				_, err := contract.PublicKeyToStandardProgramHash(pk[1:])
+				if err != nil {
+					return fmt.Errorf("invalid  multi signature public key %x", pk)
+				}
+			}
 		default:
 			return fmt.Errorf("invalid program code type, %x", program.Code)
 		}
