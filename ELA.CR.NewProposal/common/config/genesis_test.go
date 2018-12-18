0d7b5ebda1fd062ad5d2c62841cd9ff6979bb285
AlexPan <ppgod@live.cn>
2018-12-18 17:54:07 +0800

    move configuration file reading into main package

--- ELA.CR.NewProposal/common/config/genesis_test.go
+++ ELA.CR.NewProposal/common/config/genesis_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TestGenesisBlock(t *testing.T) {
-	block := genesisBlock(mainNetFoundation)
+	block := GenesisBlock(mainNetFoundation)
 	assert.Equal(t, len(block.Transactions), 2)
 
 	genesisHash := block.Hash().String()
 	assert.Equal(t, "8d7014f2f941caa1972c8033b2f0a860ec8d4938b12bae2c62512852a558f405", genesisHash)
 
-	genesisHash = genesisBlock(testNetFoundation).Hash().String()
+	genesisHash = GenesisBlock(testNetFoundation).Hash().String()
 	assert.Equal(t, "b3314f465ea5556d570bcc473d59a0855b4405a25b1ea0c957c81b2920be1864", genesisHash)
 }
