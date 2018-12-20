elastos/Elastos.ELA.SPV.Cpp

Fix incorrect txn record return by SubWallet::GetAllTransaction().

SDK/Transaction/Transaction.h
@@ -175,7 +175,7 @@ namespace Elastos {
 
 			void setRemark(const std::string &remark);
 
-			void GetSummary(nlohmann::json &rawTxJson, const boost::shared_ptr<Wallet> &wallet, uint32_t blockHeight);
+			nlohmann::json GetSummary(const boost::shared_ptr<Wallet> &wallet, uint32_t confirms, bool detail);
 
 			void removeDuplicatePrograms();
 
@@ -190,10 +190,6 @@ namespace Elastos {
 			IPayload *newPayload(ELATransaction::Type type);
 
 			bool transactionSign(int forkId, const WrapperList<Key, BRKey> keys);
-
-			std::string getConfirmInfo(uint32_t blockHeight);
-
-			std::string getStatus(uint32_t blockHeight);
 
 		private:
 			bool _isRegistered;

elastos/Elastos

SPV.CPP/SDK/Plugin/Transaction/Transaction.h
@@ -133,8 +133,7 @@ namespace Elastos {
 
 			void setRemark(const std::string &remark);
 
-			nlohmann::json GetSummary(const boost::shared_ptr<TransactionHub> &wallet,
-											  uint32_t lastBlockHeight);
+			nlohmann::json GetSummary(const boost::shared_ptr<TransactionHub> &wallet, uint32_t confirms, bool detail);
 
 			uint8_t	getPayloadVersion() const;
 
@@ -167,10 +166,6 @@ namespace Elastos {
 			void reinit();
 
 			bool transactionSign(const WrapperList<Key, BRKey> keys, const boost::shared_ptr<TransactionHub> &wallet);
-
-			std::string getConfirmInfo(uint32_t lastBlockHeight);
-
-			std::string getStatus(uint32_t lastBlockHeight);
 
 		private:
 			bool _isRegistered;