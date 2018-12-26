ba6f79a4cf8e1e54004d11b417c5f587d2388578
Andrew yz <deedecho@gmail.com>
2018-12-26 13:35:27 +0800

    fix script

--- ELA.CR.NewProposal/test/transaction/return_pledge_coin.lua
+++ ELA.CR.NewProposal/test/transaction/return_pledge_coin.lua
@@ -14,9 +14,10 @@ print(pubkey)
 local assetID = m.get_asset_id()
 
 -- amount, fee
-local amount = 0.2
+local amount = 0.199
 local fee = 0.001
 local recipient = "EJMzC16Eorq9CuFCGtyMrq4Jmgw9jYCHQR"
+local deposit_addr = "DVgnDnVfPVuPa2y2E4JitaWjWgRGJDuyrD"
 
 -- return deposit payload
 local rp_payload = returndepositcoin.new()
@@ -26,14 +27,14 @@ print(rp_payload:get())
 local tx = transaction.new(9, 0x0c, 0, rp_payload, 0)
 
 -- input: from, amount + fee
-local charge = tx:appendenough(addr, (amount + fee) * 100000000)
+local charge = tx:appendenough(deposit_addr, (amount + fee) * 100000000)
 print(charge)
 
 -- default output payload
 local default_output = defaultoutput.new()
 
 -- output: asset_id, value, recipient, output_paload_type, output_paload
-local amount_output = output.new(assetID, (amount * 100000000) * charge, recipient, 0, default_output)
+local amount_output = output.new(assetID, (amount * 100000000) + charge, recipient, 0, default_output)
 tx:appendtxout(amount_output)
 -- print(amount_output:get())
 
