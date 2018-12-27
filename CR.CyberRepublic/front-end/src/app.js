07cd7e3a0269f81e9475df302e3ebe2e44398f5e
Clarence Liu <clarence.hl.liu@gmail.com>
2018-12-26 16:51:10 -0800

    remove hotjar

--- CR.CyberRepublic/front-end/src/app.js
+++ CR.CyberRepublic/front-end/src/app.js
@@ -29,16 +29,6 @@ const App = () => {
                 {process.env.NODE_ENV === 'production' && <script async src={'https://www.googletagmanager.com/gtag/js?id=' + process.env.GA_ID}></script>}
                 {process.env.NODE_ENV === 'production' && <script>{`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '` + process.env.GA_ID + `');`}</script>}
                 {window.location.pathname === '/' && <script defer src="/assets/js/elastos.js"></script>}
-                {false && process.env.NODE_ENV === 'production' && <script>{`
-                    (function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:1076743,hjsv:6};
-                    a=o.getElementsByTagName('head')[0];
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`}
-                </script>}
                 {/*
                 <script>{
                     (function() {
