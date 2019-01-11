77ff8f4eaf30d38da3ca4593158d54d511822cf7
Harry Liu <2411mail@gmail.com>
2019-01-11 16:47:46 +0800

    added suggestion list module

---
+++ CR.CyberRepublic/front-end/src/module/page/suggestion/list/Container.js
@@ -0,0 +1,66 @@
+import {
+  createContainer,
+} from '@/util'
+import {
+  SUGGESTION_STATUS,
+} from '@/constant'
+import SuggestionService from '@/service/SuggestionService'
+import CommentService from '@/service/CommentService'
+import Component from './Component'
+
+const mapState = (state) => {
+  const currentUserId = state.user.current_user_id
+  // const isAdmin = state.user.role === USER_ROLE.ADMIN
+
+  const suggestionState = {
+    ...state.suggestion,
+    dataList: state.suggestion.all_suggestions,
+    total: state.suggestion.all_suggestions_total,
+    currentUserId,
+    filter: state.suggestion.filter || {},
+  }
+
+  return suggestionState
+}
+
+const mapDispatch = () => {
+  const service = new SuggestionService()
+  const commentService = new CommentService()
+
+  return {
+    async getList(query) {
+      return service.list({
+        status: SUGGESTION_STATUS.ACTIVE,
+        ...query,
+      })
+    },
+
+    async loadMore(query) {
+      return service.loadMore({
+        status: SUGGESTION_STATUS.ACTIVE,
+        ...query,
+      })
+    },
+
+    resetAll() {
+      return service.resetAll()
+    },
+
+    async create(doc) {
+      return service.create(doc)
+    },
+
+    async reportAbuse(id) {
+      return service.reportAbuse(id)
+    },
+    async subscribe(id) {
+      return commentService.subscribe('suggestion', id)
+    },
+
+    async unsubscribe(id) {
+      return commentService.unsubscribe('suggestion', id)
+    },
+  }
+}
+
+export default createContainer(Component, mapState, mapDispatch)
