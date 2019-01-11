03d2e9d72ddfef2a1e15dc7ece4f30528a949dac
Harry Liu <2411mail@gmail.com>
2019-01-11 17:30:09 +0800

    autofix eslint errors

--- CR.CyberRepublic/front-end/src/module/page/CVote/create/Container.js
+++ CR.CyberRepublic/front-end/src/module/page/CVote/create/Container.js
@@ -1,6 +1,4 @@
-import {createContainer} from '@/util'
+import { createContainer } from '@/util'
 import Component from './Component'
 
-export default createContainer(Component, () => {
-    return {}
-})
+export default createContainer(Component, () => ({}))
--- CR.CyberRepublic/front-end/src/module/page/CVote/list/Container.js
+++ CR.CyberRepublic/front-end/src/module/page/CVote/list/Container.js
@@ -1,46 +1,39 @@
 import { createContainer, api_request } from '@/util'
 import Component from './Component'
 
-export default createContainer(Component, (state) => {
+export default createContainer(Component, state => ({
+  currentUserId: state.user.current_user_id,
+  isLogin: state.user.is_login,
 
-    return {
-        currentUserId: state.user.current_user_id,
-        isLogin : state.user.is_login,
+  // TODO: this should be dynamic
+  isCouncil: [
 
-        // TODO: this should be dynamic
-        isCouncil: [
+    '5b28be2784f6f900350d30b9',
+    '5b367c128f23a70035d35425',
+    '5bcf21f030826d68a940b017',
+    '5b4c3ba6450ff10035954c80',
 
-            '5b28be2784f6f900350d30b9',
-            '5b367c128f23a70035d35425',
-            '5bcf21f030826d68a940b017',
-            '5b4c3ba6450ff10035954c80'
+  ].indexOf(state.user.current_user_id) >= 0,
+  language: state.language.language,
+}), () => ({
 
-        ].indexOf(state.user.current_user_id) >= 0,
-        language: state.language.language
+  async listData(param, isCouncil) {
+    let result
+
+    if (isCouncil) {
+      result = await api_request({
+        path: '/api/cvote/list',
+        method: 'get',
+        data: param,
+      });
+    } else {
+      result = await api_request({
+        path: '/api/cvote/list_public',
+        method: 'get',
+        data: param,
+      });
     }
-}, ()=>{
-    return {
 
-        async listData(param, isCouncil){
-
-            let result
-
-            if (isCouncil) {
-                result = await api_request({
-                    path: '/api/cvote/list',
-                    method: 'get',
-                    data: param
-                });
-
-            } else {
-                result = await api_request({
-                    path: '/api/cvote/list_public',
-                    method: 'get',
-                    data: param
-                });
-            }
-
-            return result;
-        }
-    }
-})
+    return result;
+  },
+}))
