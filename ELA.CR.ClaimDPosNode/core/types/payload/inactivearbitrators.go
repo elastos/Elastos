0df44960abb1aaf88ec8edb683e7cc125c727429
Yan Mingzhi <yanmingzhi@elastos.org>
2019-01-17 17:59:28 +0800

    add inactive arbitrators tx and related checks

---
+++ ELA.CR.ClaimDPosNode/core/types/payload/inactivearbitrators.go
@@ -0,0 +1,99 @@
+package payload
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/elastos/Elastos.ELA/common"
+	"github.com/elastos/Elastos.ELA/crypto"
+)
+
+const PayloadInactiveArbitratorsVersion byte = 0x00
+
+type InactiveArbitrators struct {
+	Sponsor     []byte
+	Arbitrators [][]byte
+	Sign        []byte
+
+	hash *common.Uint256
+}
+
+func (i *InactiveArbitrators) Data(version byte) []byte {
+	buf := new(bytes.Buffer)
+	if err := i.Serialize(buf, version); err != nil {
+		return []byte{0}
+	}
+	return buf.Bytes()
+}
+
+func (i *InactiveArbitrators) Serialize(w io.Writer, version byte) error {
+	if err := i.SerializeUnsigned(w, version); err != nil {
+		return err
+	}
+
+	if err := common.WriteVarBytes(w, i.Sign); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (i *InactiveArbitrators) SerializeUnsigned(w io.Writer, version byte) error {
+	if err := common.WriteVarBytes(w, i.Sponsor); err != nil {
+		return err
+	}
+
+	if err := common.WriteVarUint(w, uint64(len(i.Arbitrators))); err != nil {
+		return err
+	}
+
+	for _, v := range i.Arbitrators {
+		if err := common.WriteVarBytes(w, v); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (i *InactiveArbitrators) Deserialize(r io.Reader, version byte) (err error) {
+	if err = i.DeserializeUnsigned(r, version); err != nil {
+		return err
+	}
+
+	if i.Sign, err = common.ReadVarBytes(r, crypto.SignatureLength, "sign data"); err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (i *InactiveArbitrators) DeserializeUnsigned(r io.Reader, version byte) (err error) {
+	if i.Sponsor, err = common.ReadVarBytes(r, crypto.NegativeBigLength, "public key"); err != nil {
+		return err
+	}
+
+	var count uint64
+	if count, err = common.ReadVarUint(r, 0); err != nil {
+		return err
+	}
+
+	i.Arbitrators = make([][]byte, count)
+	for u := uint64(0); u < count; u++ {
+		if i.Arbitrators[u], err = common.ReadVarBytes(r, crypto.NegativeBigLength, "public key"); err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func (i *InactiveArbitrators) Hash() common.Uint256 {
+	if i.hash == nil {
+		buf := new(bytes.Buffer)
+		i.SerializeUnsigned(buf, PayloadInactiveArbitratorsVersion)
+		hash := common.Uint256(common.Sha256D(buf.Bytes()))
+		i.hash = &hash
+	}
+	return *i.hash
+}
