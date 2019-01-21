8cbcb255e13f36f7be2136c3876d90f8d213edbe
Yan Mingzhi <yanmingzhi@elastos.org>
2019-01-21 15:27:56 +0800

    format code imports

--- ELA.CR.ClaimDPosNode/dpos/manager/evidencecache.go
+++ ELA.CR.ClaimDPosNode/dpos/manager/evidencecache.go
@@ -1,10 +1,9 @@
 package manager
 
 import (
+	"github.com/elastos/Elastos.ELA/common"
 	"github.com/elastos/Elastos.ELA/core/types"
 	"github.com/elastos/Elastos.ELA/core/types/payload"
-
-	"github.com/elastos/Elastos.ELA/common"
 )
 
 type evidenceCache struct {
