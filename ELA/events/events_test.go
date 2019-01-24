85b9172d337a7013ffb2c8457d96b3ad320aff2a
AlexPan <ppgod@live.cn>
2019-01-24 17:29:46 +0800

    implement concurrent recursive call detect for events.go

---
+++ ELA/events/events_test.go
@@ -0,0 +1,48 @@
+package events
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotify(t *testing.T) {
+	notifyChan := make(chan struct{})
+	Subscribe(func(event *Event) {
+		notifyChan <- struct{}{}
+	})
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			Notify(ETBlockAccepted, nil)
+		}()
+	}
+
+	for i := 0; i < 100; i++ {
+		select {
+		case <-notifyChan:
+		case <-time.After(time.Millisecond):
+			t.Error("notify timeout")
+		}
+	}
+}
+
+func TestRecursiveNotify(t *testing.T) {
+	Subscribe(func(event *Event) {
+		Notify(ETBlockConnected, nil)
+	})
+
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				if !assert.Equal(t, err, "recursive notifies detected") {
+					t.FailNow()
+				}
+			}
+		}()
+		Notify(ETBlockAccepted, nil)
+	}()
+
+	<-time.After(time.Millisecond)
+}
