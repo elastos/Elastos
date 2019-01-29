be2b9af4c7b95a846b6f10a40c4f4a1a189466a3
jiangzehua <jiangzehua@elastos.org>
2019-01-29 16:01:51 +0800

    change peer last active timeout from 10 minutes to 1 minute

--- ELA.CR.ClaimDPosNode/node/node.go
+++ ELA.CR.ClaimDPosNode/node/node.go
@@ -185,7 +185,7 @@ func (node *node) nodeHeartBeat() {
 	for {
 		log.Info("node heart beat")
 		for _, peer := range node.GetNeighborNodes() {
-			if time.Now().Sub(peer.GetLastActive()) > 10*time.Minute {
+			if time.Now().Sub(peer.GetLastActive()) > time.Minute {
 				log.Warn("does not update last active time for 10 minutes.")
 				peer.Disconnect()
 			}
