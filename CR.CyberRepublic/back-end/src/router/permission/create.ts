d91a25d3224b27b282aa21657bfc979091ae3545
Harry Liu <harryliu@cyberrepublic.org>
2019-01-31 21:21:22 +0800

    remove unused code

--- CR.CyberRepublic/back-end/src/router/permission/create.ts
+++ CR.CyberRepublic/back-end/src/router/permission/create.ts
@@ -6,7 +6,7 @@ export default class extends Base {
   public async action() {
     const service = this.buildService(Service)
     const rs = await service.create(this.getParam())
-    console.log('create,,,....')
+
     return this.result(1, rs)
   }
 }
