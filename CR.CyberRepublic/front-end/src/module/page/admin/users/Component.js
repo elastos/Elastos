83cae92c7f2982aacc7e26c64488ff1aea7b0099
Harry Liu <harryliu@cyberrepublic.org>
2019-02-10 12:50:59 +0800

    fixed eslint

--- CR.CyberRepublic/front-end/src/module/page/admin/users/Component.js
+++ CR.CyberRepublic/front-end/src/module/page/admin/users/Component.js
@@ -1,65 +1,68 @@
 import React from 'react'
+import {
+  Col, Row, Icon, Input,
+} from 'antd'
 import AdminPage from '../BaseAdmin'
 
 import '../admin.scss'
 import './style.scss'
 
-import { Col, Row, Breadcrumb, Icon, Input } from 'antd'
 import ListUsers from './ListUsers/Component'
 import Navigator from '../../shared/HomeNavigator/Container'
 
 export default class extends AdminPage {
     state = {
-        usernameFilter: ''
+      usernameFilter: '',
     }
 
     async componentDidMount() {
-        await super.componentDidMount()
-        this.props.listUsers()
+      await super.componentDidMount()
+      this.props.listUsers()
     }
 
     handleSearchUser(value) {
-        this.setState({usernameFilter: value})
+      this.setState({ usernameFilter: value })
     }
 
-    ord_renderContent () {
+    ord_renderContent() {
+      let { users } = this.props
 
-        let users = this.props.users
+      if (this.state.usernameFilter) {
+        users = users.filter((user) => {
+          const regExp = new RegExp(this.state.usernameFilter, 'i')
+          return (
+            regExp.test(user.username)
+                    || regExp.test(user.profile.firstName)
+                    || regExp.test(user.profile.lastName)
+                    || regExp.test(user.email)
+          )
+        })
+      }
 
-        if (this.state.usernameFilter) {
-            users = users.filter((user) => {
-                let regExp = new RegExp(this.state.usernameFilter, 'i')
-                return (
-                    regExp.test(user.username) ||
-                    regExp.test(user.profile.firstName) ||
-                    regExp.test(user.profile.lastName) ||
-                    regExp.test(user.email)
-                )
-            })
-        }
-
-        return (
-            <div className="p_admin_index ebp-wrap">
-                <div className="ebp-header-divider" />
-                <div className="d_box">
-                    <div className="p_admin_content">
-                        <Row>
-                            <Col span={4} className="wrap-box-navigator">
-                                <Navigator selectedItem={'users'}/>
-                            </Col>
-                            <Col span={20} className="admin-right-column wrap-box-user">
-                                <div class="pull-right">
-                                    <Input.Search onSearch={this.handleSearchUser.bind(this)}
-                                                  prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                                                  placeholder="search user"/>
-                                </div>
-                                <div class="vert-gap-sm clearfix"/>
-                                <ListUsers users={users} history={this.props.history} loading={this.props.loading}/>
-                            </Col>
-                        </Row>
-                    </div>
-                </div>
+      return (
+        <div className="p_admin_index ebp-wrap">
+          <div className="ebp-header-divider" />
+          <div className="d_box">
+            <div className="p_admin_content">
+              <Row>
+                <Col span={4} className="wrap-box-navigator">
+                  <Navigator selectedItem="users" />
+                </Col>
+                <Col span={20} className="admin-right-column wrap-box-user">
+                  <div className="pull-right">
+                    <Input.Search
+                      onSearch={this.handleSearchUser.bind(this)}
+                      prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      placeholder="search user"
+                    />
+                  </div>
+                  <div className="vert-gap-sm clearfix" />
+                  <ListUsers users={users} history={this.props.history} loading={this.props.loading} />
+                </Col>
+              </Row>
             </div>
-        )
+          </div>
+        </div>
+      )
     }
 }
--- CR.CyberRepublic/front-end/src/module/page/admin/users/ListUsers/Component.js
+++ CR.CyberRepublic/front-end/src/module/page/admin/users/ListUsers/Component.js
@@ -1,77 +1,63 @@
 import React from 'react'
+import _ from 'lodash'
 import BaseComponent from '@/model/BaseComponent'
-import { Table, Icon } from 'antd'
+import { Table } from 'antd'
 import I18N from '@/I18N'
 import config from '@/config'
 
 import './style.scss'
-import moment from 'moment/moment'
 
 export default class extends BaseComponent {
+  ord_render() {
+    const columns = [{
+      title: I18N.get('profile.detail.avatar'),
+      dataIndex: 'profile.avatar',
+      render: avatar => (avatar ? (
+        <img src={avatar} style={{ height: 30, width: 30 }} alt="avatar" />
+      ) : 'None'),
+    }, {
+      title: I18N.get('1201'),
+      dataIndex: 'username',
+      render: (username, record) => <a onClick={this.linkProfileInfo.bind(this, record._id)} className="tableLink">{username}</a>,
+      sorter: (a, b) => a.username.localeCompare(b.username),
+      defaultSortOrder: 'ascend',
+    }, {
+      title: I18N.get('1208'),
+      dataIndex: '_id',
+      render: (id, item) => `${item.profile.firstName} ${item.profile.lastName}`,
+    }, {
+      title: I18N.get('1202'),
+      dataIndex: 'email',
+    }, {
+      title: I18N.get('1209'),
+      dataIndex: 'profile.country',
+      render: geolocation => config.data.mappingCountryCodeToName[geolocation],
+      sorter: (a, b) => config.data.mappingCountryCodeToName[a.profile.country].localeCompare(config.data.mappingCountryCodeToName[b.profile.country]),
+    }, {
+      title: I18N.get('1204'),
+      dataIndex: 'role',
+      render: (role) => {
+        if (role === 'LEADER') {
+          return _.capitalize('ORGANIZER')
+        }
 
-    ord_render () {
-        const columns = [{
-            title: I18N.get('profile.detail.avatar'),
-            dataIndex: 'profile.avatar',
-            render: (avatar, item) => {
-                return avatar ? <img src={avatar} style={{ height: 30,
-                    width: 30}} /> : 'None'
-            }
-        }, {
-            title: I18N.get('1201'),
-            dataIndex: 'username',
-            render: (username, record) => {
-                return <a onClick={this.linkProfileInfo.bind(this, record._id)} className="tableLink">{username}</a>
-            },
-            sorter: (a, b) => {
-                return a.username.localeCompare(b.username)
-            },
-            defaultSortOrder: 'ascend'
-        }, {
-            title: I18N.get('1208'),
-            dataIndex: '_id',
-            render: (id, item) => {
-                return item.profile.firstName + ' ' + item.profile.lastName
-            }
+        return _.capitalize(role)
+      },
+    }]
 
-        }, {
-            title: I18N.get('1202'),
-            dataIndex: 'email',
-        }, {
-            title: I18N.get('1209'),
-            dataIndex: 'profile.country',
-            render: (geolocation, item) => {
-                return config.data.mappingCountryCodeToName[geolocation]
-            },
-            sorter: (a, b) => {
-                return config.data.mappingCountryCodeToName[a.profile.country].localeCompare(config.data.mappingCountryCodeToName[b.profile.country])
-            }
-        }, {
-            title: I18N.get('1204'),
-            dataIndex: 'role',
-            render: (role) => {
-                if (role === 'LEADER') {
-                    role = 'ORGANIZER'
-                }
+    const data = this.props.users
 
-                return _.capitalize(role)
-            }
-        }]
+    return (
+      <Table
+        columns={columns}
+        dataSource={data}
+        rowKey={record => record.username}
+        loading={this.props.loading}
+      />
+    )
+  }
 
-        const data = this.props.users
-
-        return (
-            <Table
-                columns={columns}
-                dataSource={data}
-                rowKey={record => record.username}
-                loading={this.props.loading}
-            />
-        )
-    }
-
-    linkProfileInfo(userId) {
-        this.props.history.push(`/admin/profile/${userId}`)
-    }
-
+  linkProfileInfo(userId) {
+    this.props.history.push(`/admin/profile/${userId}`)
+  }
 }
