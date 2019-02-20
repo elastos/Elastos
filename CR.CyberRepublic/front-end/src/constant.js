c06a87227383e314c587921359a5dae1da675e28
Harry Liu <harryliu@cyberrepublic.org>
2019-02-20 18:38:36 +0800

    make council role dynamic

--- CR.CyberRepublic/front-end/src/constant.js
+++ CR.CyberRepublic/front-end/src/constant.js
@@ -41,26 +41,6 @@ export const USER_ROLE_TO_TEXT = {
     LEADER : 'Leader',
 }
 
-// council related
-// '5c2f5a15f13d65008969be61', // Feng Zhang
-// '5b28be2784f6f900350d30b9', // Kevin Zhang
-// '5bcf21f030826d68a940b017', //  Yipeng Su
-// '5b4c3ba6450ff10035954c80', // Feng zhu
-// '5b367c128f23a70035d35425', // Fay Li
-export const COUNCIL_MEMBERS = {
-    '5bcf21f030826d68a940b017': 'Yipeng Su',
-    // '5c2f5a15f13d65008969be61': 'Feng Zhang',
-    '5b367c128f23a70035d35425': 'Fay Li',
-    '5b28be2784f6f900350d30b9': 'Kevin Zhang',
-}
-
-export const COUNCIL_MEMBER_IDS = _.keys(COUNCIL_MEMBERS)
-
-export const ADMIN_MEMBER_IDS = [
-    ...COUNCIL_MEMBER_IDS,
-    '5b4c3ba6450ff10035954c80'
-]
-
 export const USER_EMPOWER_TYPE = {
     MARKETING: 'MARKETING',
     PRODUCT_MANAGER: 'PRODUCT_MANAGER',
