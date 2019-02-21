c44bc1f07551b130e69ae6ae9cb458da99898dbc
Harry Liu <harryliu@cyberrepublic.org>
2019-02-22 00:47:18 +0800

    do not show the reason for now

--- CR.CyberRepublic/front-end/src/module/page/CVote/common/vote_result/Component.js
+++ CR.CyberRepublic/front-end/src/module/page/CVote/common/vote_result/Component.js
@@ -1,32 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
-import { Row, Col } from 'antd'
 
 import './style.scss'
 
 const Component = ({ label, type, dataList }) => {
   const votesNode = _.map(dataList, (data, key) => {
-    const span = data.reason ? 24 : 6
-    const innerSpan = data.reason ? 6 : 24
     return (
-      <Col className="voter-item" key={key} span={span}>
-        <Row>
-          <Col span={innerSpan}>
-            <img src={data.avatar} alt="voter avatar" />
-            <div>{data.name}</div>
-          </Col>
-          <Col span={24 - innerSpan}>
-            <div className="reason">{data.reason}</div>
-          </Col>
-        </Row>
-      </Col>
+      <div className="voter-item" key={key}>
+        <img src={data.avatar} alt="voter avatar" />
+        <div>{data.name}</div>
+      </div>
     )
   })
   return (
     <div className="c_VoteResult">
       <div className="vote-label">{label}</div>
-      <Row className={`voter-list ${type}`}>{votesNode}</Row>
+      <div className={`voter-list ${type}`}>{votesNode}</div>
     </div>
   )
 }
