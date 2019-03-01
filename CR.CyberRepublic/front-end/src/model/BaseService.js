c1504282d5c88578acfd6510845f3caf4457fe12
Harry Liu <harryliu@cyberrepublic.org>
2019-03-01 15:33:44 +0800

    eslint fix

--- CR.CyberRepublic/front-end/src/model/BaseService.js
+++ CR.CyberRepublic/front-end/src/model/BaseService.js
@@ -2,13 +2,13 @@
 import store from '@/store';
 
 export default class {
-    constructor() {
-        this.store = store
-        this.path = store.history
-        this.abortControllers = []
+  constructor() {
+    this.store = store
+    this.path = store.history
+    this.abortControllers = []
 
-        this.init()
-    }
+    this.init()
+  }
 
     init(){
         let isLogout = !localStorage.getItem('api-token') && sessionStorage.getItem('api-token')
@@ -17,21 +17,21 @@ export default class {
         }
     }
 
-    dispatch(action){
-        return this.store.dispatch(action)
-    }
+  dispatch(action) {
+    return this.store.dispatch(action)
+  }
 
-    getAbortSignal(path) {
-        this.abortControllers[path] = this.abortControllers[path] || new AbortController()
-        return this.abortControllers[path].signal
-    }
+  getAbortSignal(path) {
+    this.abortControllers[path] = this.abortControllers[path] || new AbortController()
+    return this.abortControllers[path].signal
+  }
 
-    abortFetch(path) {
-        const controller = this.abortControllers[path]
+  abortFetch(path) {
+    const controller = this.abortControllers[path]
 
-        if (controller) {
-            controller.abort()
-            delete this.abortControllers[path]
-        }
+    if (controller) {
+      controller.abort()
+      delete this.abortControllers[path]
     }
-};
+  }
+}
