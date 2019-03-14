49089c300ac70d8115bd8cb089028e74e163c093
AlexPan <ppgod@live.cn>
2019-03-14 21:10:16 +0800

    Add Signature for Mapping output payload

--- ELA.CR.ClaimDPosNode/core/types/outputpayload/mapping_test.go
+++ ELA.CR.ClaimDPosNode/core/types/outputpayload/mapping_test.go
@@ -5,22 +5,30 @@ import (
 	"crypto/rand"
 	"testing"
 
+	"github.com/elastos/Elastos.ELA/crypto"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMapping_Serialize(t *testing.T) {
 	m := Mapping{}
-	m.OwnerPublicKey = make([]byte, 33)
+	priKey, pubKey, err := crypto.GenerateKeyPair()
+	m.OwnerPublicKey, _ = pubKey.EncodePoint(true)
 	m.SideProducerID = make([]byte, 33)
+	m.Signature, _ = crypto.Sign(priKey, m.Data())
+
 	buf := new(bytes.Buffer)
-	err := m.Serialize(buf)
+	err = m.Serialize(buf)
 	assert.NoError(t, err)
 
 	// 1 byte(Version)
 	// 1 byte(len) + 33 bytes(OwnerPublicKey)
 	// 1 byte(len) + 33 bytes(SideProducerID)
-	// = 69 bytes
-	assert.Equal(t, 69, buf.Len())
+	// 1 byte(len) + 64 bytes(Signature)
+	// = 134 bytes
+	assert.Equal(t, 134, buf.Len())
+
+	err = m.Validate()
+	assert.NoError(t, err)
 }
 
 func TestMapping_Deserialize(t *testing.T) {
@@ -46,10 +54,11 @@ func TestMapping_Deserialize(t *testing.T) {
 	// Equal test.
 	m1, m2 := Mapping{}, Mapping{}
 	m1.Version = 2
-	m1.OwnerPublicKey = make([]byte, 33)
+	priKey, pubKey, err := crypto.GenerateKeyPair()
+	m1.OwnerPublicKey, _ = pubKey.EncodePoint(true)
 	m1.SideProducerID = make([]byte, 33)
-	rand.Read(m1.OwnerPublicKey)
 	rand.Read(m1.SideProducerID)
+	m1.Signature, _ = crypto.Sign(priKey, m1.Data())
 
 	buf = new(bytes.Buffer)
 	err = m1.Serialize(buf)
@@ -59,6 +68,9 @@ func TestMapping_Deserialize(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, m1, m2)
+
+	err = m2.Validate()
+	assert.NoError(t, err)
 }
 
 func TestMapping_Validate(t *testing.T) {
