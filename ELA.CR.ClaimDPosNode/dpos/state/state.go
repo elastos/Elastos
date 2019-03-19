53bf462a69aac82c7fee74542929cee92f8a3c3b
Yan Mingzhi <yanmingzhi@elastos.org>
2019-03-19 18:52:52 +0800

    fix dead lock when return deposit

--- ELA.CR.ClaimDPosNode/dpos/state/state.go
+++ ELA.CR.ClaimDPosNode/dpos/state/state.go
@@ -670,7 +670,7 @@ func (s *State) returnDeposit(tx *types.Transaction, height uint32) {
 
 	for _, program := range tx.Programs {
 		pk := program.Code[1 : len(program.Code)-1]
-		if producer := s.GetProducer(pk); producer != nil && producer.state == Canceled {
+		if producer := s.getProducer(pk); producer != nil && producer.state == Canceled {
 			returnAction(producer)
 		}
 	}
