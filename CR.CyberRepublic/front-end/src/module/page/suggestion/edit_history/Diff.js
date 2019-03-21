93561353a1ebe06f86305ef91423e64832d8615d
Harry Liu <harryliu@cyberrepublic.org>
2019-03-21 14:56:49 +0800

    convert to markdown to diff then convert back to html to display the result

--- CR.CyberRepublic/front-end/src/module/page/suggestion/edit_history/Diff.js
+++ CR.CyberRepublic/front-end/src/module/page/suggestion/edit_history/Diff.js
@@ -2,6 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 
 const jsdiff = require('diff')
+const showdown = require('showdown')
+const TurndownService = require('turndown/lib/turndown.browser.umd.js')
+
+const converter = new showdown.Converter()
+// text      = '# hello, markdown!',
+// html      = converter.makeHtml(text);
+const turndownService = new TurndownService()
+// const markdown = turndownService.turndown('<h1>Hello world!</h1>')
 
 const fnMap = {
   chars: jsdiff.diffChars,
@@ -11,14 +19,14 @@ const fnMap = {
 };
 
 const StyledSpan = styled.span`
-  p {
+  > * {
     padding-bottom: 4px;
     background-color: ${props => props.highlight && 'rgba(29, 233, 182, 0.1)'};
   }
 `
 
 export default ({ type, inputA, inputB }) => {
-  const diff = fnMap[type || 'chars'](inputA, inputB);
+  const diff = fnMap[type || 'chars'](turndownService.turndown(inputA), turndownService.turndown(inputB));
   const result = diff.map((part, index) => {
     const spanStyle = {}
     if (part.added || part.removed) {
@@ -29,7 +37,7 @@ export default ({ type, inputA, inputB }) => {
       spanStyle.textDecoration = 'line-through'
     }
 
-    return <StyledSpan key={index} highlight={part.added || part.removed} style={spanStyle} dangerouslySetInnerHTML={{ __html: part.value }} />
+    return <StyledSpan key={index} highlight={part.added || part.removed} style={spanStyle} dangerouslySetInnerHTML={{ __html: converter.makeHtml(part.value) }} />
     // return <span key={index} style={spanStyle}>{part.value}</span>
   });
   return (
