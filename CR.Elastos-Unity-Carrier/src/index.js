10c2d1b273edbfd0b1a3bc571050498661d98430
jacky.li <jacky.li@accenture.com>
2019-04-07 01:06:30 +0800

    update code for  #15

--- CR.Elastos-Unity-Carrier/src/index.js
+++ CR.Elastos-Unity-Carrier/src/index.js
@@ -141,87 +141,237 @@ const Carrier = class {
     return exec('setSelfPresence', this.id, presence);
   }
 
+  /*
+   * @brief: compare with a node address as friend
+   * @param: (string) address, node address
+   * @param: (string) msg, hello message
+   * @return: ok
+   */
   addFriend(address, msg){
     return exec('addFriend', this.id, address, msg);
   }
+
+  /*
+   * @brief: accept the friend compare request
+   * @param: (string) friend userId
+   * @return: ok
+   */
   acceptFriend(userId){
     return exec('acceptFriend', this.id, userId);
   }
+
+  /*
+   * @brief: get friend info
+   * @param: friend user id
+   * @return: json (friendInfo)
+   */
   getFriendInfo(friendId){
     return exec('getFriendInfo', this.id, friendId);
   }
+
+  /*
+   * @brief: send message to friend node
+   * @param: friend userId
+   * @param: friend message
+   * @return: ok
+   */
   sendMessage(friendId, msg){
     return exec('sendFriendMessageTo', this.id, friendId, msg);
   }
+
+  /*
+   * @brief: remove friend from friend list
+   * @param: friend userId
+   * @return: ok
+   */
   removeFriend(friendId){
     return exec('removeFriend', this.id, friendId);
   }
+
+  /*
+   * @brief: set label for a friend
+   * @param: friend userId
+   * @param: friend label
+   * @return: ok
+   */
   setLabel(friendId, label){
     return exec('setLabel', this.id, friendId, label);
   }
+
+  /*
+   * @brief: get friends info list
+   * @return: (json array) friends info
+   */
   getFriendList(){
     return exec('getFriendList', this.id);
   }
   
+  /*
+   * @brief: close carrier
+   * @return: ok
+   */
   close(){
     return exec('close', this.id);
   }
+
+  /*
+   * @brief: clean carrier
+   * @return: ok
+   */
   clean(){
     return exec('clean', this.id);
   }
 
+  /*
+   * @brief: create carrier session
+   * @param: friend userId
+   * @param: (enum) STREAM_STATE
+   * @param: (enum) STREAM_MODE
+   * @return: ok
+   */
   createSession(friendId, streamType, streamMode){
     return exec('createSession', this.id, friendId, streamType, streamMode);
   }
 
+  /*
+   * @brief: request to connect as session
+   * @param: friend userId
+   * @return: ok
+   */
   sessionRequest(friendId){
     return exec('sessionRequest', this.id, friendId);
   }
 
+  /*
+   * @brief: reply the session request
+   * @param: friend userId
+   * @param: accept or refuse
+   * @param: if refuse, here is the reason
+   * @return: ok
+   */
   sessionReplyRequest(friendId, status, reason){
     return exec('sessionReplyRequest', this.id, friendId, status, reason);
   }
 
-  // addStreamWithType()
-
+  /*
+   * @brief: send data to the session stream
+   * @param: stream id or friend userId
+   * @param: (string) data string.
+   * @return: ok
+   */
   writeStream(streamIdOrFriendId, data){
     return exec('writeStream', this.id, streamIdOrFriendId, data);
   }
 
+  /*
+   * @brief: remove the session stream for friend node
+   * @param: friend userId
+   * @return: ok
+   */
   removeStream(friendId){
     return exec('removeStream', this.id, friendId);
   }
 
+  /*
+   * @brief: close the session for friend node
+   * @param: friend userId
+   * @return: ok
+   */
   closeSession(friendId){
     return exec('closeSession', this.id, friendId);
   }
 
+  /*
+   * @brief: add session service to friend node
+   * @param: friend userId
+   * @param: service name
+   * @param: target host name
+   * @param: host port
+   * @return: ok
+   */
   addService(friendId, serviceName, host, port){
     return exec('addService', this.id, friendId, serviceName, host, port);
   }
 
+  /*
+   * @brief: remove session service
+   * @param: friend userId
+   * @param: service name 
+   * @return: ok
+   */
   removeService(friendId, serviceName){
     return exec('removeService', this.id, friendId, serviceName);
   }
 
+  /*
+   * @brief: connect the server with carrier port forwarding
+   * @param: friend userId
+   * @param: service name
+   * @param: server host or ip address
+   * @param: server port
+   * @return: (Number) port forwarding id
+   */
   openPortFowarding(friendId, serviceName, host, port){
     return exec('openPortFowarding', this.id, friendId, serviceName, host, port);
   }
+
+  /*
+   * @brief: close the server connection with carrier port forwaring
+   * @param: friend userId
+   * @param: port forwarding id
+   * @return: ok
+   */
   closePortForwarding(friendId, portForwardingId){
     return exec('closePortForwarding', this.id, friendId, portForwardingId);
   }
+
+  /*
+   * @brief: open the channel for friend node.
+   * @param: friend userId
+   * @param: (string)cookie string
+   * @return: (Number)channel id
+   */
   openChannel(friendId, cookie){
     return exec('openChannel', this.id, friendId, cookie);
   }
+
+  /*
+   * @brief: close the channel with channel id
+   * @param: friend userId
+   * @param: channel id
+   * @return: ok
+   */
   closeChannel(friendId, channelId){
     return exec('closeChannel', this.id, friendId, channelId);
   }
+
+  /*
+   * @brief: send data to the friend channel
+   * @param: friend userId
+   * @param: channel id
+   * @param: data string
+   * @return: (Number) data size
+   */
   writeChannel(friendId, channelId, data){
     return exec('writeChannel', this.id, friendId, channelId, data)
   }
+
+  /*
+   * @brief: pend the channel with channel id
+   * @param: friend userId
+   * @param: channel id
+   * @return: ok
+   */
   pendChannel(friendId, channelId){
     return exec('pendChannel', this.id, friendId, channelId);
   }
+
+  /*
+   * @brief: resume the channel with channel id
+   * @param: friend userId
+   * @param: channel id
+   * @return: ok
+   */
   resumeChannel(friendId, channelId){
     return exec('resumeChannel', this.id, friendId, channelId);
   }
