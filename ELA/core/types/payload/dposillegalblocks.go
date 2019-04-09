04f6823100fc088422c8dfcddc4f72fb7461477e
Yan Mingzhi <yanmingzhi@elastos.org>
2019-04-09 21:02:33 +0800

    modify hash calculation about illegal payloads

--- ELA/core/types/payload/dposillegalblocks.go
+++ ELA/core/types/payload/dposillegalblocks.go
@@ -2,10 +2,10 @@ package payload
 
 import (
 	"bytes"
-	"errors"
 	"io"
 
 	"github.com/elastos/Elastos.ELA/common"
+	"github.com/elastos/Elastos.ELA/crypto"
 	"github.com/elastos/Elastos.ELA/elanet/pact"
 )
 
@@ -34,22 +34,85 @@ type DPOSIllegalBlocks struct {
 	hash *common.Uint256
 }
 
-func (b *BlockEvidence) Serialize(w io.Writer) error {
-	if b.Header == nil {
-		return errors.New("block data can not be null")
-	}
+func (b *BlockEvidence) SerializeUnsigned(w io.Writer) error {
 	if err := common.WriteVarBytes(w, b.Header); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (b *BlockEvidence) Deserialize(r io.Reader) error {
+func (b *BlockEvidence) SerializeOthers(w io.Writer) error {
+	if err := common.WriteVarBytes(w, b.BlockConfirm); err != nil {
+		return err
+	}
+
+	if err := common.WriteVarUint(w, uint64(len(b.Signers))); err != nil {
+		return err
+	}
+
+	for _, v := range b.Signers {
+		if err := common.WriteVarBytes(w, v); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (b *BlockEvidence) Serialize(w io.Writer) error {
+	if err := b.SerializeUnsigned(w); err != nil {
+		return err
+	}
+
+	if err := b.SerializeOthers(w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *BlockEvidence) DeserializeUnsigned(r io.Reader) error {
 	var err error
 	if b.Header, err = common.ReadVarBytes(r, pact.MaxBlockSize,
 		"block data"); err != nil {
 		return err
 	}
+	return nil
+}
+
+func (b *BlockEvidence) DeserializeOthers(r io.Reader) (err error) {
+	if b.BlockConfirm, err = common.ReadVarBytes(r, pact.MaxBlockSize,
+		"confirm data"); err != nil {
+		return err
+	}
+
+	var len uint64
+	if len, err = common.ReadVarUint(r, 0); err != nil {
+		return err
+	}
+
+	b.Signers = make([][]byte, 0, len)
+	for i := uint64(0); i < len; i++ {
+		var signer []byte
+		if signer, err = common.ReadVarBytes(r, crypto.COMPRESSEDLEN,
+			"public key"); err != nil {
+			return err
+		}
+		b.Signers = append(b.Signers, signer)
+	}
+
+	return nil
+}
+
+func (b *BlockEvidence) Deserialize(r io.Reader) error {
+	if err := b.DeserializeUnsigned(r); err != nil {
+		return err
+	}
+
+	if err := b.DeserializeOthers(r); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -71,7 +134,7 @@ func (d *DPOSIllegalBlocks) Data(version byte) []byte {
 	return buf.Bytes()
 }
 
-func (d *DPOSIllegalBlocks) Serialize(w io.Writer, version byte) error {
+func (d *DPOSIllegalBlocks) SerializeUnsigned(w io.Writer, version byte) error {
 	if err := common.WriteUint32(w, uint32(d.CoinType)); err != nil {
 		return err
 	}
@@ -80,18 +143,34 @@ func (d *DPOSIllegalBlocks) Serialize(w io.Writer, version byte) error {
 		return err
 	}
 
-	if err := d.Evidence.Serialize(w); err != nil {
+	if err := d.Evidence.SerializeUnsigned(w); err != nil {
 		return err
 	}
 
-	if err := d.CompareEvidence.Serialize(w); err != nil {
+	if err := d.CompareEvidence.SerializeUnsigned(w); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (d *DPOSIllegalBlocks) Deserialize(r io.Reader, version byte) error {
+func (d *DPOSIllegalBlocks) Serialize(w io.Writer, version byte) error {
+	if err := d.SerializeUnsigned(w, version); err != nil {
+		return err
+	}
+
+	if err := d.Evidence.SerializeOthers(w); err != nil {
+		return err
+	}
+
+	if err := d.CompareEvidence.SerializeOthers(w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *DPOSIllegalBlocks) DeserializeUnsigned(r io.Reader, version byte) error {
 	var err error
 	var coinType uint32
 	if coinType, err = common.ReadUint32(r); err != nil {
@@ -103,11 +182,27 @@ func (d *DPOSIllegalBlocks) Deserialize(r io.Reader, version byte) error {
 		return err
 	}
 
-	if err = d.Evidence.Deserialize(r); err != nil {
+	if err = d.Evidence.DeserializeUnsigned(r); err != nil {
 		return err
 	}
 
-	if err = d.CompareEvidence.Deserialize(r); err != nil {
+	if err = d.CompareEvidence.DeserializeUnsigned(r); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *DPOSIllegalBlocks) Deserialize(r io.Reader, version byte) error {
+	if err := d.DeserializeUnsigned(r, version); err != nil {
+		return err
+	}
+
+	if err := d.Evidence.DeserializeOthers(r); err != nil {
+		return err
+	}
+
+	if err := d.CompareEvidence.DeserializeOthers(r); err != nil {
 		return err
 	}
 
@@ -117,7 +212,7 @@ func (d *DPOSIllegalBlocks) Deserialize(r io.Reader, version byte) error {
 func (d *DPOSIllegalBlocks) Hash() common.Uint256 {
 	if d.hash == nil {
 		buf := new(bytes.Buffer)
-		d.Serialize(buf, IllegalBlockVersion)
+		d.SerializeUnsigned(buf, IllegalBlockVersion)
 		hash := common.Uint256(common.Sha256D(buf.Bytes()))
 		d.hash = &hash
 	}
