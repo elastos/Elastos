e876ee554ad00bb465e566742da84be6d673dc18
jacky.li <jacky.li@accenture.com>
2019-04-25 11:20:17 +0800

    update code to add appium

---
+++ CR.Elastos-Unity-Carrier/example/test/specs/native.js
@@ -0,0 +1,11 @@
+describe('WebdriverIO and Appium, when interacting with a login form,', () => {
+  // beforeEach(() => {
+      
+  // });
+
+  it('should be able login successfully', () => {
+      expect(1).toEqual(1);
+  });
+
+ 
+});
