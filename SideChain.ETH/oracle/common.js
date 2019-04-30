8f9bf26fffddae83ab8050425a5b582ba829a388
unknownine <34362260@qq.com>
2019-04-30 15:09:30 +0800

    modify common.js about address and abi
    modify common.js about address and abi

--- SideChain.ETH/oracle/common.js
+++ SideChain.ETH/oracle/common.js
@@ -5,12 +5,13 @@ const Web3 = require("web3");
 const web3 = new Web3(sysconst.ORACLE_SYS_CONNECTION_URL);
 const ks = require("./ks_sample");
 const acc = web3.eth.accounts.decrypt(ks.kstore, ks.kpass);
-const ctrt = require("./ctrt_sub_sample");
-const contract = new web3.eth.Contract(ctrt.abi);
-contract.options.address = ctrt.address;
+const sub_ctrt = require("./ctrt_sub_sample");
+const main_ctrt = require("./ctrt_main_sample");
+const contract = new web3.eth.Contract(sub_ctrt.abi);
+contract.options.address = main_ctrt.address;
 const payloadReceived = {name: null, inputs: null, signature: null};
 
-for (const event of ctrt.abi) {
+for (const event of sub_ctrt.abi) {
     if (event.name === "PayloadReceived" && event.type === "event") {
         payloadReceived.name = event.name;
         payloadReceived.inputs = event.inputs;
