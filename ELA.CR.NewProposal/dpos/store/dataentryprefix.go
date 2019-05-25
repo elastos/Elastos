dc4839c45ceeeef4288801170a7e4c9f17e31ce4
Yan Mingzhi <yanmingzhi@elastos.org>
2019-05-25 16:18:13 +0800

    implement serialize and deserialize for check point

--- ELA.CR.NewProposal/dpos/store/dataentryprefix.go
+++ ELA.CR.NewProposal/dpos/store/dataentryprefix.go
@@ -5,13 +5,6 @@ type DataEntryPrefix byte
 
 const (
 	// DPOS
-	DPOSCheckPoints        DataEntryPrefix = 0x10
-	DPOSDutyIndex          DataEntryPrefix = 0x11
-	DPOSCurrentArbitrators DataEntryPrefix = 0x12
-	DPOSCurrentCandidates  DataEntryPrefix = 0x13
-	DPOSNextArbitrators    DataEntryPrefix = 0x14
-	DPOSNextCandidates     DataEntryPrefix = 0x15
-	DPOSCurrentReward      DataEntryPrefix = 0x16
-	DPOSNextReward         DataEntryPrefix = 0x17
-	DPOSState              DataEntryPrefix = 0x18
+	DPOSCheckPointHeights  DataEntryPrefix = 0x10
+	DPOSSingleCheckPoint   DataEntryPrefix = 0x11
 )
