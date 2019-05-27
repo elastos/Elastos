eab0bd4db6d9c87300864727a262440cd535b2ed
jiangzehua <jiangzehua@elastos.org>
2019-05-27 17:21:06 +0800

    fix list producers error

--- ELA/servers/interfaces.go
+++ ELA/servers/interfaces.go
@@ -1216,8 +1216,8 @@ func ListProducers(param Params) map[string]interface{} {
 
 	sort.Slice(producers, func(i, j int) bool {
 		if producers[i].Votes() == producers[j].Votes() {
-			return bytes.Compare(producers[i].OwnerPublicKey(),
-				producers[j].OwnerPublicKey()) < 0
+			return bytes.Compare(producers[i].NodePublicKey(),
+				producers[j].NodePublicKey()) < 0
 		}
 		return producers[i].Votes() > producers[j].Votes()
 	})
