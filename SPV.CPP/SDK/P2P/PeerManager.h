d3a234ccbe94954aa580bb1c81c38a898bc63b86
poon <heroopan@gmail.com>
2019-06-05 16:56:15 +0800

    fix crash when import with keystore which already exist #734

--- SPV.CPP/SDK/P2P/PeerManager.h
+++ SPV.CPP/SDK/P2P/PeerManager.h
@@ -121,6 +121,8 @@ namespace Elastos {
 
 			void SetReconnectEnableStatus(bool status);
 
+			bool GetReconnectEnableStatus() const;
+
 			void SetFixedPeer(uint128 address, uint16_t port);
 
 			void SetFixedPeers(const std::vector<PeerInfo> &peers);
