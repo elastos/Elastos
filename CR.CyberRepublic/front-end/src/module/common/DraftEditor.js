12be2fe83d15b6a53f800e258e348f5315b128fa
Harry Liu <harryliu@cyberrepublic.org>
2019-06-13 14:27:33 +0800

    able to parse markdown h1 to h6

--- CR.CyberRepublic/front-end/src/module/common/DraftEditor.js
+++ CR.CyberRepublic/front-end/src/module/common/DraftEditor.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import BaseComponent from '@/model/BaseComponent'
-import { Editor, createEditorState } from 'medium-draft'
+import { Editor, createEditorState, StringToTypeMap, Block, HANDLED, NOT_HANDLED, resetBlockWithType, getCurrentBlock } from 'medium-draft'
 import { convertFromHTML, ContentState, EditorState } from 'draft-js'
 import { MEDIUM_DRAFT_TOOLBAR_OPTIONS } from '@/config/constant'
 
 // if using webpack
 import 'medium-draft/lib/index.css'
 
+const newTypeMap = {
+  ...StringToTypeMap,
+  '## ': Block.H2,
+  '### ': Block.H3,
+  '#### ': Block.H4,
+  '##### ': Block.H5,
+  '###### ': Block.H6,
+}
+delete newTypeMap['##']
+
 class Component extends BaseComponent {
   constructor(props) {
     super(props)
-    console.log('constructor props: ', props)
     this.updateContent()
 
     this.refsEditor = React.createRef()
@@ -19,9 +28,6 @@ class Component extends BaseComponent {
 
   componentDidMount() {
     this.refsEditor.current && this.refsEditor.current.focus()
-  }
-
-  componentWillReceiveProps() {
   }
 
   updateContent = () => {
@@ -40,8 +46,6 @@ class Component extends BaseComponent {
       editorState = EditorState.createWithContent(state)
     }
 
-    console.log('constructor editorState: ', editorState)
-
     this.state = {
       editorState,
     }
@@ -50,6 +54,55 @@ class Component extends BaseComponent {
   onChange = (editorState) => {
     this.setState({ editorState })
     this.props.onChange && this.props.onChange(editorState)
+  }
+
+  handleBeforeInput = (editorState, inputString, onChange) => {
+    const mapping = newTypeMap
+    // const selection = editorState.getSelection()
+    const block = getCurrentBlock(editorState)
+    const blockType = block.getType()
+    if (blockType.indexOf(Block.ATOMIC) === 0) {
+      return NOT_HANDLED
+    }
+    // const blockLength = block.getLength()
+    // if (selection.getAnchorOffset() > 1 || blockLength > 1) {
+    //   return NOT_HANDLED
+    // }
+    // const blockTo = mapping[block.getText()[0] + inputString]
+    const blockTo = mapping[block.getText() + inputString]
+    if (!blockTo) {
+      return NOT_HANDLED
+    }
+    const finalType = blockTo.split(':')
+    if (finalType.length < 1 || finalType.length > 3) {
+      return NOT_HANDLED
+    }
+    let fType = finalType[0]
+    if (finalType.length === 1) {
+      if (blockType === finalType[0]) {
+        return NOT_HANDLED
+      }
+    } else if (finalType.length === 2) {
+      if (blockType === finalType[1]) {
+        return NOT_HANDLED
+      }
+      if (blockType === finalType[0]) {
+        fType = finalType[1]
+      }
+    } else if (finalType.length === 3) {
+      if (blockType === finalType[2]) {
+        return NOT_HANDLED
+      }
+      if (blockType === finalType[0]) {
+        fType = finalType[1]
+      } else {
+        fType = finalType[2]
+      }
+    }
+    onChange(resetBlockWithType(editorState, fType, {
+      text: '',
+    }))
+    return HANDLED
   }
 
   ord_render() {
@@ -64,6 +117,7 @@ class Component extends BaseComponent {
         inlineButtons={MEDIUM_DRAFT_TOOLBAR_OPTIONS.INLINE_BUTTONS}
         editorState={this.state.editorState}
         onChange={this.onChange}
+        beforeInput={this.handleBeforeInput}
       />
     )
   }
