597411847c0f3e558ed2416eb1bc9081f6ea27b5
Harry Liu <harryliu@cyberrepublic.org>
2019-06-21 10:25:16 +0800

    updated tests for new schemas

--- CR.CyberRepublic/back-end/test/unit/data/cvote.js
+++ CR.CyberRepublic/back-end/test/unit/data/cvote.js
@@ -3,12 +3,19 @@ global.DB = Object.assign(global.DB, {
 
     CVOTE_1: {
         title: 'Hello World',
-        title_zh: 'Hello World _zh',
-        type: 1,
-        content: 'Lorem Ipsum',
-        content_zh: 'Lorem Ipsum_zh',
+        // title_zh: 'Hello World _zh',
+        // type: 1,
+        // content: 'Lorem Ipsum',
+        // content_zh: 'Lorem Ipsum_zh',
+        // isConflict: 'NO',
+        abstract: 'abstract',
+        goal: 'goal',
+        motivation: 'motivation',
+        relevance: 'relevance',
+        budget: 'budget',
+        plan: 'plan',
         proposedBy: 'Kevin Zhang',
-        isConflict: 'NO',
+        proposedByEmail: 'council_kevin@cyberrepublic.org',
         status: 'ACTIVE',
     }
 
