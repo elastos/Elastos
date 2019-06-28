ae793ba09eb89ff0376b6d9ab82b0ceff0402f2e
Harry Liu <harryliu@cyberrepublic.org>
2019-06-28 15:16:30 +0800

    added tracking and summary schema for proposal

---
+++ CR.CyberRepublic/back-end/src/db/schema/CVoteTrackingSchema.ts
@@ -0,0 +1,34 @@
+import { Schema } from 'mongoose'
+import * as _ from 'lodash'
+import { constant } from '../../constant'
+
+const CommentSchema = {
+  createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+  },
+  comment: {
+      type: String,
+  },
+  createdAt: Date
+}
+
+export const CVote_Tracking = {
+  content: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: _.values(constant.CVOTE_TRACKING_STATUS)
+  },
+  comment: CommentSchema,
+  proposalId: {
+    type: Schema.Types.ObjectId,
+    ref: 'cvote'
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+}
