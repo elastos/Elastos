0e9a80372d08500daaa5af78edf81dd575c9ce7c
poon <heroopan@gmail.com>
2019-07-08 18:17:42 +0800

    update release notes

--- UnionSquare.Android/release_notes/js/index.js
+++ UnionSquare.Android/release_notes/js/index.js
@@ -1,30 +1,7 @@
-$(function () {
-    langgua(getUrlParam("langua")||"ch")
-})
-function langgua(lang){
-    $.ajax({
-        url:lang+'.json',
-        type:'get',
-        dataType : "jsonp",//jsonp数据类型
-        jsonp: "jsonpCallback",//服务端用于接收callback调用的function名的参数
-        success:function(data){
-            if (lang == 'en') {
-                $('header').find('div').eq(2).find("i").eq(1).css("color","#4A8AFF").prev().css("color","#333");
-            }else{
-                $('header').find('div').eq(2).find("i").eq(0).css("color","#4A8AFF").next().css("color","#333");
-            }
-            $('header').find('div').eq(1).text(data.text);
-            $('.content_box').eq(0).find("p").eq(0).text(data.text1);
-            $('.content_box').eq(0).find("p").eq(1).text(data.text5);
-            $('.content_box').eq(0).find("p").eq(2).text(data.text6);
-            $('.content_box').eq(0).find("p").eq(3).text(data.text7);
-            $('.content_box').eq(0).find("p").eq(4).text(data.text8);
-            $('.content_box').eq(1).find("p").eq(0).text(data.text2);
-            $('.content_box').eq(1).find("p").eq(1).text(data.text9);
-            $('#download').text(data.text10)
-        }
-    })
-}
+// $(function () {
+//     langgua(getUrlParam("langua")||"ch")
+// })
+
 //判断iPhone|iPad|iPod|iOS
 if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
     $('#download').attr('href','https://itunes.apple.com/us/app/elastos-wallet/id1453348077?ls=1&mt=8');
