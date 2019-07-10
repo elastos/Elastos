f8772f533f97fd35aad08b20e312d9a340d8293b
Harry Liu <harryliu@cyberrepublic.org>
2019-07-10 16:11:45 +0800

    refactor with service code

--- CR.CyberRepublic/front-end/src/module/page/CVote/detail/Container.js
+++ CR.CyberRepublic/front-end/src/module/page/CVote/detail/Container.js
@@ -1,9 +1,10 @@
-import { createContainer, api_request } from '@/util'
+import { createContainer } from '@/util'
 import Component from './Component'
 import _ from 'lodash'
 import { avatar_map } from '@/constant'
+import CVoteService from '@/service/CVoteService'
 
-export default createContainer(Component, state => ({
+const mapState = state => ({
   user: state.user,
   currentUserId: state.user.current_user_id,
   isLogin: state.user.is_login,
@@ -13,52 +14,39 @@ export default createContainer(Component, state => ({
   avatar_map,
   trackingStatus: _.get(_.last(state.cvoteTracking.all_public), 'status'),
   summaryStatus: _.get(_.last(state.cvoteSummary.all_public), 'status'),
-}), () => ({
-  async getData(id) {
-    const d = await api_request({
-      path: `/api/cvote/get/${id}`,
-    })
+})
 
-    return d
-  },
-  async createCVote(param) {
-    const rs = await api_request({
-      path: '/api/cvote/create',
-      method: 'post',
-      data: param,
-    })
-    return rs
-  },
-  async updateCVote(param) {
-    const rs = await api_request({
-      path: '/api/cvote/update',
-      method: 'post',
-      data: param,
-    })
-    return rs
-  },
-  async vote(param) {
-    const rs = await api_request({
-      path: '/api/cvote/vote',
-      method: 'post',
-      data: param,
-    })
-    return rs
-  },
-  async finishCVote(param) {
-    const rs = await api_request({
-      path: '/api/cvote/finish',
-      method: 'get',
-      data: param,
-    })
-    return rs
-  },
-  async updateNotes(param) {
-    const rs = await api_request({
-      path: '/api/cvote/update_notes',
-      method: 'post',
-      data: param,
-    })
-    return rs
-  },
-}))
+const mapDispatch = () => {
+  const service = new CVoteService()
+  return {
+    async createDraft(param) {
+      return service.createDraft(param)
+    },
+    async listData(param, isCouncil) {
+      return service.listData(param, isCouncil)
+    },
+    async getData(param) {
+      return service.getData(param)
+    },
+    async createCVote(param) {
+      return service.createCVote(param)
+    },
+    async updateCVote(param) {
+      return service.updateCVote(param)
+    },
+    async vote(param) {
+      return service.vote(param)
+    },
+    async finishCVote(param) {
+      return service.finishCVote(param)
+    },
+    async unfinishCVote(param) {
+      return service.unfinishCVote(param)
+    },
+    async updateNotes(param) {
+      return service.updateNotes(param)
+    },
+  }
+}
+
+export default createContainer(Component, mapState, mapDispatch)
