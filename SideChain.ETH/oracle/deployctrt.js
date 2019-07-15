efa36ffd3b58fe9ba6f325b815038b52eb058343
xiaowenwe <xiaowenwe@gmail.com>
2019-07-15 13:20:13 +0800

    Update withdrawal contract bytecode and withdrawal method abi

--- SideChain.ETH/oracle/deployctrt.js
+++ SideChain.ETH/oracle/deployctrt.js
@@ -2,12 +2,15 @@
 
 const Web3 = require("web3");
 const web3 = new Web3("http://127.0.0.1:8545");
-const address= "0x840534b46b3b3bf8c1c3e4c7d34bc86933de7814";
+const ks = require("./ks");
+const acc = web3.eth.accounts.decrypt(ks.kstore, ks.kpass);
 const ctrt = require("./ctrt");
 const contract = new web3.eth.Contract(ctrt.abi);
 const cdata = require("./bytecode");
 const data = contract.deploy({data: cdata.data}).encodeABI();
-const tx = {data: data, gas: "8000000", gasPrice: "200000",from :address};
-web3.eth.sendTransaction(tx).on("transactionHash", console.log).then(console.log) .catch(console.log);
-
-
+const tx = {data: data, gas: "2000000", gasPrice: "2000000000"};
+acc.signTransaction(tx).then((stx) => {
+    web3.eth.sendSignedTransaction(stx.rawTransaction).on("transactionHash", console.log)
+        .then(console.log)
+        .catch(console.log);
+}).catch(console.log);
