ec6354cfb91039d95796aed6e450d78b88de0431
ttblack <zhangxiaobin@elastos.org>
2019-07-17 15:54:34 +0800

    add uint256.cpp

--- SPV.CPP/SDK/Common/uint256.h
+++ SPV.CPP/SDK/Common/uint256.h
@@ -432,54 +432,19 @@ class uint128 : public base_uint128
 public:
     typedef base_uint128 basetype;
 
-    uint128()
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = 0;
-    }
+    uint128();
 
-    uint128(const basetype& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = b.pn[i];
-    }
+    uint128(const basetype &b);
 
-    uint128& operator=(const basetype& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = b.pn[i];
-        return *this;
-    }
+    uint128 &operator=(const basetype &b);
 
-    uint128(uint64_t b)
-    {
-        pn[0] = (unsigned int)b;
-        pn[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            pn[i] = 0;
-    }
+    uint128(uint64_t b);
 
-    uint128& operator=(uint64_t b)
-    {
-        pn[0] = (unsigned int)b;
-        pn[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            pn[i] = 0;
-        return *this;
-    }
+    uint128 &operator=(uint64_t b);
 
-    explicit uint128(const std::string& str)
-    {
-        SetHex(str);
-    }
+    explicit uint128(const std::string &str);
 
-    explicit uint128(const std::vector<unsigned char>& vch)
-    {
-        if (vch.size() == sizeof(pn))
-            memcpy(pn, &vch[0], sizeof(pn));
-        else
-            *this = 0;
-    }
+    explicit uint128(const std::vector<unsigned char> &vch);
 };
 
 inline bool operator==(const uint128& a, uint64_t b)                           { return (base_uint128)a == b; }
@@ -544,54 +509,19 @@ class uint160 : public base_uint160
 public:
     typedef base_uint160 basetype;
 
-    uint160()
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = 0;
-    }
+    uint160();
 
-    uint160(const basetype& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = b.pn[i];
-    }
+    uint160(const basetype &b);
 
-    uint160& operator=(const basetype& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = b.pn[i];
-        return *this;
-    }
+    uint160 &operator=(const basetype &b);
 
-    uint160(uint64_t b)
-    {
-        pn[0] = (unsigned int)b;
-        pn[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            pn[i] = 0;
-    }
+    uint160(uint64_t b);
 
-    uint160& operator=(uint64_t b)
-    {
-        pn[0] = (unsigned int)b;
-        pn[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            pn[i] = 0;
-        return *this;
-    }
+    uint160 &operator=(uint64_t b);
 
-    explicit uint160(const std::string& str)
-    {
-        SetHex(str);
-    }
+    explicit uint160(const std::string &str);
 
-    explicit uint160(const std::vector<unsigned char>& vch)
-    {
-        if (vch.size() == sizeof(pn))
-            memcpy(pn, &vch[0], sizeof(pn));
-        else
-            *this = 0;
-    }
+    explicit uint160(const std::vector<unsigned char> &vch);
 };
 
 inline bool operator==(const uint160& a, uint64_t b)                           { return (base_uint160)a == b; }
@@ -653,95 +583,41 @@ inline const uint160 operator-(const uint160& a, const uint160& b)      { return
 class uint168
 {
 public:
-    uint168() {
-	    _u168.resize(21, 0);
-    }
+    uint168();
 
-    uint168(const uint168& b) {
-        operator=(b);
-    }
+    uint168(const uint168 &b);
 
-    uint168(const std::string &str) {
-        SetHex(str);
-    }
+    uint168(const std::string &str);
 
-    explicit uint168(const bytes_t& vch) {
-        if (vch.size() == 21) {
-        	_u168 = vch;
-        } else {
-            _u168.resize(21, 0);
-        }
-    }
+    explicit uint168(const bytes_t &vch);
 
-    explicit uint168(uint8_t prefix, const bytes_t &vch) {
-        if (vch.size() == 20) {
-        	_u168.push_back(prefix);
-        	_u168 += vch;
-        } else {
-        	_u168.resize(21, 0);
-        }
-    }
+    explicit uint168(uint8_t prefix, const bytes_t &vch);
 
-    uint168& operator=(const uint168& b)
-    {
-    	_u168 = b._u168;
-        return *this;
-    }
+    uint168 &operator=(const uint168 &b);
 
-    bool operator<(const uint168& b) const {
-        return _u168 < b._u168;
-    }
+    bool operator<(const uint168 &b) const;
 
-    bool operator==(const uint168& b) const {
-        return _u168 == b._u168;
-    }
+    bool operator==(const uint168 &b) const;
 
-    bool operator!=(const uint168& b) const {
-        return !(operator==(b));
-    }
+    bool operator!=(const uint168 &b) const;
 
-    unsigned char* begin()
-    {
-        return (unsigned char*)_u168.data();
-    }
+    unsigned char *begin();
 
-    unsigned char* end()
-    {
-        return (unsigned char*)_u168.data() + _u168.size();
-    }
+    unsigned char *end();
 
-    const unsigned char* begin() const
-    {
-        return (unsigned char*)_u168.data();
-    }
+    const unsigned char *begin() const;
 
-    const unsigned char* end() const
-    {
-        return (unsigned char*)_u168.data() + _u168.size();
-    }
+    const unsigned char *end() const;
 
-    std::string GetHex() const {
-        return _u168.getReverse().getHex();
-    }
+    std::string GetHex() const;
 
-    void SetHex(const std::string& str) {
-        _u168.setHex(str);
-        _u168.reverse();
-        if (_u168.size() != 21)
-            _u168.resize(21, 0);
-    }
+    void SetHex(const std::string &str);
 
-    unsigned int size() const {
-        return _u168.size();
-    }
+    unsigned int size() const;
 
-    uint8_t prefix() const {
-    	return _u168[0];
-    }
+	uint8_t prefix() const;
 
-    const bytes_t &bytes() const {
-        return _u168;
-    }
+	const bytes_t &bytes() const;
 
 private:
     bytes_t _u168;
@@ -760,61 +636,21 @@ class uint256 : public base_uint256
 public:
     typedef base_uint256 basetype;
 
-    uint256()
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = 0;
-    }
+    uint256();
 
-    uint256(const basetype& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = b.pn[i];
-    }
+    uint256(const basetype &b);
 
-    uint256& operator=(const basetype& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = b.pn[i];
-        return *this;
-    }
+    uint256 &operator=(const basetype &b);
 
-    uint256& operator=(const bytes_t& vch)
-    {
-        if (vch.size() == sizeof(pn))
-            memcpy(pn, &vch[0], sizeof(pn));
-        else
-            memset(pn, 0, sizeof(pn));
+    uint256 &operator=(const bytes_t &vch);
 
-        return *this;
-    }
+    uint256(uint64_t b);
 
-    uint256(uint64_t b)
-    {
-        pn[0] = (unsigned int)b;
-        pn[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            pn[i] = 0;
-    }
+    uint256 &operator=(uint64_t b);
 
-    uint256& operator=(uint64_t b)
-    {
-        pn[0] = (unsigned int)b;
-        pn[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            pn[i] = 0;
-        return *this;
-    }
+    explicit uint256(const std::string &str);
 
-    explicit uint256(const std::string& str)
-    {
-        SetHex(str);
-    }
-
-    explicit uint256(const bytes_t& vch)
-    {
-	    operator=(vch);
-    }
+    explicit uint256(const bytes_t &vch);
 };
 
 inline bool operator==(const uint256& a, uint64_t b)                           { return (base_uint256)a == b; }
@@ -882,63 +718,21 @@ class uint512 : public base_uint512
 public:
     typedef base_uint512 basetype;
 
-    uint512()
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = 0;
-    }
+    uint512();
 
-    uint512(const basetype& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = b.pn[i];
-    }
+    uint512(const basetype &b);
 
-    uint512& operator=(const basetype& b)
-    {
-        for (int i = 0; i < WIDTH; i++)
-            pn[i] = b.pn[i];
-        return *this;
-    }
+    uint512 &operator=(const basetype &b);
 
-    uint512(uint64_t b)
-    {
-        pn[0] = (unsigned int)b;
-        pn[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            pn[i] = 0;
-    }
+    uint512(uint64_t b);
 
-    uint512& operator=(uint64_t b)
-    {
-        pn[0] = (unsigned int)b;
-        pn[1] = (unsigned int)(b >> 32);
-        for (int i = 2; i < WIDTH; i++)
-            pn[i] = 0;
-        return *this;
-    }
+    uint512 &operator=(uint64_t b);
 
-    explicit uint512(const std::string& str)
-    {
-        SetHex(str);
-    }
+    explicit uint512(const std::string &str);
 
-    explicit uint512(const std::vector<unsigned char>& vch)
-    {
-        if (vch.size() == sizeof(pn))
-            memcpy(pn, &vch[0], sizeof(pn));
-        else
-            *this = 0;
-    }
+    explicit uint512(const std::vector<unsigned char> &vch);
 
-    uint256 trim256() const
-    {
-        uint256 ret;
-        for (unsigned int i = 0; i < uint256::WIDTH; i++){
-            ret.pn[i] = pn[i];
-        }
-        return ret;
-    }
+    uint256 trim256() const;
 };
 
 inline bool operator==(const uint512& a, uint64_t b)                           { return (base_uint512)a == b; }
