e53c29463223f2f599ff8b79ab2e6c9b2f871496
Xiaobo Tian <peterwillcn@gmail.com>
2019-07-17 17:22:17 +0800

    Add referenced third party copyright information
    Signed-off-by: Xiaobo Tian <peterwillcn@gmail.com>

--- ELA/crypto/ecies/params.go
+++ ELA/crypto/ecies/params.go
@@ -1,3 +1,5 @@
+// Copyright (c) 2013 Kyle Isom <kyle@tyrfingr.is>
+// Copyright (c) 2012 The Go Authors. All rights reserved.
 // Copyright (c) 2017-2019 Elastos Foundation
 // Use of this source code is governed by an MIT
 // license that can be found in the LICENSE file.
