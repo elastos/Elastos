aba31e0235fc39b2f1e1c25e79f17d8e2f1f0461
bin.lv <lvbin@elastos.org>
2019-07-17 18:04:30 +0800

    if blocknum is zero return error

--- SideChain.ETH/oracle/getblknum.js
+++ SideChain.ETH/oracle/getblknum.js
@@ -7,6 +7,10 @@ module.exports = async function(res) {
         console.log("Getting Sidechain Block Number...");
         console.log("============================================================");
         let blkNum = await common.web3.eth.getBlockNumber();
+        if (blkNum === 0){
+            common.reterr("InternalError", res);
+            return;
+        }
         console.log("Sidechain Block Number: " + blkNum);
         console.log("============================================================");
         res.json({"result": blkNum, "id": null, "error": null, "jsonrpc": "2.0"});
