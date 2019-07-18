c01cf96327d78776421acfa0225fc7b1c5f7d5c9
Yan Mingzhi <yanmingzhi@elastos.org>
2019-07-18 18:15:52 +0800

    add config to define the start height to enable active illegal producer

--- ELA/common/config/params.go
+++ ELA/common/config/params.go
@@ -138,26 +138,27 @@ var DefaultParams = Params{
 		"02b95b000f087a97e988c24331bf6769b4a75e4b7d5d2a38105092a3aa841be33b",
 		"02a0aa9eac0e168f3474c2a0d04e50130833905740a5270e8a44d6c6e85cf6d98c",
 	},
-	PowLimit:                 powLimit,
-	PowLimitBits:             0x1f0008ff,
-	TargetTimespan:           24 * time.Hour,  // 24 hours
-	TargetTimePerBlock:       2 * time.Minute, // 2 minute
-	AdjustmentFactor:         4,               // 25% less, 400% more
-	RewardPerBlock:           rewardPerBlock(2 * time.Minute),
-	CoinbaseMaturity:         100,
-	MinTransactionFee:        100,
-	MinCrossChainTxFee:       10000,
-	CheckAddressHeight:       88812,
-	VoteStartHeight:          290000,
-	CRCOnlyDPOSHeight:        343400,
-	PublicDPOSHeight:         402680,
-	ToleranceDuration:        5 * time.Second,
-	MaxInactiveRounds:        720 * 2,
-	InactivePenalty:          0, //there will be no penalty in this version
-	EmergencyInactivePenalty: 0, //there will be no penalty in this version
-	GeneralArbiters:          24,
-	CandidateArbiters:        72,
-	PreConnectOffset:         360,
+	PowLimit:                    powLimit,
+	PowLimitBits:                0x1f0008ff,
+	TargetTimespan:              24 * time.Hour,  // 24 hours
+	TargetTimePerBlock:          2 * time.Minute, // 2 minute
+	AdjustmentFactor:            4,               // 25% less, 400% more
+	RewardPerBlock:              rewardPerBlock(2 * time.Minute),
+	CoinbaseMaturity:            100,
+	MinTransactionFee:           100,
+	MinCrossChainTxFee:          10000,
+	CheckAddressHeight:          88812,
+	VoteStartHeight:             290000,
+	CRCOnlyDPOSHeight:           343400,
+	PublicDPOSHeight:            402680,
+	EnableActivateIllegalHeight: 439000,
+	ToleranceDuration:           5 * time.Second,
+	MaxInactiveRounds:           720 * 2,
+	InactivePenalty:             0, //there will be no penalty in this version
+	EmergencyInactivePenalty:    0, //there will be no penalty in this version
+	GeneralArbiters:             24,
+	CandidateArbiters:           72,
+	PreConnectOffset:            360,
 }
 
 // TestNet returns the network parameters for the test network.
@@ -202,6 +203,7 @@ func (p *Params) TestNet() *Params {
 	copy.VoteStartHeight = 200000
 	copy.CRCOnlyDPOSHeight = 246700
 	copy.PublicDPOSHeight = 300000
+	copy.EnableActivateIllegalHeight = 1000000 //todo correct me later
 	return &copy
 }
 
@@ -248,6 +250,7 @@ func (p *Params) RegNet() *Params {
 	copy.VoteStartHeight = 170000
 	copy.CRCOnlyDPOSHeight = 211000
 	copy.PublicDPOSHeight = 234000
+	copy.EnableActivateIllegalHeight = 1000000 //todo correct me later
 	return &copy
 }
 
@@ -340,6 +343,10 @@ type Params struct {
 	// PublicDPOSHeight (H2) indicates the height when public registered and
 	// elected producers participate in DPOS consensus.
 	PublicDPOSHeight uint32
+
+	// PublicDPOSHeight defines the start height to enable activate illegal
+	// producer though activate tx
+	EnableActivateIllegalHeight uint32
 
 	// CRCArbiters defines the fixed CRC arbiters producing the block.
 	CRCArbiters []string
