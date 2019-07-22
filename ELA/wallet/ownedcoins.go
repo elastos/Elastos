46655f4307a9cda3fc0e6a595164be88fd14bdd4
Andrew yz <deedecho@gmail.com>
2019-07-22 11:04:06 +0800

    add generic method for wallet to append a coin

--- ELA/wallet/ownedcoins.go
+++ ELA/wallet/ownedcoins.go
@@ -5,7 +5,12 @@
 
 package wallet
 
-import "github.com/elastos/Elastos.ELA/core/types"
+import (
+	"io"
+
+	"github.com/elastos/Elastos.ELA/common"
+	"github.com/elastos/Elastos.ELA/core/types"
+)
 
 // CoinOwnership as a key for OwnedCoins map to represents the ownership
 // between OutPoint and owner.
@@ -14,15 +19,117 @@ type CoinOwnership struct {
 	op    types.OutPoint
 }
 
+func (co *CoinOwnership) Serialize(w io.Writer) error {
+	if err := common.WriteVarString(w, co.owner); err != nil {
+		return err
+	}
+
+	return co.op.Serialize(w)
+}
+
+func (co *CoinOwnership) Deserialize(r io.Reader) error {
+	owner, err := common.ReadVarString(r)
+	if err != nil {
+		return err
+	}
+	co.owner = owner
+
+	return co.op.Deserialize(r)
+}
+
 // CoinLinkedItem as a value for OwnedCoins map to be a linked list item.
 type CoinLinkedItem struct {
 	prev *types.OutPoint
 	next *types.OutPoint
 }
 
+func (cl *CoinLinkedItem) Serialize(w io.Writer) error {
+	if cl.prev == nil {
+		prev := types.OutPoint{}
+		if err := prev.Serialize(w); err != nil {
+			return err
+		}
+	} else {
+		if err := cl.prev.Serialize(w); err != nil {
+			return err
+		}
+	}
+	if cl.next == nil {
+		next := types.OutPoint{}
+		if err := next.Serialize(w); err != nil {
+			return err
+		}
+	} else {
+		if err := cl.next.Serialize(w); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (cl *CoinLinkedItem) Deserialize(r io.Reader) error {
+	var zeroOP types.OutPoint
+	var prev types.OutPoint
+	if err := prev.Deserialize(r); err != nil {
+		return err
+	}
+	if prev == zeroOP {
+		cl.prev = nil
+	} else {
+		cl.prev = &prev
+	}
+	var next types.OutPoint
+	if err := next.Deserialize(r); err != nil {
+		return err
+	}
+	if next == zeroOP {
+		cl.next = nil
+	} else {
+		cl.next = &next
+	}
+
+	return nil
+}
+
 // OwnedCoins store the ownership between OutPoint and owner, and can traverse the
 // OutPoint of an owner through a linked list.
 type OwnedCoins map[CoinOwnership]CoinLinkedItem
+
+func (oc OwnedCoins) Serialize(w io.Writer) error {
+	if err := common.WriteUint32(w, uint32(len(oc))); err != nil {
+		return err
+	}
+	for k, v := range oc {
+		if err := k.Serialize(w); err != nil {
+			return err
+		}
+		if err := v.Serialize(w); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (oc OwnedCoins) Deserialize(r io.Reader) error {
+	count, err := common.ReadUint32(r)
+	if err != nil {
+		return err
+	}
+	for i := uint32(0); i < count; i++ {
+		var co CoinOwnership
+		if err := co.Deserialize(r); err != nil {
+			return err
+		}
+		var cl CoinLinkedItem
+		if err := cl.Deserialize(r); err != nil {
+			return err
+		}
+		oc[co] = cl
+	}
+
+	return nil
+}
 
 func (oc OwnedCoins) read(owner string, op *types.OutPoint) CoinLinkedItem {
 	if op == nil {
