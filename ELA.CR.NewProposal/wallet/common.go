d056612ac7bbbb51d675a898a20aa6112457a77e
Andrew yz <deedecho@gmail.com>
2019-07-23 17:02:23 +0800

    store vote and deposit utxo to wallet

--- ELA.CR.NewProposal/wallet/common.go
+++ ELA.CR.NewProposal/wallet/common.go
@@ -6,6 +6,8 @@
 package wallet
 
 import (
+	"sync"
+
 	"github.com/elastos/Elastos.ELA/blockchain"
 )
 
@@ -20,4 +22,24 @@ const (
 
 var (
 	Store blockchain.IChainStore
+
+	addressBook = make(map[string]*AddressInfo, 0)
+	abMutex     sync.RWMutex
 )
+
+// GetWalletAccount retrieval an address information in wallet by address
+func GetWalletAccount(address string) (*AddressInfo, bool) {
+	abMutex.RLock()
+	defer abMutex.RUnlock()
+
+	addressInfo, exist := addressBook[address]
+	return addressInfo, exist
+}
+
+// SetWalletAccount set an address information to wallet
+func SetWalletAccount(addressInfo *AddressInfo) {
+	abMutex.Lock()
+	defer abMutex.Unlock()
+
+	addressBook[addressInfo.address] = addressInfo
+}
