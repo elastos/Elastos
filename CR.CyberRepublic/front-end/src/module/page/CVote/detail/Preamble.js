5b5c8afad2c25da727fba63a5ed35b62c5861462
Billie Zhang <billiecoder@gmail.com>
2019-07-24 14:31:19 +0800

    format the code of Preamble component

--- CR.CyberRepublic/front-end/src/module/page/CVote/detail/Preamble.js
+++ CR.CyberRepublic/front-end/src/module/page/CVote/detail/Preamble.js
@@ -9,42 +9,73 @@ import styled from 'styled-components'
 
 const Component = ({ vid, title, proposedBy, proposer, status, createdAt }) => {
   // header
-  const headerNode = <Header id="preamble">{I18N.get('proposal.fields.preamble')}</Header>
+  const headerNode = (
+    <Header id="preamble">{I18N.get('proposal.fields.preamble')}</Header>
+  )
   // note
-  // const noteNode = <Note>Note: For confidential purpose, no one elase including council members is not able to access these files untill the bidding ended.</Note>
+  // const noteNode = (
+  //   <Note>
+  //     Note: For confidential purpose, no one elase including council members is
+  //     not able to access these files untill the bidding ended.
+  //   </Note>
+  // )
   // id
   const idNode = (
     <Item>
-      <Col span={6}><ItemTitle>{I18N.get('proposal.fields.preambleSub.proposal')}</ItemTitle></Col>
-      <Col span={18}><ItemText>{`#${vid}`}</ItemText></Col>
+      <Col span={6}>
+        <ItemTitle>
+          {I18N.get('proposal.fields.preambleSub.proposal')}
+        </ItemTitle>
+      </Col>
+      <Col span={18}>
+        <ItemText>{`#${vid}`}</ItemText>
+      </Col>
     </Item>
   )
   // title
   const titleNode = (
     <Item>
-      <Col span={6}><ItemTitle>{I18N.get('proposal.fields.preambleSub.title')}</ItemTitle></Col>
-      <Col span={18}><ItemText>{title}</ItemText></Col>
+      <Col span={6}>
+        <ItemTitle>{I18N.get('proposal.fields.preambleSub.title')}</ItemTitle>
+      </Col>
+      <Col span={18}>
+        <ItemText>{title}</ItemText>
+      </Col>
     </Item>
   )
   // proposer
   const proposerNode = (
     <Item>
-      <Col span={6}><ItemTitle>{I18N.get('proposal.fields.preambleSub.proposer')}</ItemTitle></Col>
-      <Col span={18}><ItemText>{`${proposedBy} <${_.get(proposer, 'email')}>`}</ItemText></Col>
+      <Col span={6}>
+        <ItemTitle>
+          {I18N.get('proposal.fields.preambleSub.proposer')}
+        </ItemTitle>
+      </Col>
+      <Col span={18}>
+        <ItemText>{`${proposedBy} <${_.get(proposer, 'email')}>`}</ItemText>
+      </Col>
     </Item>
   )
   // status
   const statusNode = (
     <Item>
-      <Col span={6}><ItemTitle>{I18N.get('proposal.fields.preambleSub.status')}</ItemTitle></Col>
-      <Col span={18}><ItemText>{I18N.get(`cvoteStatus.${status}`)}</ItemText></Col>
+      <Col span={6}>
+        <ItemTitle>{I18N.get('proposal.fields.preambleSub.status')}</ItemTitle>
+      </Col>
+      <Col span={18}>
+        <ItemText>{I18N.get(`cvoteStatus.${status}`)}</ItemText>
+      </Col>
     </Item>
   )
   // created
   const createdNode = (
     <Item>
-      <Col span={6}><ItemTitle>{I18N.get('proposal.fields.preambleSub.created')}</ItemTitle></Col>
-      <Col span={18}><ItemText>{moment(createdAt).format('MMM D, YYYY')}</ItemText></Col>
+      <Col span={6}>
+        <ItemTitle>{I18N.get('proposal.fields.preambleSub.created')}</ItemTitle>
+      </Col>
+      <Col span={18}>
+        <ItemText>{moment(createdAt).format('MMM D, YYYY')}</ItemText>
+      </Col>
     </Item>
   )
   return (
@@ -66,11 +97,10 @@ const propTypes = {
   proposedBy: PropTypes.string.isRequired,
   proposer: PropTypes.object.isRequired,
   status: PropTypes.string.isRequired,
-  createdAt: PropTypes.string.isRequired,
+  createdAt: PropTypes.string.isRequired
 }
 
 Component.propTypes = propTypes
-
 
 export default Component
 
@@ -85,7 +115,7 @@ const Item = styled(Row)`
 const ItemTitle = styled.div`
   font-weight: 400;
   :after {
-    content: ":";
+    content: ':';
   }
 `
 const ItemText = styled.div`
