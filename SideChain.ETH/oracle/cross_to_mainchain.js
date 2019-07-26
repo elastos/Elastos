27794fe7eca40391cd2521b7dae4df7d7f53c023
xiaowenwe <xiaowenwe@gmail.com>
2019-07-26 11:29:38 +0800

    Modify tx.value

--- SideChain.ETH/oracle/cross_to_mainchain.js
+++ SideChain.ETH/oracle/cross_to_mainchain.js
@@ -5,13 +5,10 @@ contract.options.address="0x491bC043672B9286fA02FA7e0d6A3E5A0384A31A"
 acc = web3.eth.accounts.decrypt({"address":"840534b46b3b3bf8c1c3e4c7d34bc86933de7814","crypto":{"cipher":"aes-128-ctr","ciphertext":"2e8ed4f40c71538a12df95fa0b5b21707be75c7dd1b57e390e505659d6a4ab72","cipherparams":{"iv":"f8b3e54a710dc7ee7faae3e7870d0cc0"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"3affb21811ef5115de926976e9b3119f92545bcfa574ba51d9200cd4d2d8531d"},"mac":"526443e3cf1e3194afbfccc9f8f7aa8ce30b5dbb7653da513851a7d8d85407f9"},"id":"c66a6ceb-1542-429f-81db-0ca916b72fd3","version":3}, "12345678");
 cdata  = contract.methods.receivePayload("Ef2Ug9gtJCiGt7opWzz22GZuJvB9ETDJxL", "2000000000000000000","100000000000000").encodeABI()
 tx = {data: cdata,to:contract.options.address, from:acc.address, gas: "3000000", gasPrice: "20000000000"};
-tx.value="400000000000000000"
-// web3.eth.estimateGas(tx);
+tx.value="200000000000000000"
  acc.signTransaction(tx).then((res)=>{
-     console.log("coming")
      stx=res;
      console.log(stx.rawTransaction)
-     console.log("==================")
      web3.eth.sendSignedTransaction(stx.rawTransaction).then(console.log)});
 
 
