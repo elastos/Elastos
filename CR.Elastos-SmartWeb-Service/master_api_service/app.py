399b0361c5e89b0d736e29419bf6e9ba5e37bf85
Rahul Gunasekaran <rahulguna@Rahuls-MacBook.byod.gmu.edu>
2019-07-29 13:28:12 -0400

    commented the sample user API

--- CR.Elastos-SmartWeb-Service/master_api_service/app.py
+++ CR.Elastos-SmartWeb-Service/master_api_service/app.py
@@ -3,7 +3,7 @@ import logging.config
 import os
 from flask import Flask, Blueprint
 from master_api_service import settings
-from master_api_service.api.nodes.endpoints.user import ns as nodes_user_namespace
+#from master_api_service.api.nodes.endpoints.user import ns as nodes_user_namespace
 from master_api_service.api.wallet.endpoints.wallet import ns as nodes_user_namespace
 from master_api_service.api.common.endpoints.common import ns as nodes_user_namespace
 from master_api_service.api.mainchain.endpoints.mainchain import ns as nodes_user_namespace
