ea1ea5eb97622822505dcdde99188ffce61a13e1
xinlei zhao <xinlei.z.zhao@pwc.com>
2019-08-05 22:34:39 +0800

    adjust margin-left to edit / create

--- CR.CyberRepublic/front-end/src/module/page/suggestion/create/style.js
+++ CR.CyberRepublic/front-end/src/module/page/suggestion/create/style.js
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   max-width: 1200px;
-  margin: 80px auto;
+  margin: 80px auto 80px 210px;
 
   @media only screen and (max-width: ${breakPoint.xl}) {
-    margin-left: 108px;
+    margin-left: 210px;
     margin-right: 5%;
   }
   @media only screen and (max-width: ${breakPoint.lg}) {
-    margin-left: 5%;
+    margin-left: 210px;
   }
   @media only screen and (max-width: ${breakPoint.mobile}) {
     width: 90vw;
