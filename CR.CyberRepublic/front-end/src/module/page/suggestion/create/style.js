1ecacbf5a011c5f0e6708324f4211bc7a1894afd
xinlei zhao <xinlei.z.zhao@pwc.com>
2019-08-13 12:11:49 +0800

    adjust style for suggestion form

--- CR.CyberRepublic/front-end/src/module/page/suggestion/create/style.js
+++ CR.CyberRepublic/front-end/src/module/page/suggestion/create/style.js
@@ -1,18 +1,12 @@
-import { breakPoint } from '@/constants/breakPoint'
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  max-width: 1200px;
-  margin: 80px auto 80px 210px;
-
-  @media only screen and (max-width: ${breakPoint.xl}) {
-    margin-left: 210px;
-    margin-right: 5%;
-  }
-  @media only screen and (max-width: ${breakPoint.lg}) {
-    margin-left: 210px;
-  }
-  @media only screen and (max-width: ${breakPoint.mobile}) {
-    width: 90vw;
+  padding: 0 50px 80px;
+  width: 80vw;
+  margin: 80px auto 0;
+  background: #ffffff;
+  text-align: left;
+  @media only screen and (max-width: 576px) {
+    margin: 15px;
   }
 `
