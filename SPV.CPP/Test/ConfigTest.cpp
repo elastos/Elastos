9ffb1108dc700f49e6c1b161db67d0414d82d650
poon <heroopan@gmail.com>
2019-08-19 13:31:24 +0800

    add PrvNet to Config.cfg

--- SPV.CPP/Test/ConfigTest.cpp
+++ SPV.CPP/Test/ConfigTest.cpp
@@ -58,10 +58,8 @@ TEST_CASE("Config test", "[Config]") {
 			REQUIRE(dnsSeed[10] == "node-mainnet-023.elastos.org");
 
 			const std::vector<CheckPoint> &checkPoints = chainParams->Checkpoints();
-			REQUIRE(checkPoints[1].Hash() == uint256("333d9a0e874cf1b165a998c061c2f8be8e03ce31712046d001823d528e1fec84"));
-			REQUIRE(checkPoints[1].Height() == 2016);
-			REQUIRE(checkPoints[2].Hash() == uint256("6347d62e227dbf74c8a4c478fa4f8d351f24a3db3f5a01aad32ea262b6e6f6aa"));
-			REQUIRE(checkPoints[2].Height() == 12096);
+			REQUIRE(checkPoints[0].Hash() == uint256("05f458a5522851622cae2bb138498dec60a8f0b233802c97a1ca41f9f214708d"));
+			REQUIRE(checkPoints[0].Height() == 0);
 		}
 
 		SECTION("TestNet") {
@@ -91,10 +89,8 @@ TEST_CASE("Config test", "[Config]") {
 			REQUIRE(dnsSeed[2] == "node-testnet-007.elastos.org");
 
 			const std::vector<CheckPoint> &checkPoints = chainParams->Checkpoints();
-			REQUIRE(checkPoints[4].Hash() == uint256("0fd0ecfabdd3f3405b9808e4f67749232d23404fd1c90a4e2c755ead8651e759"));
-			REQUIRE(checkPoints[4].Height() == 20160);
-			REQUIRE(checkPoints[5].Hash() == uint256("d3ab4cdccac516981d90b4eed4956d23a5c6cda76b033386754a7493f3e2e54c"));
-			REQUIRE(checkPoints[5].Height() == 26208);
+			REQUIRE(checkPoints[0].Hash() == uint256("6418be20291bc857c9a01e5ba205445b85a0593d47cc0b576d55a55e464f31b3"));
+			REQUIRE(checkPoints[0].Height() == 0);
 		}
 
 		SECTION("RegTest") {
@@ -124,10 +120,8 @@ TEST_CASE("Config test", "[Config]") {
 			REQUIRE(dnsSeed[2] == "node-regtest-107.eadd.co");
 
 			const std::vector<CheckPoint> &checkPoints = chainParams->Checkpoints();
-			REQUIRE(checkPoints[4].Hash() == uint256("3d2269ae53697c648a40aabaf6841cd2a43bc38699264a64bcd166e000cd452a"));
-			REQUIRE(checkPoints[4].Height() == 26208);
-			REQUIRE(checkPoints[5].Hash() == uint256("8dfe3ec6125c06c40bcd9a72963b6e60dc214b7a61ad2e805b138f60ac7e92de"));
-			REQUIRE(checkPoints[5].Height() == 34272);
+			REQUIRE(checkPoints[0].Hash() == uint256("6418be20291bc857c9a01e5ba205445b85a0593d47cc0b576d55a55e464f31b3"));
+			REQUIRE(checkPoints[0].Height() == 0);
 		}
 
 	}
@@ -172,10 +166,8 @@ TEST_CASE("Config test", "[Config]") {
 			REQUIRE(dnsSeed[9] == "node-mainnet-035.elastos.org");
 
 			const std::vector<CheckPoint> &checkPoints = chainParams->Checkpoints();
-			REQUIRE(checkPoints[4].Hash() == uint256("d342ee0817e30a57a515ccfaaf836e7314aac1235d0659d5e6ffc46671ae4979"));
-			REQUIRE(checkPoints[4].Height() == 14112);
-			REQUIRE(checkPoints[5].Hash() == uint256("fc4e7ba460e38964faeb33873418c9fcd3ae2648a543cc1e8f5c47806b593ac8"));
-			REQUIRE(checkPoints[5].Height() == 18144);
+			REQUIRE(checkPoints[0].Hash() == uint256("56be936978c261b2e649d58dbfaf3f23d4a868274f5522cd2adb4308a955c4a3"));
+			REQUIRE(checkPoints[0].Height() == 0);
 		}
 
 		SECTION("TestNet") {
@@ -207,10 +199,8 @@ TEST_CASE("Config test", "[Config]") {
 			REQUIRE(dnsSeed[4] == "node-testnet-015.elastos.org");
 
 			const std::vector<CheckPoint> &checkPoints = chainParams->Checkpoints();
-			REQUIRE(checkPoints[4].Hash() == uint256("46a9dd847b62f278a26a93e3f7a4cc135b30754e07f2754d6ac0baf6870c8060"));
-			REQUIRE(checkPoints[4].Height() == 8064);
-			REQUIRE(checkPoints[5].Hash() == uint256("09cdb3d720a7095c7023241361ca8a317c75273fbdd0e5dc268667683e360780"));
-			REQUIRE(checkPoints[5].Height() == 10080);
+			REQUIRE(checkPoints[0].Hash() == uint256("56be936978c261b2e649d58dbfaf3f23d4a868274f5522cd2adb4308a955c4a3"));
+			REQUIRE(checkPoints[0].Height() == 0);
 		}
 
 		SECTION("RegTest") {
@@ -244,8 +234,6 @@ TEST_CASE("Config test", "[Config]") {
 			const std::vector<CheckPoint> &checkPoints = chainParams->Checkpoints();
 			REQUIRE(checkPoints[0].Hash() == uint256("56be936978c261b2e649d58dbfaf3f23d4a868274f5522cd2adb4308a955c4a3"));
 			REQUIRE(checkPoints[0].Height() == 0);
-			REQUIRE(checkPoints[1].Hash() == uint256("fb29c3de03ab8dbdc9e50ee882efc9a8115697a7681593ec1c587dd8ddc0da70"));
-			REQUIRE(checkPoints[1].Height() == 2016);
 		}
 	}
 
