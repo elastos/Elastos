916161323751a1c9334f6b3b3f1beea3d418693b
Harry Liu <harryliu@cyberrepublic.org>
2019-08-20 00:17:16 +0800

    improved editor util

--- CR.CyberRepublic/front-end/src/util/editor.js
+++ CR.CyberRepublic/front-end/src/util/editor.js
@@ -1,13 +1,29 @@
 import { createEditorState } from 'medium-draft'
 import mediumDraftExporter from 'medium-draft/lib/exporter'
+import { convertFromHTML, ContentState, EditorState } from 'draft-js'
+import _ from 'lodash'
 
 export const getHTML = (data, key) => {
   const { contentType } = data
   const content = _.get(data, key, '')
   let editorState
-  if (content && contentType === 'MARKDOWN') {
-    editorState = createEditorState(JSON.parse(content))
-    return mediumDraftExporter(editorState.getCurrentContent())
+  if (!content) {
+    editorState = createEditorState()
+  } else if (contentType === 'MARKDOWN') {
+    try {
+      editorState = createEditorState(JSON.parse(content))
+    } catch (err) {}
   }
-  return content
+
+  if (!editorState) {
+    const blocksFromHTML = convertFromHTML(content)
+    const state = ContentState.createFromBlockArray(
+      blocksFromHTML.contentBlocks,
+      blocksFromHTML.entityMap
+    )
+    editorState = EditorState.createWithContent(state)
+  }
+
+  return mediumDraftExporter(editorState.getCurrentContent())
+  // return content
 }
