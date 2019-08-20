18a080c5b9028af552e7f26282f654e63049109b
Rahul Gunasekaran <rahulguna10@gmail.com>
2019-08-20 16:29:38 -0400

    updated the documentation and code

--- CR.Elastos-Nucleus/src/views/uploadandsign.jsx
+++ CR.Elastos-Nucleus/src/views/uploadandsign.jsx
@@ -51,31 +51,26 @@ class UserProfile extends Component {
                         <FormGroup controlId="formControlsTextarea">
                           <p>
                             <span className="category" />
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing
-                            elit, sed diem nonummy nibh euismod tincidunt ut
-                            lacreet dolore magna aliguam erat volutpat. Ut wisis
-                            enim ad minim veniam, quis nostrud exerci tution
-                            ullam corper suscipit lobortis nisi ut aliquip ex ea
-                            commodo consequat. Duis te feugi facilisi. Duis
-                            autem dolor in hendrerit in vulputate velit esse
-                            molestie consequat, vel illum dolore eu feugiat
-                            nulla facilisis at vero eros et accumsan et iusto
-                            odio dignissim qui blandit praesent luptatum zzril
-                            delenit au gue duis dolore te feugat nulla facilisi.
+                            UploadAndSign API uploads the request file to hive and signs the content of the file using DID sidechain.
                           </p>
                         </FormGroup>
                         <SyntaxHighlighter
                           language="javascript"
                           style={gruvboxDark}
                         >
-                          {`POST /api/1/sign HTTP/1.1
-Host: localhost:8090
-Content-Type: application/json
+                          {`POST /api/1/console/uploadAndSign HTTP/1.1
+Host: localhost:8888
+Content-Type: multipart/form-data; boundary=--------------------------942705689964164935672351
 
-  {
-      "privateKey":"0D5D7566CA36BC05CFF8E3287C43977DCBB492990EA1822643656D85B3CB0226",
-      "msg":"Hello World"
-  }`}
+headers:{
+    "api_key":564732BHU,
+    "private_key":039C9EF3BD38C8E677BFH398R32F40A998F8872ADC23R32UHR89DE2A21631310E2F200E43B3,
+}
+
+request.files{
+    "file":"sample.txt"
+}   
+`}
                         </SyntaxHighlighter>
                         <SyntaxHighlighter
                           language="javascript"
@@ -87,9 +82,10 @@ Content-Type: application/json
 
 {
     "result": {
-        "msg": "E4BDA0E5A5BDEFBC8CE4B896E7958C",
-        "pub": "02C3F59F337814C6715BBE684EC525B9A3CFCE55D9DEEC53E1EDDB0B352DBB4A54",
-        "sig": "E6BB279CBD4727B41F2AA8B18E99B3F99DECBB8737D284FFDD408B356C912EE21AD478BCC0ABD65246938F17DDE64258FD8A9684C0649B23AE1318F7B9CEEEC7"
+        "msg": "516D61527843366D62444B3546786F785757414C704D546A4B705662443938466A574A756D737469694439364A6D",
+        "pub": "039C9EF3BD38C8E677B2F40A998F8872ADC9D9D5A89CE2A21631310E2F200E43B3",
+        "sig": "F2827C10BBB1C065AB558828E134FD9FE5F080D63233344FEB7DA162EBF8EECA8BE6A099E584DA75E2EB4E6698F4E8491BB11657EFA33B21958B05D205C046C1",
+        "hash": "QmaRxC6mbDK5FxoxWWALpMTjKpVbD98FjWJumstiiD96Jm"
     },
     "status": 200
 }`}
@@ -112,28 +108,52 @@ Content-Type: application/json
                     <Row>
                       <Col md={12}>
                         <SyntaxHighlighter language="jsx" style={gruvboxDark}>
-                          {`import React from 'react';
-import ReactDOM from "react-dom";
+                          {`    api_key = request.headers.get('api_key')
+    api_status = validate_api_key(api_key)
+    if not api_status:
+      data = {"error message":"API Key could not be verified","status":401, "timestamp":getTime(),"path":request.url}
+      return Response(json.dumps(data), 
+        status=401,
+        mimetype='application/json'
+      )
 
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+    #reading the file content
+    request_file = request.files['file']
+    if not request_file:
+      data = {"error message":"No file attached","status":404, "timestamp":getTime(),"path":request.url}
+      return Response(json.dumps(data), 
+        status=404,
+        mimetype='application/json'
+      )
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/animate.min.css";
-import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
-import "./assets/css/demo.css";
-import "./assets/css/pe-icon-7-stroke.css";
+    file_contents = request_file.stream.read().decode("utf-8")
 
-import AdminLayout from "layouts/Admin.jsx";
+    #upload file to hive
+    api_url_base = settings.GMU_NET_IP_ADDRESS + settings.HIVE_PORT + settings.HIVE_ADD
+    headers = {'Content-Disposition': 'multipart/form-data;boundary=--------------------------608819652137318562927303'}
+    myResponse1 = requests.get(api_url_base, files={'file':file_contents}, headers=headers).json()
+    if not myResponse1:
+      data = {"error message":"File could not be uploaded","status":404, "timestamp":getTime(),"path":request.url}
+      return Response(json.dumps(data), 
+        status=404,
+        mimetype='application/json'
+      )
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);`}
+
+    #signing the hash key
+    private_key = request.headers.get('private_key')
+    api_url_base = settings.DID_SERVICE_URL + settings.DID_SERVICE_SIGN
+    headers = {'Content-type': 'application/json'}
+    req_data =  {
+                "privateKey":private_key,
+                "msg":myResponse1['Hash']
+            }
+    myResponse2 = requests.post(api_url_base, data=json.dumps(req_data), headers=headers).json()
+    myResponse2['result']['hash'] = myResponse1['Hash']
+    return Response(json.dumps(myResponse2), 
+        status=myResponse2['status'],
+        mimetype='application/json'
+      )`}
                         </SyntaxHighlighter>
                       </Col>
                     </Row>
