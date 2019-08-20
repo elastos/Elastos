c56d4b147ea68f90ac1fa05ff4915d52987ec8ef
Rahul Gunasekaran <rahulguna10@gmail.com>
2019-08-20 17:11:56 -0400

    updated the documentation and code 4

--- CR.Elastos-Nucleus/src/views/ServiceThree.jsx
+++ CR.Elastos-Nucleus/src/views/ServiceThree.jsx
@@ -175,31 +175,23 @@ class UserProfile extends Component {
                         <FormGroup controlId="formControlsTextarea">
                           <p>
                             <span className="category" />
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing
-                            elit, sed diem nonummy nibh euismod tincidunt ut
-                            lacreet dolore magna aliguam erat volutpat. Ut wisis
-                            enim ad minim veniam, quis nostrud exerci tution
-                            ullam corper suscipit lobortis nisi ut aliquip ex ea
-                            commodo consequat. Duis te feugi facilisi. Duis
-                            autem dolor in hendrerit in vulputate velit esse
-                            molestie consequat, vel illum dolore eu feugiat
-                            nulla facilisis at vero eros et accumsan et iusto
-                            odio dignissim qui blandit praesent luptatum zzril
-                            delenit au gue duis dolore te feugat nulla facilisi.
+                            Verify contents of the message using DID Public key
                           </p>
                         </FormGroup>
                         <SyntaxHighlighter
                           language="javascript"
                           style={gruvboxDark}
                         >
-                          {`POST /api/1/sign HTTP/1.1
-Host: localhost:8090
+                          {`POST /api/1/service/sidechain/did/verify HTTP/1.1
+Host: localhost:8888
 Content-Type: application/json
 
-  {
-      "privateKey":"0D5D7566CA36BC05CFF8E3287C43977DCBB492990EA1822643656D85B3CB0226",
-      "msg":"Hello World"
-  }`}
+{
+    "msg": "E4BDA0E5A5BDEFBC8CE4B896E7958D",
+    "pub": "02C3F59F337814C6715BBE684EC525B9A3CFCE55D9DEEC53E1EDDB0B352DBB4A54",
+    "sig": "E6BB279CBD4727B41F2AA8B18E99B3F99DECBB8737D284FFDD408B356C912EE21AD478BCC0ABD65246938F17DDE64258FD8A9684C0649B23AE1318F7B9CEEEC7"
+}
+`}
                         </SyntaxHighlighter>
                         <SyntaxHighlighter
                           language="javascript"
@@ -210,11 +202,7 @@ Vary: Accept
 Content-Type: application/json
 
 {
-    "result": {
-        "msg": "E4BDA0E5A5BDEFBC8CE4B896E7958C",
-        "pub": "02C3F59F337814C6715BBE684EC525B9A3CFCE55D9DEEC53E1EDDB0B352DBB4A54",
-        "sig": "E6BB279CBD4727B41F2AA8B18E99B3F99DECBB8737D284FFDD408B356C912EE21AD478BCC0ABD65246938F17DDE64258FD8A9684C0649B23AE1318F7B9CEEEC7"
-    },
+    "result": true,
     "status": 200
 }`}
                         </SyntaxHighlighter>
@@ -236,28 +224,24 @@ Content-Type: application/json
                     <Row>
                       <Col md={12}>
                         <SyntaxHighlighter language="jsx" style={gruvboxDark}>
-                          {`import React from 'react';
-import ReactDOM from "react-dom";
+                          {`    api_key = request.headers.get('api_key')
+        api_status = validate_api_key(api_key)
+        if not api_status:
+            data = {"error message":"API Key could not be verified","status":401, "timestamp":getTime(),"path":request.url}
+            return Response(json.dumps(data), 
+                status=401,
+                mimetype='application/json'
+            )
 
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/animate.min.css";
-import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
-import "./assets/css/demo.css";
-import "./assets/css/pe-icon-7-stroke.css";
-
-import AdminLayout from "layouts/Admin.jsx";
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);`}
+        api_url_base = settings.DID_SERVICE_URL + settings.DID_SERVICE_VERIFY
+        headers = {'Content-type': 'application/json'}
+        req_data = request.get_json()
+        myResponse = requests.post(api_url_base, data=json.dumps(req_data), headers=headers).json()
+        return Response(json.dumps(myResponse), 
+                status=myResponse['status'],
+                mimetype='application/json'
+            )
+                          `}
                         </SyntaxHighlighter>
                       </Col>
                     </Row>
