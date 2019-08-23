f810d45b70d1feb47dc7c62f3b9d1c250a7f333e
Ramuji <lingamaneni.r@gmail.com>
2019-08-23 12:24:54 -0400

    added iframe component

--- CR.Elastos-Nucleus/src/views/Documentation.jsx
+++ CR.Elastos-Nucleus/src/views/Documentation.jsx
@@ -1,58 +1,30 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, FormGroup } from "react-bootstrap";
+import Iframe from 'react-iframe'
+import {Col, Grid, Row} from "react-bootstrap";
 
-import { Card } from "components/Card/Card.jsx";
+class Documentation extends Component{
 
-class Dashboard extends Component {
-  createLegend(json) {
-    var legend = [];
-    for (var i = 0; i < json["names"].length; i++) {
-      var type = "fa fa-circle text-" + json["types"][i];
-      legend.push(<i className={type} key={i} />);
-      legend.push(" ");
-      legend.push(json["names"][i]);
-    }
-    return legend;
-  }
-  render() {
-    return (
-      <div className="content">
-        <Grid fluid>
+    render(){
+        return(
+        <div class="container-fluid">
+        <Grid >
           <Row>
             <Col md={12}>
-              <Card
-                title="Documentation"
-                content={
-                  <form>
-                    <Row>
-                      <Col md={12}>
-                        <FormGroup controlId="formControlsTextarea">
-                          <p>
-                            <span className="category" />
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing
-                            elit, sed diem nonummy nibh euismod tincidunt ut
-                            lacreet dolore magna aliguam erat volutpat. Ut wisis
-                            enim ad minim veniam, quis nostrud exerci tution
-                            ullam corper suscipit lobortis nisi ut aliquip ex ea
-                            commodo consequat. Duis te feugi facilisi. Duis
-                            autem dolor in hendrerit in vulputate velit esse
-                            molestie consequat, vel illum dolore eu feugiat
-                            nulla facilisis at vero eros et accumsan et iusto
-                            odio dignissim qui blandit praesent luptatum zzril
-                            delenit au gue duis dolore te feugat nulla facilisi.
-                          </p>
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                  </form>
-                }
-              />
+                <Iframe url="http://localhost:8888/api/"
+                width= "100%"
+                height="100%"
+                position="relative"
+                frameBorder={0}
+                />
+
             </Col>
           </Row>
         </Grid>
-      </div>
-    );
-  }
+        </div>
+        )
+
+    }
 }
 
-export default Dashboard;
+export default Documentation
+
