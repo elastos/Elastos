7d18443d19ec450b599a5d03311d337377a4bcf8
Benjamin Piette <benjamin@findizer.fr>
2019-08-30 11:59:04 +0800

    Split docs with docs_setup.sh to prevent docs.sh from asking password every time. Used by the dev site to automatically generate a doc

--- Carrier.Swift.SDK/docs.sh
+++ Carrier.Swift.SDK/docs.sh
@@ -5,7 +5,6 @@ if [[ $FIRST_ARGUMENT == "objc" ]]
 then
 appledoc --project-name ElastosCarrier --project-company "elastos.org" --company-id org.elastos --no-create-docset --output ./oc-docs/ .
 else
-sudo gem install jazzy
 jazzy --min-acl internal -o swift-docs
 fi
 
