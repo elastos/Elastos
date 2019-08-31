df07c303a82fab638fdec7e07f65905aa50b57e7
Billie Zhang <billiecoder@gmail.com>
2019-08-31 15:37:37 +0800

    remove unused code on suggestion create and edit pages

--- CR.CyberRepublic/front-end/src/module/form/SuggestionForm/Container.js
+++ CR.CyberRepublic/front-end/src/module/form/SuggestionForm/Container.js
@@ -3,27 +3,21 @@ import {
 } from '@/util'
 import Component from './Component'
 import SuggestionService from '@/service/SuggestionService'
-import CouncilService from '@/service/CouncilService'
 
 const mapState = state => ({
   loading: state.suggestion.loading,
-  councilMembers: state.council.council_members,
   language: state.language.language
 })
 
 const mapDispatch = () => {
   const suggestionService = new SuggestionService()
-  const councilService = new CouncilService()
   return {
     async create(param) {
       return suggestionService.create(param)
     },
     async update(param) {
       return suggestionService.update(param)
-    },
-    async getCouncilMembers() {
-      return councilService.getCouncilMembers()
-    },
+    }
   }
 }
 
