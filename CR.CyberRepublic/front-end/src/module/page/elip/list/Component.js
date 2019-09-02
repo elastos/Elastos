e83a86f928efda68c17e4c77c2698bc3bd2b4087
Billie Zhang <billiecoder@gmail.com>
2019-09-02 15:29:26 +0800

    trim elip search string

--- CR.CyberRepublic/front-end/src/module/page/elip/list/Component.js
+++ CR.CyberRepublic/front-end/src/module/page/elip/list/Component.js
@@ -64,7 +64,7 @@ export default class extends BaseComponent {
   }
 
   searchChangedHandler = (search) => {
-    this.setState({ search }, this.debouncedRefetch)
+    this.setState({ search: search.trim() }, this.debouncedRefetch)
   }
 
   setFilter = (filter) => {
