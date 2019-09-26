958358f8795aaf1c7c2fc8973162b53689437c66
Billie Zhang <billiecoder@gmail.com>
2019-09-26 11:39:32 +0800

    add SuggestionEditHistory schema

---
+++ CR.CyberRepublic/back-end/src/db/schema/SuggestionEditHistorySchema.ts
@@ -0,0 +1,64 @@
+import { Schema } from 'mongoose'
+import { constant } from '../../constant'
+import * as _ from 'lodash'
+
+const SuggestionCore = {
+  title: {
+    type: String
+  },
+
+  // old fields
+  shortDesc: {
+    type: String,
+    maxLength: 255
+  },
+  desc: {
+    type: String
+  },
+  benefits: {
+    type: String
+  },
+  funding: {
+    type: Number
+  },
+  timeline: {
+    type: Date
+  },
+  link: [String],
+  coverImg: String,
+  
+  // new fields
+  type: {
+    type: String,
+    enum: _.values(constant.SUGGESTION_TYPE)
+  },
+  abstract: {
+    type: String
+  },
+  goal: {
+    type: String
+  },
+  motivation: {
+    type: String
+  },
+  relevance: {
+    type: String
+  },
+  budget: {
+    type: String
+  },
+  plan: {
+    type: String
+  }
+}
+
+
+export const SuggestionEditHistory = {
+  ...SuggestionCore,
+  updatedAt: { type: Date },
+  suggestion: {
+    type: Schema.Types.ObjectId,
+    ref: 'suggestion',
+    required: true
+  }
+}
