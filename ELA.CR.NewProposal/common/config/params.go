a4467a73e28ac0b92be986688d35eff5f9561f45
jiangzehua <jiangzehua@elastos.org>
2019-09-27 11:08:55 +0800

    remove comment

--- ELA.CR.NewProposal/common/config/params.go
+++ ELA.CR.NewProposal/common/config/params.go
@@ -273,7 +273,7 @@ func (p *Params) RegNet() *Params {
 	copy.CRVotingStartHeight = 900000          // todo correct me when height has been confirmed
 	copy.CRCommitteeStartHeight = 1000000      // todo correct me when height has been confirmed
 	copy.EnableActivateIllegalHeight = 1000000 //todo correct me later
-	copy.CheckRewardHeight = 280000            //todo correct me later
+	copy.CheckRewardHeight = 280000
 	return &copy
 }
 
