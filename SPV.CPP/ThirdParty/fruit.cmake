514fb9c21111abe438fc30824799d7555eb4dc45
AlexPan <ppgod@live.cn>
2019-10-11 16:12:21 +0800

    Rename cmake variable Boost_LIBRATIES to SPV_Boost_LIBRATIES

--- SPV.CPP/ThirdParty/fruit.cmake
+++ SPV.CPP/ThirdParty/fruit.cmake
@@ -5,7 +5,7 @@ set(BOOST_DIR ${Boost_INCLUDE_DIRS} CACHE INTERNAL "boost dir")
 include_directories("${BOOST_DIR}")
 
 add_subdirectory(fruit EXCLUDE_FROM_ALL)
-add_dependencies(fruit ${Boost_LIBRARIES})
+add_dependencies(fruit ${SPV_Boost_LIBRARIES})
 set_target_properties(fruit PROPERTIES POSITION_INDEPENDENT_CODE ON)
 
 set(ThirdParty_FRUIT_INC_DIR
