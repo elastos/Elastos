2ca7c605f904a7618c5b78ba9e8a08d6f22df0fb
Admin <admin@Admins-MBP.fios-router.home>
2019-10-11 22:29:42 -0400

    Updated requirements.txt , updated gitignore file

--- CR.Elastos-Nucleus/HelloWorld/urls.py
+++ CR.Elastos-Nucleus/HelloWorld/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'hello', views.helloWorld, name = 'helloWorld' ),
+    path(r'hello', views.helloWorld, name = 'helloWorld'),
 ]
