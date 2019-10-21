1804b663a9b0814cfbe4d52613e9c6d68d6ffae5
liaihong <478701610@qq.com>
2019-10-21 10:57:04 +0800

    add DIDAntlr4ErrorListener

--- DID.Swift.SDK/ElastosDIDSDK/DID/DIDError.swift
+++ DID.Swift.SDK/ElastosDIDSDK/DID/DIDError.swift
@@ -2,12 +2,15 @@ import Foundation
 
 public enum DIDError: Error {
     case failue(_ des: String?)
+    case illegalArgument(_ des: String?)
 }
 
 extension DIDError {
     static func des(_ error: DIDError) -> String {
         switch error {
         case .failue(let err):
+            return err ?? "Operation failed"
+        case .illegalArgument(let err):
             return err ?? "Operation failed"
         }
     }
--- DID.Swift.SDK/ElastosDIDSDK/DID/Parser/ParserHelper.swift
+++ DID.Swift.SDK/ElastosDIDSDK/DID/Parser/ParserHelper.swift
@@ -1,11 +1,18 @@
 import Foundation
 import Antlr4
 
+
+class DIDAntlr4ErrorListener: BaseErrorListener {
+    
+    override func syntaxError<T>(_ recognizer: Recognizer<T>, _ offendingSymbol: AnyObject?, _ line: Int, _ charPositionInLine: Int, _ msg: String, _ e: AnyObject?) where T : ATNSimulator {
+        print("At position \(charPositionInLine) : \(msg), \(String(describing: e))")
+    }
+}
+
 class ParserHelper: NSObject {
     
     public static func parase(_ didurl: String, _ didOnly: Bool, _ listener: DIDURLBaseListener) throws {
-        // TODO: 自定义BaseErrorListener
-        let error: BaseErrorListener = BaseErrorListener()
+        let error: DIDAntlr4ErrorListener = DIDAntlr4ErrorListener()
         let input: ANTLRInputStream =  ANTLRInputStream(didurl)
         let lexer: DIDURLLexer = DIDURLLexer(input)
         lexer.removeErrorListeners()
