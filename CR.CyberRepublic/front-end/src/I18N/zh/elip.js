2726b539de0935644fab812b09764fcac6073e99
Andy Lian <andy.lodestar00@gmail.com>
2019-10-22 17:55:57 +0800

    update I18N

--- CR.CyberRepublic/front-end/src/I18N/zh/elip.js
+++ CR.CyberRepublic/front-end/src/I18N/zh/elip.js
@@ -1,5 +1,6 @@
 import {
   ELIP_STATUS,
+  ELIP_TYPE,
   ELIP_FILTER,
   ELIP_DESC_MAX_WORDS
 } from '@/constant'
@@ -14,6 +15,7 @@ export default {
     createdAt: '创建日期',
     description: '描述',
     elipType: '类型',
+    preamble: '前导',
     abstract: '摘要',
     specifications: '规范',
     motivation: '动机',
@@ -21,16 +23,16 @@ export default {
     backwardCompatibility: '向后兼容性',
     referenceImplementation: '参考实现',
     copyright: '版权/公共域',
-    review: 'Review',
-    vote: 'Council Member Votes',
+    review: '审查',
+    vote: '投票',
     preambleItems: {
       elip: 'ELIP',
-      title: 'Title',
-      author: 'Author',
+      title: '标题',
+      author: '作者',
       discussions: 'Discussions-To',
-      status: 'Status',
-      type: 'Type',
-      created: 'Created',
+      status: '状态',
+      type: '类型',
+      created: '创建日期',
       requires: 'Requires',
       replaces: 'Replaces',
       superseded: 'Superseded-By'
@@ -59,9 +61,9 @@ export default {
     markAsSubmitted: '标记为提交状态',
     markAsCancelled: '标记为取消状态',
     continue: '继续',
-    yes: 'Vote Yes',
-    oppose: 'Oppose with a Reason',
-    abstain: 'Abstain'
+    yes: '赞成',
+    oppose: '反对',
+    abstain: '弃权'
   },
   msg: {
     updated: '更新成功',
@@ -73,22 +75,24 @@ export default {
   form: {
     note: {
       elipType: '选择ELIP类型.',
-      abstract: 'Abstract',
-      specifications: 'Specifications',
-      motivation: 'Motivation',
-      rationale: 'Rationale',
-      backwardCompatibility: 'Backward Compatibility',
-      referenceImplementation: 'Reference Implementation',
-      copyright: 'Copyright/Public Domain',
-      review: 'Review'
+      abstract: '摘要',
+      specifications: '规范',
+      motivation: '动机',
+      rationale: '基本原理',
+      backwardCompatibility: '向后兼容性',
+      referenceImplementation: '参考实现',
+      copyright: '版权/公共域',
+      review: '审查'
     },
     type: {
-      standardTitle: 'Standards Tracking ELIP',
-      informationTitle: 'Information ELIP',
-      processTitle: 'Process ELIP',
-      standard: 'The standards tracking ELIP is divided into two parts: design and implementation. The design part should provide basic technical specifications and basic principles as well as implementation guidance. The implementation part is to track the status of the ELIP implementation.',
-      information: 'Describe the design issues of the Elastos, or provide general guidelines or information to the Elastos community, but does not propose new features. The informational ELIP does not necessarily represent the consensus or recommendation of the Elastos community, so users and implementers are free to ignore Informational ELIP or follow their advice.',
-      process: 'Describes a process surrounding Elastos, or proposes a change to a process. The process ELIP is similar to the Standards ELIP, but it is applicable to areas other than Elastos platform technology. Implementations may also be proposed in the ELIP, but should not involve changes to the Elastos platform codebase.<br />In general, the Process ELIP needs to get community consensus. Unlike the information ELIP, the Process ELIP is more than recommendation, and users are typically not free to ignore them. Examples include procedures, guidelines, changes to the decision-making process, and changes to the tools or environments used in Elastos development. ELIP-1 (this proposal) belongs to the process ELIP.',
+      [ELIP_TYPE.STANDARD_TRACK]: '标准跟踪ELIP分为设计和实现两个部分，设计部分应提供基本的技术规范和基本原理以及在实现上的指引，实现部分是对该ELIP实现状态的跟踪。',
+      [ELIP_TYPE.INFORMATIONAL]: '描述亦来云的设计问题，或者向亦来云社区提供一般性的指南或信息，但没有提出新的功能。信息ELIP不一定代表亦来云社区的共识或者建议，因此用户和实施者可以自由选择遵循或者忽视。',
+      [ELIP_TYPE.PROCESS]: '描述亦来云相关的流程定义或者对流程的改变，流程ELIP类似于标准ELIP，但适用于亦来云平台技术以外的领域。ELIP中也可能提出实施方案，但不应涉及亦来云平台代码库的变更。<br />一般而言，流程类ELIP需要得到社区的共识。与信息类ELIP不同，流程类ELIP不仅仅只是建议，用户通常也不能随意忽略它们。比如：包含规程、指南、决策过程的变化以及对亦来云开发中使用的工具或环境的更改。ELIP-1（本提案）即属于流程ELIP。',
+    },
+    typeTitle: {
+      [ELIP_TYPE.STANDARD_TRACK]: '标准跟踪 ELIP',
+      [ELIP_TYPE.INFORMATIONAL]: '信息 ELIP',
+      [ELIP_TYPE.PROCESS]: '流程 ELIP',
     },
     error: {
       required: '必填项',
