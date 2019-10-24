ebd05219759a3f92437144916efd68dad99abac9
Billie Zhang <billiecoder@gmail.com>
2019-10-24 10:36:53 +0800

    modify the style of suggestion sort options

--- CR.CyberRepublic/front-end/src/module/page/suggestion/list/Component.js
+++ CR.CyberRepublic/front-end/src/module/page/suggestion/list/Component.js
@@ -255,35 +255,19 @@ export default class extends StandardPage {
             }
           </h2>
         </div>
-        <MediaQuery maxWidth={LG_WIDTH}>
-          {I18N.get('suggestion.sort')}: &nbsp;
-          <Select
-            name="type"
-            style={{width: 200}}
-            onChange={this.onSortByChanged}
-            value={sortBy}
-          >
-            {_.map(SORT_BY, value => (
-              <Select.Option key={value} value={value}>
-                {SORT_BY_TEXT[value]}
-              </Select.Option>
-            ))}
-          </Select>
-        </MediaQuery>
-        <MediaQuery minWidth={LG_WIDTH + 1}>
-          {I18N.get('suggestion.sort')}: &nbsp;
-          <Button.Group className="filter-group">
-            {_.map(SORT_BY, value => (
-              <Button
-                key={value}
-                onClick={() => this.onSortByChanged(value)}
-                className={(sortBy === value && 'cr-strikethrough') || ''}
-              >
-                {SORT_BY_TEXT[value]}
-              </Button>
-            ))}
-          </Button.Group>
-        </MediaQuery>
+        {I18N.get('suggestion.sort')}: {' '}
+        <Select
+          name="type"
+          style={{width: 200}}
+          onChange={this.onSortByChanged}
+          value={sortBy}
+        >
+          {_.map(SORT_BY, value => (
+            <Select.Option key={value} value={value}>
+              {SORT_BY_TEXT[value]}
+            </Select.Option>
+          ))}
+        </Select>
       </div>
     )
   }
@@ -312,7 +296,6 @@ export default class extends StandardPage {
   }
 
   renderFilters() {
-
     const {
       tagsIncluded: {
         infoNeeded,
