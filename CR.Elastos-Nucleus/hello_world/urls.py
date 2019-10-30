18fb74d5cbec2d7418aaee9251a2684c49b82bdc
Admin <admin@Admins-MBP.byod.gmu.edu>
2019-10-30 13:57:03 -0400

    added generate key

--- CR.Elastos-Nucleus/hello_world/urls.py
+++ CR.Elastos-Nucleus/hello_world/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path(r'hello', views.hello_World, name = 'helloWorld'),
+    url(r'hello', views.hello_World, name = 'helloWorld'),
 ]
