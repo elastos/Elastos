386cb9b67a120c04beb278dac8727a3537e44b95
polaris hong <polarishongql@gmail.com>
2019-11-04 18:04:46 +0800

    cvote elip test unit

--- CR.CyberRepublic/back-end/test/unit/data/elip.js
+++ CR.CyberRepublic/back-end/test/unit/data/elip.js
@@ -1,14 +1,14 @@
 global.DB = Object.assign(global.DB, {
   ELIP_1: {
     title: 'Hello World',
-    description: 'description'
+    abstract: 'description'
   },
   ELIP_2: {
     title: 'test update title',
-    description: 'test update description'
+    abstract: 'test update description'
   },
   ELIP_3: {
     title: 'elip detail',
-    description: 'get elip detail'
+    abstract: 'get elip detail'
   }
 })
