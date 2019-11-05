8a1175c57f717c62a55ea15cbfbf7fd59360a48d
Andy Lian <andy.lodestar00@gmail.com>
2019-11-05 09:33:50 +0800

    Revert "cvote elip test unit"
    This reverts commit f63de526509e7bf926f78000ed1f97c2c5dcf719.

--- CR.CyberRepublic/back-end/test/unit/data/elip.js
+++ CR.CyberRepublic/back-end/test/unit/data/elip.js
@@ -1,14 +1,14 @@
 global.DB = Object.assign(global.DB, {
   ELIP_1: {
     title: 'Hello World',
-    abstract: 'description'
+    description: 'description'
   },
   ELIP_2: {
     title: 'test update title',
-    abstract: 'test update description'
+    description: 'test update description'
   },
   ELIP_3: {
     title: 'elip detail',
-    abstract: 'get elip detail'
+    description: 'get elip detail'
   }
 })
