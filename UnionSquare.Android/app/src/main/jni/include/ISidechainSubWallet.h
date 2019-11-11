104d307c5b95a42ac6b47dfcc43ddb811a262aca
poon <heroopan@gmail.com>
2019-11-11 14:02:13 +0800

    libspvsdk -> v0.4.2-73-gdd9d0b1b

--- UnionSquare.Android/app/src/main/jni/include/ISidechainSubWallet.h
+++ UnionSquare.Android/app/src/main/jni/include/ISidechainSubWallet.h
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "ISubWallet.h"
+#include <ISubWallet.h>
 
 #ifndef __ELASTOS_SDK_ISIDECHAINSUBWALLET_H__
 #define __ELASTOS_SDK_ISIDECHAINSUBWALLET_H__
--- UnionSquare.Android/app/src/main/jni/include/ITokenchainSubWallet.h
+++ UnionSquare.Android/app/src/main/jni/include/ITokenchainSubWallet.h
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "ISidechainSubWallet.h"
+#include <ISidechainSubWallet.h>
 
 #ifndef __ELASTOS_SDK_ITOKENCHAINSUBWALLET_H__
 #define __ELASTOS_SDK_ITOKENCHAINSUBWALLET_H__
