cd969b2c93e9b263b787bac6be397c26a0cee19e
Kiran Pachhai <kiran.pachhai@gmail.com>
2019-11-12 19:26:07 -0500

    Updated STATIC_ROOT

--- CR.Elastos-Nucleus/console_main/settings.py
+++ CR.Elastos-Nucleus/console_main/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
