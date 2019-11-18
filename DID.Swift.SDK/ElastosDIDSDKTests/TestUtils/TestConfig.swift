a075d601e99709e4c5812fde3390d3018d605449
liaihong <478701610@qq.com>
2019-11-18 10:25:17 +0800

    fix gitigonre

---
+++ DID.Swift.SDK/ElastosDIDSDKTests/TestUtils/TestConfig.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+let tempStoreRoot: String = "/PATH/TO/DIDStore.temp"
+
+let storePath: String = "\(NSHomeDirectory())/Library/Caches/DIDStore"
+let passphrase: String = "secret"
+let storePass: String = "passph"
+
+let walletDir: String = "/PATH/TO/WALLET"
+let walletId: String = "test"
+let networkConfig: String = "/PATH/TO/privnet.json"
+let resolver: String = "https://coreservices-didsidechain-privnet.elastos.org"
---
+++ DID.Swift.SDK/Externals/libhdkey/include/spvadapter.h
@@ -0,0 +1,26 @@
+#ifndef __SPV_ADAPTER_H__
+#define __SPV_ADAPTER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct SpvDidAdapter SpvDidAdapter;
+
+SpvDidAdapter *SpvDidAdapter_Create(const char *walletDir, const char *walletId,
+        const char *network, const char *resolver);
+
+void SpvDidAdapter_Destroy(SpvDidAdapter *adapter);
+
+int SpvDidAdapter_CreateIdTransaction(SpvDidAdapter *adapter,
+        const char *payload, const char *memo, const char *password);
+
+const char *SpvDidAdapter_Resolve(SpvDidAdapter *adapter, const char *did);
+
+void SpvDidAdapter_FreeMemory(SpvDidAdapter *adapter, void *mem);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SPV_ADAPTER_H__ */
