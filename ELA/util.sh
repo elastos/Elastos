58f0b24c6223e5bf6be89131617eceb2c0463b3e
Yan Mingzhi <yanmingzhi@elastos.org>
2019-11-22 17:16:19 +0800

    add unit test and benchmark in utils.sh

--- ELA/util.sh
+++ ELA/util.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
 function cleanDirectoryRecursively() {
-	find . -name $1 | sed 's#^#rm -fr #g' | sh
+  find . -name "$1" | sed 's#^#rm -fr #g' | sh
 }
 
 function cleanTempData() {
-	cleanDirectoryRecursively "config.json"
-	cleanDirectoryRecursively "peers.json"
-	cleanDirectoryRecursively "elastos_test"
+  cleanDirectoryRecursively "config.json"
+  cleanDirectoryRecursively "peers.json"
+  cleanDirectoryRecursively "elastos_test"
 }
 
-if [[ "$1" = "clean" ]]; then
-    cleanTempData
-elif [[ "$1" = "cleanall" ]]; then
-    cleanTempData
-	cleanDirectoryRecursively "elastos"
-elif [[ "$1" = "test" ]]; then
-	./ela-cli script -f test/white_box/main/test_all.lua
+cpuArgs=''
+if [[ "$2" == "cpu" || "$3" == "cpu" ]]; then
+  cpuArgs='-cpuprofile profile.out'
+fi
+memArgs=''
+if [[ "$2" == "mem" || "$3" == "mem" ]]; then
+  memArgs='-benchmem -memprofile memprofile.out'
+fi
+
+if [[ "$1" == "clean" ]]; then
+  cleanTempData
+elif [[ "$1" == "cleanall" ]]; then
+  cleanTempData
+  cleanDirectoryRecursively "elastos"
+elif [[ "$1" == "test" ]]; then
+  ./ela-cli script -f test/white_box/main/test_all.lua
+elif [[ "$1" == "unittest" ]]; then
+  go test ./... -short
+elif [[ "$1" == "benchall" ]]; then
+  go test ./benchmark/... -bench=. $cpuArgs $memArgs
+elif [[ "$1" == "benchspec" ]]; then
+  go test ./benchmark/special/... -bench=. $cpuArgs $memArgs
+elif [[ "$1" == "benchproc" ]]; then
+  go test ./benchmark/process/... -bench=. $cpuArgs $memArgs
 fi
