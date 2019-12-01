f51ee8814ec39d0215148a5349f8ddcf950e8d9a
Rahul Gunasekaran <rahulguna10@gmail.com>
2019-12-01 14:39:30 -0500

    Insert details to user table

--- CR.Elastos-SmartWeb-Service/grpc_adenine/implementations/common.py
+++ CR.Elastos-SmartWeb-Service/grpc_adenine/implementations/common.py
@@ -2,11 +2,15 @@ import random
 import string
 import os
 import grpc
+import datetime
 from decouple import config
-
+from sqlalchemy import create_engine
 from grpc_adenine.database import (connection as db)
+from grpc_adenine.database.user import Users
+from grpc_adenine.implementations.utilities import get_time
 from grpc_adenine.stubs import common_pb2
 from grpc_adenine.stubs import common_pb2_grpc
+from sqlalchemy.orm import sessionmaker
 
 class Common(common_pb2_grpc.CommonServicer):
 
@@ -14,6 +18,24 @@ class Common(common_pb2_grpc.CommonServicer):
 		stringLength = 32
 		secret_key = config('SHARED_SECRET_ADENINE')
 		
+		DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
+		engine = create_engine(DATABASE_URI)
+		Session = sessionmaker(bind=engine)
+		date_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S %z")
+
+		user = Users(
+	    	did = request.did,
+	    	name = 'Elastos User',
+	    	email = 'elastos_user@elastos.org',
+	    	created_on = date_now,
+	    	last_logged_on = date_now
+    	)
+		s = Session()
+		s.add(user)
+		s.commit()
+		#print("ID value ",s.id)
+		s.close()
+
 		if(secret_key==request.secret_key):
 			api_key = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(stringLength))
 			return common_pb2.Response(api_key=api_key, status_message='Success', status=True)
--- CR.Elastos-SmartWeb-Service/grpc_adenine/implementations/utilities.py
+++ CR.Elastos-SmartWeb-Service/grpc_adenine/implementations/utilities.py
@@ -8,5 +8,5 @@ def validate_api_key(api_key):
 	result = db.query(exists().where(UserApiRelations.api_key == api_key)).scalar()
 	return result
 
-def getTime():
+def get_time():
 	return datetime.datetime.now(pytz.timezone('America/New_York')).strftime("%Y-%m-%d %H:%M:%S %z")
