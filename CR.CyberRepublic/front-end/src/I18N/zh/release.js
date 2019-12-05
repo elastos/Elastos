ac5bfa1ef172653f7fc9fda142f0f86f38be4e05
Billie Zhang <billiecoder@gmail.com>
2019-12-05 19:14:31 +0800

    I18N some text on release page

--- CR.CyberRepublic/front-end/src/I18N/zh/release.js
+++ CR.CyberRepublic/front-end/src/I18N/zh/release.js
@@ -3,14 +3,14 @@ export default {
   whatsNew: '版本更新',
   subtitle: 'CR网站的版本更新',
   btn: {
-    add: '添加Release',
-    edit: '编辑',
+    add: '添加版本',
+    edit: '编辑'
   },
   form: {
-    add: 'Add a Release',
-    edit: 'Edit Release',
+    add: '添加版本',
+    edit: '编辑版本'
   },
   modal: {
-    delete: '你确定要删除当前版本更新?',
-  },
+    delete: '你确定要删除当前版本更新?'
+  }
 }
