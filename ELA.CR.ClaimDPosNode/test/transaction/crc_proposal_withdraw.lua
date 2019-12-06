964202d44b3ea24f51f3eac20b12981a1c1b554c
houpei <houshoupei@126.com>
2019-12-06 17:23:34 +0800

    crc_proposal_withdraw.lua script completed

--- ELA.CR.ClaimDPosNode/test/transaction/crc_proposal_withdraw.lua
+++ ELA.CR.ClaimDPosNode/test/transaction/crc_proposal_withdraw.lua
@@ -5,8 +5,6 @@
 
 local m = require("api")
 
---local tx_util = dofile("common.lua")
---local wallet = tx_util.wallet
 local keystore = getWallet()
 local password = getPassword()
 
@@ -18,62 +16,43 @@ if password == "" then
 end
 
 local wallet = client.new(keystore, password, false)
--- client: path, password, if create
---local wallet = client.new("keystore.dat", "123", false)
 
 -- account
 local addr = wallet:get_address()
-local SponsorPublicKey = wallet:get_publickey()
-print(addr)
-print(SponsorPublicKey)
-
+local crcCommitteeAddress = getCRCCommitteeAddress()
 -- asset_id
 local asset_id = m.get_asset_id()
-
--- amount, fee
--- local amount = 5000
---local fee = 0.001
-
--- deposit params
---local cr_publickey = getPublicKey()
-
-proposal_hash = getProposalHash()
---vote_result = getVoteResult()
---code = getCode()
-
+local proposal_hash = getProposalHash()
+local amount = getAmount()
 local fee = getFee()
 local stage = getStage()
 local recipient = getToAddr()
 
---sign = getSign()
+print("crcCommitteeAddress " .. crcCommitteeAddress)
+print("amount " .. amount)
+print("fee " .. fee)
+print("stage " .. stage)
+print("addr " .. addr)
+print("recipient " .. recipient)
 
-print(proposal_hash)
-print(fee)
-print(stage)
 
 
-local crcproposalwithdraw_payload =crcproposalwithdraw.new(proposal_hash,
-        SponsorPublicKey, stage, fee , wallet)
---print(crcproposalreview_payload:get())
-
+local crcproposalwithdraw_payload =crcproposalwithdraw.new(proposal_hash, stage, fee* 100000000 , wallet)
 -- transaction: version, txType, payloadVersion, payload, locktime
-local tx = transaction.new(9, 0x28, 0, crcproposalwithdraw_payload, 0)
+local tx = transaction.new(9, 0x29, 0, crcproposalwithdraw_payload, 0)
 print(tx:get())
 
--- input: from, amount + fee
-local charge = tx:appendenough(addr, fee * 100000000)
+local charge = tx:appendenough(crcCommitteeAddress, amount * 100000000)
 print(charge)
 
 -- outputpayload
 local default_output = defaultoutput.new()
+local amount_output = output.new(asset_id, amount * 100000000-fee*
+        100000000, recipient, 0, default_output)
+tx:appendtxout(amount_output)
 
--- output: asset_id, value, recipient, output_paload_type, outputpaload
-local charge_output = output.new(asset_id, charge, recipient, 0, default_output)
+local charge_output = output.new(asset_id, charge, crcCommitteeAddress, 0, default_output)
 tx:appendtxout(charge_output)
-
-
--- sign
-tx:sign(wallet)
 print(tx:get())
 
 -- send
