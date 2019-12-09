968c7341ecac37af0730a45c2b9deadffb558b63
Kiran Pachhai <kiran.pachhai@gmail.com>
2019-12-09 13:49:54 -0500

    Getting ready for pip

--- CR.Python-GRPC-Adenine/setup.py
+++ CR.Python-GRPC-Adenine/setup.py
@@ -5,11 +5,11 @@ setup(
   packages = ['elastos_adenine'],   # Chose the same as "name"
   version = '0.1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  description = 'gRPC Client library to interact with Elastos Smartweb Service',   # Give a short description about your library
   author = 'Kiran Pachhai',                   # Type in your name
   author_email = 'kiran@tuum.tech',      # Type in your E-Mail
   url = 'https://github.com/cyber-republic/python-grpc-adenine',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/cyber-republic/python-grpc-adenine/archive/v0.1.0.tar.gz',    # I explain this later on
   keywords = ['elastos', 'smartweb-service', 'adenine'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'grpcio',
