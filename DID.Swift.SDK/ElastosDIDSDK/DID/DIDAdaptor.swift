58abf0a928aa87e7c81cdf932d9ca1d4e15ccfa8
stiartsly <stiartsly@gmail.com>
2019-12-10 21:43:09 +0800

    WIP: improvoment update

--- DID.Swift.SDK/ElastosDIDSDK/DID/DIDAdaptor.swift
+++ DID.Swift.SDK/ElastosDIDSDK/DID/DIDAdaptor.swift
@@ -1,11 +1,6 @@
-
-
 import Foundation
 
 public protocol DIDAdaptor {
-    
      func createIdTransaction(_ payload: String, _ memo: String?) throws -> Bool
-    
      func resolve(_ did: String) throws -> String?
-    
 }
