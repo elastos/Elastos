179edc15c6ec1659f2dabd3b82f2475ec3355b93
Kiran Pachhai <kiran.pachhai@gmail.com>
2019-12-11 19:47:54 -0500

    Updated the version number

--- CR.Python-GRPC-Adenine/setup.py
+++ CR.Python-GRPC-Adenine/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="elastos_adenine",
-    version="0.1.3",
+    version="0.1.4",
     license='MIT',
     author="Kiran Pachhai",
     author_email="kiran@tuum.tech",
