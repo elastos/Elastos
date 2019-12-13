4def0bd0d527a1e747d630e3c7f576cf7b37ec0c
Kiran Pachhai <kiran.pachhai@gmail.com>
2019-12-12 19:30:22 -0500

    Added new wallet service and updated ela node to v0.4.0

--- CR.Develap/cmd/blockchain/common.go
+++ CR.Develap/cmd/blockchain/common.go
@@ -65,7 +65,7 @@ var (
 	NetworkName    = "develap_localnet_network"
 	NodeDockerPath = map[string]DockerPath{
 		"mainchain": {
-			ImageName:    "cyberrepublic/elastos-mainchain-node:v0.3.9",
+			ImageName:    "cyberrepublic/elastos-mainchain-node:v0.4.0",
 			DataPath:     "/ela/elastos",
 			KeystorePath: "/ela/keystore.dat",
 			ConfigPath:   "/ela/config.json",
