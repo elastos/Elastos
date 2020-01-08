168d8d9f0a51d3979c7e59bbc40f793ea35d7fe8
polaris <polaris@polarisdeiMac.local>
2020-01-08 14:34:28 +0800

    suggestion savedraft

--- CR.CyberRepublic/back-end/src/db/Base.ts
+++ CR.CyberRepublic/back-end/src/db/Base.ts
@@ -111,6 +111,9 @@ export default abstract class {
         return await this.db.remove(query)
     }
 
+  public async findAndRemove(query) {
+    return await this.db.findAndRemove(query)
+  }
 
     private buildUpdateOptions(opts?: updateOptions): updateOptions{
         return _.extend({
