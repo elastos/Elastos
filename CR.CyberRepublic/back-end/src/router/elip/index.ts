eaf55249004a2863c8325559bfd6c6bb19387ee4
Billie Zhang <billiecoder@gmail.com>
2020-01-10 11:21:29 +0800

    remove elip vote and propose routes

--- CR.CyberRepublic/back-end/src/router/elip/index.ts
+++ CR.CyberRepublic/back-end/src/router/elip/index.ts
@@ -5,9 +5,7 @@ import update from './update'
 import list from './list'
 import comment from './comment'
 import comment_remove from './comment_remove'
-import vote from './vote'
 import remove from './remove'
-import propose from './propose'
 
 export default Base.setRouter([
   {
@@ -29,16 +27,6 @@ export default Base.setRouter([
     path: '/:id/remove/',
     router: remove,
     method: 'get'
-  },
-  {
-    path: '/:id/propose/',
-    router: propose,
-    method: 'get'
-  },
-  {
-    path: '/vote',
-    router: vote,
-    method: 'post'
   },
   {
     path: '/list',
