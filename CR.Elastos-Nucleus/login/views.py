d155a7202d35a20a309164fa4e35a94887227dcb
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-01-10 12:13:49 -0500

    Updated to redirect to landing page when going to sign_in page directly if already logged in

--- CR.Elastos-Nucleus/login/views.py
+++ CR.Elastos-Nucleus/login/views.py
@@ -190,6 +190,8 @@ def activate(request, uidb64, token):
 
 
 def sign_in(request):
+    if request.session.get('logged_in'):
+        return redirect(reverse('landing'))
     public_key = config('ELA_PUBLIC_KEY')
     did = config('ELA_DID')
     app_id = config('ELA_APP_ID')
@@ -231,7 +233,8 @@ def feed(request):
     recent_services = get_recent_services(did)
     recent_pages = TrackUserPageVisits.objects.filter(did=did).order_by('-last_visited')[:5]
     most_visited_pages = TrackUserPageVisits.objects.filter(did=did).order_by('-number_visits')[:5]
-    return render(request, 'login/feed.html', {'recent_pages': recent_pages, 'recent_services': recent_services, 'most_visited_pages': most_visited_pages})
+    return render(request, 'login/feed.html', {'recent_pages': recent_pages, 'recent_services': recent_services,
+                                               'most_visited_pages': most_visited_pages})
 
 
 def sign_out(request):
