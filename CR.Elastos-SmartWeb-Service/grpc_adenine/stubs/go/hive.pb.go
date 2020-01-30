289dfd4620bbb8ee4a10ddc5db011c8eaafafc0a
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-01-30 15:20:40 -0500

    Fixing issue with hive go

--- CR.Elastos-SmartWeb-Service/grpc_adenine/stubs/go/hive.pb.go
+++ CR.Elastos-SmartWeb-Service/grpc_adenine/stubs/go/hive.pb.go
@@ -181,11 +181,11 @@ var fileDescriptor_d635d1ead41ba02c = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConnInterface
+var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
+const _ = grpc.SupportPackageIsVersion4
 
 // HiveClient is the client API for Hive service.
 //
@@ -196,10 +196,10 @@ type HiveClient interface {
 }
 
 type hiveClient struct {
-	cc grpc.ClientConnInterface
+	cc *grpc.ClientConn
 }
 
-func NewHiveClient(cc grpc.ClientConnInterface) HiveClient {
+func NewHiveClient(cc *grpc.ClientConn) HiveClient {
 	return &hiveClient{cc}
 }
 
