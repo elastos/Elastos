633dc4164221f8a4323974d0688bb7b46d500b46
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-02-01 20:36:16 -0500

    Updated dapp store dashboard

--- CR.Elastos-Nucleus/elastos_trinity/views.py
+++ CR.Elastos-Nucleus/elastos_trinity/views.py
@@ -11,6 +11,8 @@ def dapp_store_dashboard(request):
     track_page_visit(did, 'elastOS dApp Store Dashboard', 'elastos_trinity:dapp_store_dashboard', False)
     context['recent_services'] = get_recent_services(did)
     dapp_store = DAppStore()
-    context['dapps_list'] = dapp_store.get_apps_list()
+    dapps_list = dapp_store.get_apps_list()
+    for dapp in dapps_list:
+        dapp["id"] = dapp["_id"]
+    context['dapps_list'] = dapps_list
     return render(request, "elastos_trinity/dapp_store_dashboard.html", context)
-
