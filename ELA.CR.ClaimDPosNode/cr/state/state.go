d134777b8be37ba7da917b5a74898828661c945b
jiangzehua <jiangzehua@elastos.org>
2020-02-11 11:44:40 +0800

    fixed CR related transaction checker error

--- ELA.CR.ClaimDPosNode/cr/state/state.go
+++ ELA.CR.ClaimDPosNode/cr/state/state.go
@@ -33,7 +33,7 @@ type State struct {
 	StateKeyFrame
 	manager *ProposalManager
 
-	getHistoryMember func(code []byte) *CRMember
+	getHistoryMember func(code []byte) []*CRMember
 	getTxReference   func(tx *types.Transaction) (
 		map[*types.Input]*types.Output, error)
 
@@ -47,7 +47,7 @@ func (s *State) SetManager(manager *ProposalManager) {
 }
 
 type FunctionsConfig struct {
-	GetHistoryMember func(code []byte) *CRMember
+	GetHistoryMember func(code []byte) []*CRMember
 	GetTxReference   func(tx *types.Transaction) (
 		map[*types.Input]*types.Output, error)
 }
@@ -337,9 +337,9 @@ func (s *State) returnDeposit(tx *types.Transaction, height uint32) {
 				}
 			}
 		}
-		if s.getHistoryMember != nil {
-			if member := s.getHistoryMember(program.Code); member != nil {
-				returnMemberAction(member, member.MemberState)
+		if members := s.getHistoryMember(program.Code); len(members) != 0 {
+			for _, m := range members {
+				returnMemberAction(m, m.MemberState)
 			}
 		}
 
