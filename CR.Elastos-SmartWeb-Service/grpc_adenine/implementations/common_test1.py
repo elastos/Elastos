9e4d64648845a8f3f7399c876095a6b06df049e1
Rahul Gunasekaran <rahulguna10@gmail.com>
2020-02-17 00:06:41 -0500

    unit test case

---
+++ CR.Elastos-SmartWeb-Service/grpc_adenine/implementations/common_test1.py
@@ -0,0 +1,90 @@
+import pytest
+import sys
+from decouple import config
+import grpc
+from concurrent import futures
+from contextlib import contextmanager
+import unittest
+
+from grpc_adenine.stubs.python import common_pb2, common_pb2_grpc
+from grpc_adenine.implementations.common import get_api_key, generate_api_key
+from grpc_adenine.implementations.rate_limiter import RateLimiter
+
+import codecs
+from codecs import open
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+secret_key = config('SHARED_SECRET_ADENINE')
+
+network = "gmunet"
+mnemonic_to_use = 'obtain pill nest sample caution stone candy habit silk husband give net'
+did_to_use = 'n84dqvIK9O0LIPXi27uL0aRnoR45Exdxl218eQyPDD4lW8RPov'
+api_key_to_use = ''
+private_key_to_use = '1F54BCD5592709B695E85F83EBDA515971723AFF56B32E175F14A158D5AC0D99'
+
+# Connect to the database
+db_name = 'test'
+db_user = 'gmu'
+db_password = 'gmu'
+db_host = 'localhost'
+db_port = '5434'
+
+database_uri = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+create_scripts = "/Users/rahulguna/GIT/elastos-smartweb-service/grpc_adenine/database/scripts/create_table_scripts.sql"
+reset_scripts = "/Users/rahulguna/GIT/elastos-smartweb-service/grpc_adenine/database/scripts/reset_database.sql"
+
+def create_database_tables():
+	# Connect to the PostgreSQL database and create tables
+	postgresConnection = psycopg2.connect(database_uri)
+	cursor = postgresConnection.cursor()
+
+	sql = open(create_scripts, mode='r', encoding='utf-8-sig').read()
+	cursor.execute(sql)
+	postgresConnection.commit()
+	cursor.close()
+	postgresConnection.close()
+
+
+def reset_database_tables():
+	#reset database tables
+	postgresConnection = psycopg2.connect(database_uri)
+	cursor = postgresConnection.cursor()
+
+	sql = open(reset_scripts, mode='r', encoding='utf-8-sig').read()
+	cursor.execute(sql)
+	postgresConnection.commit()
+	cursor.close()
+	postgresConnection.close()
+
+
+class RunTest(unittest.TestCase):
+	def setUp(self):
+		create_database_tables()
+		db_engine = create_engine(database_uri)
+		session_maker = sessionmaker(bind=db_engine)
+		self.session = session_maker()
+		self.rate_limiter = RateLimiter()
+
+	#testing Generate API Key with valid DID
+	def test_generate_api_key(self):
+		response = generate_api_key(self.session, self.rate_limiter, did_to_use)
+		self.assertEqual(response.status, True)
+
+	#testing Get API Key with valid DID
+	def test_get_api_key(self):
+		generate_api_key(self.session, self.rate_limiter, did_to_use)
+		response = get_api_key(self.session, did_to_use)
+		self.assertEqual(response.status, True)
+
+	def tearDown(self):
+		self.session.close()
+		reset_database_tables()
+
+		
+
+
+	
+
