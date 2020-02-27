25c138a38b88ba016490d70c83c35d60ba96fc3c
Xiaobo Tian <peterwillcn@gmail.com>
2020-02-27 20:11:00 +0800

    Update license date
    Signed-off-by: Xiaobo Tian <peterwillcn@gmail.com>

--- ELA/blockchain/indexers/manager.go
+++ ELA/blockchain/indexers/manager.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2017-2020 The Elastos Foundation
+// Use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
+// 
+
 // Copyright (c) 2016 The btcsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
--- ELA/dpos/log/log.go
+++ ELA/dpos/log/log.go
@@ -1,7 +1,7 @@
-// Copyright (c) 2017-2019 The Elastos Foundation
+// Copyright (c) 2017-2020 The Elastos Foundation
 // Use of this source code is governed by an MIT
 // license that can be found in the LICENSE file.
-//
+// 
 
 package log
 
