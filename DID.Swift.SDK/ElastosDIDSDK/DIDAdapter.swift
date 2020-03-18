ddc2119d526043e58d84c02b77ed0a450e51cbf5
stiartsly <stiartsly@gmail.com>
2020-03-18 15:34:27 +0800

    Added Logger for logging running output

--- DID.Swift.SDK/ElastosDIDSDK/DIDAdapter.swift
+++ DID.Swift.SDK/ElastosDIDSDK/DIDAdapter.swift
@@ -1,8 +1,10 @@
 import Foundation
 
 public protocol DIDAdapter {
+    typealias TransactionCallback = (String, Int, String?) -> Void
+
     func createIdTransaction(_ payload: String,
                              _ memo: String?,
                              _ confirms: Int,
-                             _ callback: (_ transctionId: String, _ status: Int, _ message: String?) -> Void )
+                             _ callback: TransactionCallback)
 }
