a60a8b42af9f41d4340fe3f52a573bb7a2bb8da2
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-03-23 21:40:09 -0400

    Upping the version to 1.1.0

--- CR.Python-GRPC-Adenine/setup.py
+++ CR.Python-GRPC-Adenine/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="elastos_adenine",
-    version="1.0.6",
+    version="1.1.0",
     license='MIT',
     author="Kiran Pachhai",
     author_email="kiran@tuum.tech",
