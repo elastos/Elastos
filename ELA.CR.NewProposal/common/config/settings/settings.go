6b08e3853f7789ad32db7f545712a8c9f5b74993
Xiaobo Tian <peterwillcn@gmail.com>
2020-03-30 18:54:35 +0800

    Fixed configuration files MaxCommitteeProposalCount position
    Signed-off-by: Xiaobo Tian <peterwillcn@gmail.com>

--- ELA.CR.NewProposal/common/config/settings/settings.go
+++ ELA.CR.NewProposal/common/config/settings/settings.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2017-2020 The Elastos Foundation
 // Use of this source code is governed by an MIT
 // license that can be found in the LICENSE file.
-// 
+//
 
 package settings
 
@@ -501,7 +501,7 @@ func NewSettings() *Settings {
 	result.Add(&settingItem{
 		Flag:         cmdcom.MaxCommitteeProposalCount,
 		DefaultValue: uint32(0),
-		ConfigPath:   "MaxCommitteeProposalCount",
+		ConfigPath:   "CRConfiguration.MaxCommitteeProposalCount",
 		ParamName:    "MaxCommitteeProposalCount"})
 
 	result.Add(&settingItem{
