4c1a22264ca7353deb35e98f401daafb11aba279
Carlos Verastegui <51972213+carlosverastegui@users.noreply.github.com>
2020-04-07 15:50:40 -0400

    Fixed dApp More Button (#201)
    * Fixed More Tab
    
    * Delete privkey.pem
    
    * Delete server.key
    
    * Delete server.req
    
    * Added Random Name Generator
    
    * Updated Naming Convention
    
    * Removed Extra Modal File

--- CR.Elastos-Nucleus/elastos_trinity/views.py
+++ CR.Elastos-Nucleus/elastos_trinity/views.py
@@ -1,3 +1,4 @@
+import namegenerator
 from decouple import config
 from django.shortcuts import render
 
@@ -18,11 +19,15 @@ def dapp_store_dashboard(request):
     dapp_store_url = config('ELASTOS_TRINITY_DAPPSTORE_URL')
     for dapp in dapps_list:
         createdAt = dapp["createdAt"][:10]
+        strippedName = dapp["appName"].replace(" ", "")
+        removedName = ''.join([i for i in strippedName if not i.isdigit()])
+        uniqueName = removedName + namegenerator.gen()
+        dapp["uniqueName"] = uniqueName
         dapp["createdAt"] = createdAt
         dapp["id"] = dapp["_id"]
         dapp["icon_url"] = f"{dapp_store_url}/apps/{dapp['id']}/icon"
         dapp["download_url"] = f"https://scheme.elastos.org/app?id={dapp['packageName']}"
     context['dapps_list'] = dapps_list
-    context['top_downloads'] = sorted(dapps_list, key=lambda k: k['downloadsCount'], reverse=True)[:4]
+    context['top_downloads'] = sorted(
+        dapps_list, key=lambda k: k['downloadsCount'], reverse=True)[:4]
     return render(request, "elastos_trinity/dapp_store_dashboard.html", context)
-
