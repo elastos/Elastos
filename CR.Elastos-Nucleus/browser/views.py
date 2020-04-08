2999c153a877ae4cb223ebe5cdaadc7c2d3ef5bf
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-04-08 12:56:30 -0400

    Updated to use latest elastos-adenine 2.0.1 version everywhere that utilizes JWT

--- CR.Elastos-Nucleus/browser/views.py
+++ CR.Elastos-Nucleus/browser/views.py
@@ -13,6 +13,7 @@ from .forms import SelectNetworkForm
 
 @login_required
 def mainchain(request):
+    api_key = request.session['api_key']
     did = request.session['did']
     track_page_visit(did, 'Mainchain Browser', 'browser:mainchain', False)
     recent_services = get_recent_services(did)
@@ -26,13 +27,13 @@ def mainchain(request):
             network = form.cleaned_data.get('network')
             context['form'] = SelectNetworkForm(initial={'network': network})
             context['network'] = network
-            data = get_blockchain_node_data(request, network, "mainchain", did)
+            data = get_blockchain_node_data(request, api_key, did, network, "mainchain")
             for k, v in data.items():
                 context[k] = v
             return render(request, "browser/mainchain.html", context)
     else:
         network = 'gmunet'
-        data = get_blockchain_node_data(request, network, "mainchain", did)
+        data = get_blockchain_node_data(request, api_key, did, network, "mainchain")
         for k, v in data.items():
             context[k] = v
         form = SelectNetworkForm(initial={'network': network})
@@ -42,6 +43,7 @@ def mainchain(request):
 
 @login_required
 def sidechain_did(request):
+    api_key = request.session['api_key']
     did = request.session['did']
     track_page_visit(did, 'DID Sidechain Browser', 'browser:sidechain_did', False)
     recent_services = get_recent_services(did)
@@ -55,13 +57,13 @@ def sidechain_did(request):
             network = form.cleaned_data.get('network')
             context['form'] = SelectNetworkForm(initial={'network': network})
             context['network'] = network
-            data = get_blockchain_node_data(request, network, "did", did)
+            data = get_blockchain_node_data(request, api_key, did, network, "did")
             for k, v in data.items():
                 context[k] = v
             return render(request, "browser/sidechain_did.html", context)
     else:
         network = 'gmunet'
-        data = get_blockchain_node_data(request, network, "did", did)
+        data = get_blockchain_node_data(request, api_key, did, network, "did")
         for k, v in data.items():
             context[k] = v
         form = SelectNetworkForm(initial={'network': network})
@@ -71,6 +73,7 @@ def sidechain_did(request):
 
 @login_required
 def sidechain_token(request):
+    api_key = request.session['api_key']
     did = request.session['did']
     track_page_visit(did, 'Token Sidechain Browser', 'browser:sidechain_token', False)
     recent_services = get_recent_services(did)
@@ -84,13 +87,13 @@ def sidechain_token(request):
             network = form.cleaned_data.get('network')
             context['form'] = SelectNetworkForm(initial={'network': network})
             context['network'] = network
-            data = get_blockchain_node_data(request, network, "token", did)
+            data = get_blockchain_node_data(request, api_key, did, network, "token")
             for k, v in data.items():
                 context[k] = v
             return render(request, "browser/sidechain_token.html", context)
     else:
         network = 'gmunet'
-        data = get_blockchain_node_data(request, network, "token", did)
+        data = get_blockchain_node_data(request, api_key, did, network, "token")
         for k, v in data.items():
             context[k] = v
         form = SelectNetworkForm(initial={'network': network})
@@ -134,20 +137,21 @@ def sidechain_neo(request):
     return render(request, "browser/sidechain_neo.html", context)
 
 
-def get_blockchain_node_data(request, network, chain, did):
+def get_blockchain_node_data(request, api_key, did, network, chain):
     data = {}
     try:
         node_rpc = NodeRpc(GRPC_SERVER_HOST, GRPC_SERVER_PORT, PRODUCTION)
-        data["current_height"] = node_rpc.get_current_height(network, chain)
-        data["current_block_details"] = node_rpc.get_current_block_details(network, chain)
+        data["current_height"] = node_rpc.get_current_height(api_key, did, network, chain)
+        data["current_block_info"] = node_rpc.get_current_block_info(api_key, did, network, chain)
         if chain == "mainchain":
-            data["current_mining_info"] = node_rpc.get_current_mining_info(network)
-            data["current_block_confirm"] = node_rpc.get_current_block_confirm(network)
-            data["current_arbitrator_info"] = node_rpc.get_current_arbitrators_info(network)
-            data["current_arbitrator_group"] = node_rpc.get_current_arbitrator_group(network)
+            data["current_mining_info"] = node_rpc.get_current_mining_info(api_key, did, network)
+            data["current_block_confirm"] = node_rpc.get_current_block_confirm(api_key, did, network)
+            data["current_arbitrator_info"] = node_rpc.get_current_arbitrators_info(api_key, did, network)
+            data["current_arbitrator_group"] = node_rpc.get_current_arbitrator_group(api_key, did, network)
     except Exception as e:
         logging.debug(f"did: {did} Method: browser:mainchain Error: {e}")
         messages.success(request, "Could not call a node rpc method. Please try again")
     finally:
         node_rpc.close()
     return data
+
