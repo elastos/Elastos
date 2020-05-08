311410499197f61f6e1b06f1552f8490afecc151
Billie Zhang <billiecoder@gmail.com>
2020-05-08 11:09:56 +0800

    format code style

--- CR.CyberRepublic/back-end/src/db/schema/UserSchema.ts
+++ CR.CyberRepublic/back-end/src/db/schema/UserSchema.ts
@@ -1,144 +1,144 @@
-import {Schema} from 'mongoose'
-import {CommentSchema} from './CommentSchema'
-import {SubscriberSchema} from './SubscriberSchema'
+import { Schema } from 'mongoose'
+import { CommentSchema } from './CommentSchema'
+import { SubscriberSchema } from './SubscriberSchema'
 
 export const Region = {
-    country: String,
-    state: String,
-    city: String
+  country: String,
+  state: String,
+  city: String
 }
 
 export const Contact = {
-    type : Map,
-    of : String
+  type: Map,
+  of: String
 }
 
 export const Profile = {
-    firstName : String,
-    lastName: String,
-    avatar : String,
-    avatarFilename: String,
-    avatarFileType: String,
-    banner : String,
-    bannerFilename: String,
-    bannerFileType: String,
-    gender : String,
-    birth : Date,
-    timezone: String,
-    region: Region,
-    country : String,
-    state : String,
-    city : String,
-    profession: String,
+  firstName: String,
+  lastName: String,
+  avatar: String,
+  avatarFilename: String,
+  avatarFileType: String,
+  banner: String,
+  bannerFilename: String,
+  bannerFileType: String,
+  gender: String,
+  birth: Date,
+  timezone: String,
+  region: Region,
+  country: String,
+  state: String,
+  city: String,
+  profession: String,
 
-    telegram: String,
-    reddit: String,
-    wechat: String,
-    twitter: String,
-    facebook: String,
-    github: String,
-    linkedin: String,
+  telegram: String,
+  reddit: String,
+  wechat: String,
+  twitter: String,
+  facebook: String,
+  github: String,
+  linkedin: String,
 
-    portfolio: String,
-    skillset: [String],
-    bio: String,
-    motto: String,
-    beOrganizer : Boolean,
-    isDeveloper : Boolean,
-    source : String,
-    walletAddress : String
+  portfolio: String,
+  skillset: [String],
+  bio: String,
+  motto: String,
+  beOrganizer: Boolean,
+  isDeveloper: Boolean,
+  source: String,
+  walletAddress: String
 }
 
 export const WorkProject = {
-    startTime : Date,
-    endTime : Date,
-    description : String,
-    name : String
+  startTime: Date,
+  endTime: Date,
+  description: String,
+  name: String
 }
 
 export const WorkAbout = {
-    status: String, // employed, student, etc
-    employment: String, // company if employed / school if student
-    skill : [String],
-    project : [WorkProject],
-    resume : String,
+  status: String, // employed, student, etc
+  employment: String, // company if employed / school if student
+  skill: [String],
+  project: [WorkProject],
+  resume: String,
 
-    notes: String // private internal notes visible only to admin/council
+  notes: String // private internal notes visible only to admin/council
 }
 
 // amount is ELA * 1000
 export const ELA = {
-    address: String,
-    amount: Schema.Types.Number
+  address: String,
+  amount: Schema.Types.Number
 }
 
 export const VotePower = {
-    amount: Number,
-    expired: Date
+  amount: Number,
+  expired: Date
 }
 
 const did = {
-    id: String,
-    expirationDate: Date,
-    active: Boolean,
-    message: String,
-    _id: false
+  id: String,
+  expirationDate: Date,
+  active: Boolean,
+  message: String,
+  _id: false
 }
 
 export const User = {
-    dids: [did],
-    username : {
-        type : String,
-        required: true,
-        index : true,
-        unique : true
-    },
-    password : {
-        type : String,
-        required : true
-    },
-    salt: {
-        type: String,
-        required: true
-    },
+  dids: [did],
+  username: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String,
+    required: true
+  },
 
-    // let's keep this on the root object
-    email: String,
-    profile : Profile,
-    defaultLanguage: String,
-    workAbout : WorkAbout,
+  // let's keep this on the root object
+  email: String,
+  profile: Profile,
+  defaultLanguage: String,
+  workAbout: WorkAbout,
 
-    // resetToken, ensure this is never returned
-    resetToken: String,
+  // resetToken, ensure this is never returned
+  resetToken: String,
 
-    // constants.USER_ROLE
-    role : String,
+  // constants.USER_ROLE
+  role: String,
 
-    // constants.USER_EMPOWER
-    empower: String,
+  // constants.USER_EMPOWER
+  empower: String,
 
-    elaOwed : [ELA],
+  elaOwed: [ELA],
 
-    notes: String, // private internal notes visible only to admin/council
+  notes: String, // private internal notes visible only to admin/council
 
-    // admin or council approved max event budget, defaults to 0
-    // decreases upon usage
-    elaBudget: [ELA],
+  // admin or council approved max event budget, defaults to 0
+  // decreases upon usage
+  elaBudget: [ELA],
 
-    votePower : [VotePower],
-    votePowerAmount : {
-        // TODO auto calculate with votePower
-    },
-    active : {
-        type : Boolean,
-        default : false
-    },
-    logins: [Date],
-    circles: [{type: Schema.Types.ObjectId, ref: 'team'}],
-    comments: [[CommentSchema]],
-    subscribers: [SubscriberSchema],
-    popupUpdate: {
-        type: Boolean,
-        default: false
-    }
+  votePower: [VotePower],
+  votePowerAmount: {
+    // TODO auto calculate with votePower
+  },
+  active: {
+    type: Boolean,
+    default: false
+  },
+  logins: [Date],
+  circles: [{ type: Schema.Types.ObjectId, ref: 'team' }],
+  comments: [[CommentSchema]],
+  subscribers: [SubscriberSchema],
+  popupUpdate: {
+    type: Boolean,
+    default: false
+  }
 }
