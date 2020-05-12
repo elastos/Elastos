3379a53dbdbf90456947494074cb8e8268094262
Billie Zhang <billiecoder@gmail.com>
2020-05-12 14:44:49 +0800

    create api to get council member signature url

--- CR.CyberRepublic/back-end/src/router/suggestion/index.ts
+++ CR.CyberRepublic/back-end/src/router/suggestion/index.ts
@@ -26,141 +26,147 @@ import signatureUrl from './signature_url'
 import signatureCallback from './signature_callback'
 import checkSignature from './check_signature'
 import getSuggestion from './getSuggestion'
+import councilMemberSignatureUrl from './cm_signature_url'
 
 export default Base.setRouter([
   {
     path: '/create',
     router: create,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/list',
     router: list,
-    method: 'get',
+    method: 'get'
   },
   {
     path: '/export2csv',
     router: export2csv,
-    method: 'get',
+    method: 'get'
   },
   {
     path: '/:id/show',
     router: show,
-    method: 'get',
+    method: 'get'
   },
   {
     path: '/:id/showDraft',
     router: showDraft,
-    method: 'get',
+    method: 'get'
   },
   {
     path: '/:id/update',
     router: update,
-    method: 'put',
+    method: 'put'
   },
   {
     path: '/:id/saveDraft',
     router: saveDraft,
-    method: 'put',
+    method: 'put'
   },
   {
     path: '/:id/addtag',
     router: addTag,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/:id/like',
     router: like,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/:id/dislike',
     router: dislike,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/:id/reportabuse',
     router: reportabuse,
-    method: 'post',
+    method: 'post'
   },
   {
-    path : '/:id/comment',
-    router : comment,
-    method : 'post'
+    path: '/:id/comment',
+    router: comment,
+    method: 'post'
   },
   {
-    path : '/:id/comment_update',
-    router : comment_update,
-    method : 'post'
+    path: '/:id/comment_update',
+    router: comment_update,
+    method: 'post'
   },
   {
-    path : '/:id/comment_remove',
-    router : comment_remove,
-    method : 'post'
+    path: '/:id/comment_remove',
+    router: comment_remove,
+    method: 'post'
   },
   {
-    path : '/:id/subscribe',
-    router : subscribe,
-    method : 'post'
+    path: '/:id/subscribe',
+    router: subscribe,
+    method: 'post'
   },
   {
-    path : '/:id/unsubscribe',
-    router : unsubscribe,
-    method : 'post'
+    path: '/:id/unsubscribe',
+    router: unsubscribe,
+    method: 'post'
   },
   {
     path: '/:id/abuse',
     router: abuse,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/:id/archive',
     router: archive,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/:id/delete',
     router: del,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/:id/editHistories',
     router: editHistories,
-    method: 'get',
+    method: 'get'
   },
   {
     path: '/:id/revertVersion',
     router: revertVersion,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/:id/investigation',
     router: investigation,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/:id/advisory',
     router: advisory,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/signature-url',
     router: signatureUrl,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/signature-callback',
     router: signatureCallback,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/signature',
     router: checkSignature,
-    method: 'post',
+    method: 'post'
   },
   {
     path: '/get_suggestion/:id',
     router: getSuggestion,
-    method: 'get',
+    method: 'get'
+  },
+  {
+    path: '/cm-signature-url',
+    router: councilMemberSignatureUrl,
+    method: 'post'
   }
 ])
