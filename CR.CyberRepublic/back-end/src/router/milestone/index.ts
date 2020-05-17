deceecce6cd194b327bcedd5353cbcf82e85b6c3
Billie Zhang <billiecoder@gmail.com>
2020-05-17 15:28:04 +0800

    check milestone signature api

--- CR.CyberRepublic/back-end/src/router/milestone/index.ts
+++ CR.CyberRepublic/back-end/src/router/milestone/index.ts
@@ -1,6 +1,7 @@
 import Base from '../Base'
 import update from './update'
 import signatureCallback from './owner_signature_callback'
+import checkSignature from './check_signature'
 
 export default Base.setRouter([
   {
@@ -11,6 +12,11 @@ export default Base.setRouter([
   {
     path: '/milestones/signature-callback',
     router: signatureCallback,
+    method: 'post'
+  },
+  {
+    path: '/:id/milestones/signature',
+    router: checkSignature,
     method: 'post'
   }
 ])
