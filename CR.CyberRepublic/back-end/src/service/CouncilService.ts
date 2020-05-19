cde1869625a8659ece72a57cae5f657dae81a120
Andy Lian <andy.lodestar00@gmail.com>
2020-05-19 15:42:55 +0800

    fixed vote history no data issue

--- CR.CyberRepublic/back-end/src/service/CouncilService.ts
+++ CR.CyberRepublic/back-end/src/service/CouncilService.ts
@@ -428,6 +428,7 @@ export default class extends Base {
             console.log('---------------- start council or secretariat cronJob -------------')
             await this.eachJob()
             await this.eachSecretariatJob()
+            
         }, 1000 * 60 * 10)
     }
 
