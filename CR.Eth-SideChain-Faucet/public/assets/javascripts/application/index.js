6ec013bcdfd89cabb2a5a7cef4db0827682585a7
Harry Liu <2411mail@gmail.com>
2020-05-24 12:23:25 +0800

    fix wrong url

--- CR.Eth-SideChain-Faucet/public/assets/javascripts/application/index.js
+++ CR.Eth-SideChain-Faucet/public/assets/javascripts/application/index.js
@@ -22,7 +22,7 @@ $(function() {
 			$("#receiver").val('');
 			loader.addClass("hidden");
 			swal("Success",
-			  `1 ETH has been successfully transferred to <a href="https://explorer.elaeth.io/tx/${data.success.txHash}" target="blank">${receiver}`,
+			  `1 ETH has been successfully transferred to <a href="https://testnet.elaeth.io/tx/${data.success.txHash}" target="blank">${receiver}`,
 			  "success"
 			);
 		}).fail(function(err) {
