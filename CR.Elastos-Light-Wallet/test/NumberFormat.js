22349b4da64839366646c3c3a8f8e791fba14c06
coranos <coranos@users.noreply.github.com>
2020-06-03 16:57:50 -0500

    fixed #16

--- CR.Elastos-Light-Wallet/test/NumberFormat.js
+++ CR.Elastos-Light-Wallet/test/NumberFormat.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 
 const app = require('../scripts/App.js');
 
-describe.only('NumberFormat', function () {
+describe('NumberFormat', function () {
   it('raw 1', function () {
     const input = 1;
     const expectedValue = '0.00000001';
