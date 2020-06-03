914688bc0dd5bd84739f41023117e1fe10f98375
Andy Lian <andy.lodestar00@gmail.com>
2020-06-03 14:49:24 +0800

    add user avatar

--- CR.CyberRepublic/front-end/src/module/page/CVote/common/vote_result/Component.js
+++ CR.CyberRepublic/front-end/src/module/page/CVote/common/vote_result/Component.js
@@ -4,73 +4,86 @@ import I18N from '@/I18N'
 import _ from 'lodash'
 import OnChain from '../../detail/OnChain'
 import Translation from '@/module/common/Translation/Container'
-
-import { Container, ResultRow, Reason, Label, List, Item, Avatar, StyledAvatarIcon } from './style'
+import { Avatar } from 'antd'
+import { Container, ResultRow, Reason, Label, List, Item, StyledAvatarIcon } from './style'
 
 const Component = (
-    { 
-      label, 
-      type, 
-      dataList,
-      id,
-      getReviewProposal,
-      getReviewProposalUrl,
-      updateProposal,
-      isProposed,
-      isCouncil,
-      currentUserId      
-    }
-  ) => {
+  {
+    label,
+    type,
+    dataList,
+    id,
+    getReviewProposal,
+    getReviewProposalUrl,
+    updateProposal,
+    isProposed,
+    isCouncil,
+    currentUserId
+  }
+) => {
   const votesNode = _.map(dataList, (data, key) => {
     let voteStatus = data.status
-    if(voteStatus == undefined || voteStatus == 'failed' || voteStatus == 'unchain') {
+    if (voteStatus == undefined || voteStatus == 'failed' || voteStatus == 'unchain') {
       voteStatus = I18N.get(`council.voting.chainStatus.unchain`)
-    } 
-    if( voteStatus == 'chained'){
+    }
+    if (voteStatus == 'chained') {
       voteStatus = I18N.get(`council.voting.chainStatus.chained`)
     }
     if (voteStatus == 'chaining') {
-       voteStatus = I18N.get(`council.voting.chainStatus.chaining`)
+      voteStatus = I18N.get(`council.voting.chainStatus.chaining`)
     }
     let isOwner = data.votedBy && data.votedBy === currentUserId
+    const avatarName = data.name.split(" ")
     const userNode = (
       <Item key={key}>
-        {data.avatar ? <Avatar src={data.avatar} alt="voter avatar" /> : <StyledAvatarIcon />}
-        <div>{ data.didName }</div>
-        <div>{ data.reason !== "" ? voteStatus : null }</div>
-        <div style={{ marginTop: '0.5rem'}}>
+        {data.avatar ? <Avatar size={64} src={data.avatar} shape="circle" alt="voter avatar" /> :
+          <Avatar
+            className="comment-avatar pull-left"
+            style={{
+              backgroundColor: '#000',
+              fontSize: 24
+            }}
+            shape="circle"
+            size={64}
+          >
+            {`${avatarName[0] && avatarName[0].toUpperCase().substr(0, 1)}${avatarName[1] && avatarName[1].toUpperCase().substr(0, 1)}`}
+          </Avatar>
+        }
+        <div>{data.didName}</div>
+        <div>{data.reason !== "" ? voteStatus : null}</div>
+        <div style={{ marginTop: '0.5rem' }}>
           {
-          ( isProposed && isCouncil && isOwner && (data.status === 'unchain' || data.status === undefined ) && data.reason !== '' ) ?
-          <OnChain 
-            getReviewProposal={getReviewProposal}
-            getReviewProposalUrl={getReviewProposalUrl}
-            updateProposal={updateProposal}
-            id={id}
-          /> : null
+            (isProposed && isCouncil && isOwner && (data.status === 'unchain' || data.status === undefined) && data.reason !== '') ?
+              <OnChain
+                getReviewProposal={getReviewProposal}
+                getReviewProposalUrl={getReviewProposalUrl}
+                updateProposal={updateProposal}
+                id={id}
+              /> : null
           }
-          </div>
+        </div>
       </Item>
     )
-    
-    const googleNode = data.reason && <div style={{ marginTop: '0.5rem'}}><Translation text={data.reason} /></div>
-  
+
+    const googleNode = data.reason && <div style={{ marginTop: '0.5rem' }}><Translation text={data.reason} /></div>
+
     // if (!isReject) return userNode
     // show reason for all vote type
     const reasonNode = (
       <Reason>
-        {data.reason.split('\n').map((item,key) => {
+        {data.reason.split('\n').map((item, key) => {
           return (
             <span key={key}>
               {item}
-              <br/>
+              <br />
             </span>
           )
         })}
         {googleNode}
-        
+
       </Reason>
     )
-    
+
     return (
       <ResultRow key={key}>
         {userNode}
@@ -78,7 +91,7 @@ const Component = (
       </ResultRow>
     )
   })
-  
+
   return (
     <Container>
       <Label>{label}</Label>
