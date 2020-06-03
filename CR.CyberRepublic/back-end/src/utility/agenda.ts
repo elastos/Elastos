435db4c33213d377b649a7c8e72a5f3920755ccd
Billie Zhang <billiecoder@gmail.com>
2020-06-03 08:27:56 +0800

    add agenda file

---
+++ CR.CyberRepublic/back-end/src/utility/agenda.ts
@@ -0,0 +1,44 @@
+import db from '../db'
+import { getProposalState } from '../utility'
+import CVoteServive from '../service/CVoteService'
+const Agenda = require('agenda')
+const agenda = new Agenda({ db: { address: process.env.DB_URL } })
+
+agenda.define('make into proposal', async (job: any) => {
+  try {
+    const DB = await db.create()
+    const cvote = await DB.getModel('CVote')
+    const suggestions = await DB.getModel('Suggestion').find({ proposed: true })
+    console.log('suggestions', suggestions.length)
+    const cvoteService = new CVoteServive(DB, { user: undefined })
+    let count = 0
+    for (let i = 0; i < suggestions.length; i++) {
+      const doc = suggestions[i]
+      console.log('doc draftHash', doc.draftHash)
+      const rs = await getProposalState({ drafthash: doc.draftHash })
+      if (rs && rs.success && rs.status === 'Registered') {
+        console.log('doc.displayId', doc.displayId)
+        count++
+        const proposal = await cvote.findOne({ draftHash: doc.draftHash })
+        console.log('existed proposal vid', proposal.vid)
+        if (proposal) {
+          continue
+        }
+        const newProposal = await cvoteService.makeSuggIntoProposal({
+          suggestion: doc,
+          proposalHash: rs.proposalHash,
+          chainDid: rs.proposal.crcouncilmemberdid
+        })
+        console.log('newProposal.vid', newProposal.vid)
+      }
+    }
+    console.log('proposed suggestion count...', count)
+  } catch (err) {
+    console.log('make into proposal cron job err...', err)
+  }
+})
+;(async function () {
+  console.log('------make into proposal cron job starting------')
+  await agenda.start()
+  await agenda.every('1 minutes', 'make into proposal')
+})()
