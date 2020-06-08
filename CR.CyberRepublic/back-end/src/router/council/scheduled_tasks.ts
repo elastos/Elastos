daa404ba2ac03b28e1067b43e77c51e92997053c
Andy Lian <andy.lodestar00@gmail.com>
2020-06-08 16:26:14 +0800

    update eachjob scheduled tasks

--- CR.CyberRepublic/back-end/src/router/council/scheduled_tasks.ts
+++ CR.CyberRepublic/back-end/src/router/council/scheduled_tasks.ts
@@ -5,7 +5,7 @@ export default class extends Base {
     async action() {
         const service = this.buildService(CouncilService)
 
-        const rs = await service.eachSecretariatJob()
+        const rs = await service.eachCouncilJobPlus()
         return this.result(1, rs)
     }
 }
