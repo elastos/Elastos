643e02b41120762e94a8e3ff87c1a78a8151877d
Carlos Verastegui <51972213+carlosverastegui@users.noreply.github.com>
2020-06-13 21:20:03 -0400

    Created Mainchain Browser (#211)

---
+++ CR.Elastos-Nucleus/browser/time_convert.py
@@ -0,0 +1,20 @@
+activity_time_intervals = (
+    ('weeks', 604800),  # 60 * 60 * 24 * 7
+    ('days', 86400),    # 60 * 60 * 24
+    ('hours', 3600),    # 60 * 60
+    ('minutes', 60),
+    ('seconds', 1),
+)
+
+
+def display_time_elapsed(seconds, granularity=2):
+    result = []
+
+    for name, count in activity_time_intervals:
+        value = seconds // count
+        if value:
+            seconds -= value * count
+            if value == 1:
+                name = name.rstrip('s')
+            result.append("{} {}".format(value, name))
+    return ', '.join(result[:granularity])
--- CR.Elastos-Nucleus/browser/views.py
+++ CR.Elastos-Nucleus/browser/views.py
@@ -1,14 +1,19 @@
 import logging
+import pprint
+import math
 
 from django.shortcuts import render
 from django.contrib import messages
-
 from elastos_adenine.node_rpc import NodeRpc
 
 from console_main.settings import GRPC_SERVER_HOST, PRODUCTION, GRPC_SERVER_PORT
 from console_main.views import login_required, track_page_visit, get_recent_services
 
+from .time_convert import display_time_elapsed
 from .forms import SelectNetworkForm
+
+from datetime import datetime
+from django.utils import timezone
 
 
 @login_required
@@ -30,7 +35,7 @@ def mainchain(request):
             data = get_blockchain_node_data(request, api_key, did, network, "mainchain")
             for k, v in data.items():
                 context[k] = v
-            return render(request, "browser/mainchain.html", context)
+            return render(request, "browser/mainchain.html", {'context': context, 'blocks': context['recent_blocks']})
     else:
         network = 'gmunet'
         data = get_blockchain_node_data(request, api_key, did, network, "mainchain")
@@ -38,7 +43,7 @@ def mainchain(request):
             context[k] = v
         form = SelectNetworkForm(initial={'network': network})
         context['form'] = form
-        return render(request, "browser/mainchain.html", context)
+        return render(request, "browser/mainchain.html", {'context': context, 'blocks': context['recent_blocks']})
 
 
 @login_required
@@ -148,7 +153,35 @@ def get_blockchain_node_data(request, api_key, did, network, chain):
     try:
         node_rpc = NodeRpc(GRPC_SERVER_HOST, GRPC_SERVER_PORT, PRODUCTION)
         data["current_height"] = node_rpc.get_current_height(api_key, did, network, chain)
-        data["current_block_info"] = node_rpc.get_current_block_info(api_key, did, network, chain)
+
+        data["recent_blocks"] = {}
+        for index in range(5):
+            data["recent_blocks"]["block" + str(index + 1)] = node_rpc.get_block_info(api_key, did, network, chain, data["current_height"] - (index + 1))
+
+            # Get number of transactions
+            blockVout = data["recent_blocks"]["block" + str(index + 1)]["tx"]
+            numTransactions = len((blockVout[0])["vout"])
+            data["recent_blocks"]["block" + str(index + 1)]["numtransactions"] = numTransactions
+
+            # Convert timestamp to datetime
+            dt = datetime.fromtimestamp(data["recent_blocks"]["block" + str(index + 1)]["time"])
+            new_datetime = timezone.make_aware(dt, timezone.utc)
+
+            time_elapsed_in_seconds = math.floor((timezone.now() - new_datetime).seconds)
+
+            last_visited = "{0} ago".format(display_time_elapsed(time_elapsed_in_seconds))
+            data["recent_blocks"]["block" + str(index + 1)]["last_time_visited"] = last_visited
+
+            # Get initials on miner
+            data["recent_blocks"]["block" + str(index + 1)]["abbreviation"] = data["recent_blocks"]["block" + str(index + 1)]["minerinfo"][:2]
+
+            # Get total ELA transferred per block
+            elaTransferred = 0
+            vout = (blockVout[0])["vout"]
+            for child in vout:
+                elaTransferred += float(child["value"])
+            data["recent_blocks"]["block" + str(index + 1)]["totaltransactions"] = elaTransferred
+
         if chain == "mainchain":
             data["current_mining_info"] = node_rpc.get_current_mining_info(api_key, did, network)
             data["current_block_confirm"] = node_rpc.get_current_block_confirm(api_key, did, network)
