7707e9919fd5f005fe74e1a88d88bddc37a309a0
Billie Zhang <billiecoder@gmail.com>
2020-06-28 20:13:08 +0800

    remove unused code

--- CR.CyberRepublic/front-end/src/module/form/SuggestionForm/PaymentSchedule.js
+++ CR.CyberRepublic/front-end/src/module/form/SuggestionForm/PaymentSchedule.js
@@ -15,8 +15,7 @@ class PaymentSchedule extends Component {
       visible: false,
       total: _.get(value, 'budgetAmount'),
       address: (value && value.elaAddress) || '',
-      paymentItems: (value && value.paymentItems) || [],
-      errors: {}
+      paymentItems: (value && value.paymentItems) || []
     }
   }
 
@@ -35,36 +34,9 @@ class PaymentSchedule extends Component {
     })
   }
 
-  validateAmount = (value) => {
-    const reg = /^(0|[1-9][0-9]*)(\.[0-9]*)?$/
-    return (!isNaN(value) && reg.test(value)) || value === '' ? true : false
-  }
-
-  validateAddress = (value) => {
-    const reg = /^[E8][a-zA-Z0-9]{33}$/
-    return reg.test(value)
-  }
-
-  validateFields = (field, value) => {
-    const { errors } = this.state
-    if (field === 'total' && !this.validateAmount(value)) {
-      return {
-        ...errors,
-        [field]: I18N.get('suggestion.form.error.isNaN')
-      }
-    }
-    if (field === 'address' && !this.validateAddress(value)) {
-      return {
-        ...errors,
-        [field]: I18N.get('suggestion.form.error.elaAddress')
-      }
-    }
-    return { ...errors, [field]: '' }
-  }
 
   handleChange = (e, field) => {
-    const errors = this.validateFields(field, e.target.value)
-    this.setState({ [field]: e.target.value, errors }, () => {
+    this.setState({ [field]: e.target.value }, () => {
       this.passDataToParent()
     })
   }
@@ -88,12 +60,8 @@ class PaymentSchedule extends Component {
       ...paymentItems.slice(index + 1)
     ]
     const sortedItems = this.sortPayments(rs)
-    const errors = this.validateFields('schedule', sortedItems)
     this.setState(
-      {
-        paymentItems: sortedItems,
-        errors
-      },
+      { paymentItems: sortedItems },
       () => {
         this.passDataToParent()
       }
@@ -121,12 +89,10 @@ class PaymentSchedule extends Component {
     }
     const rs = [...paymentItems, values]
     const sortedItems = this.sortPayments(rs)
-    const errors = this.validateFields('schedule', sortedItems)
     this.setState(
       {
         paymentItems: sortedItems,
-        visible: false,
-        errors
+        visible: false
       },
       () => {
         this.passDataToParent()
@@ -145,7 +111,7 @@ class PaymentSchedule extends Component {
   }
 
   render() {
-    const { paymentItems, index, total, address, errors } = this.state
+    const { paymentItems, index, total, address } = this.state
     const milestone = this.getMilestone()
     const flag = milestone && milestone.length <= paymentItems.length
     const disabled = !milestone || flag
@@ -154,20 +120,16 @@ class PaymentSchedule extends Component {
         <Section>
           <Label>{`${I18N.get('suggestion.budget.total')} (ELA)`}</Label>
           <StyledInput
-            error={errors.total ? true : false}
             value={total}
             onChange={(e) => this.handleChange(e, 'total')}
           />
-          {errors.total ? <Error>{errors.total}</Error> : null}
         </Section>
         <Section>
           <Label>{I18N.get('suggestion.budget.address')}</Label>
           <StyledInput
-            error={errors.address ? true : false}
             value={address}
             onChange={(e) => this.handleChange(e, 'address')}
           />
-          {errors.address ? <Error>{errors.address}</Error> : null}
         </Section>
         <Section>
           <Label>{I18N.get('suggestion.budget.schedule')}</Label>
@@ -176,7 +138,6 @@ class PaymentSchedule extends Component {
           </Button>
           {disabled ? <Tip>{I18N.get('suggestion.budget.tip')}</Tip> : null}
         </Section>
-        {errors.schedule ? <Error>{errors.schedule}</Error> : null}
         {paymentItems.length ? (
           <PaymentList
             list={paymentItems}
