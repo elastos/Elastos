2a083b96105709407c066f19dfcde69da26e644e
Andy Lian <andy.lodestar00@gmail.com>
2020-06-29 17:20:13 +0800

    fixed proposal status in notification, qrcode display error

--- CR.CyberRepublic/back-end/src/router/cvote/memberVote.ts
+++ CR.CyberRepublic/back-end/src/router/cvote/memberVote.ts
@@ -2,7 +2,6 @@ import Base from '../Base'
 import CVoteService from '../../service/CVoteService'
 
 export default class extends Base{
-    protected needLogin = true;
     async action(){
         const param = this.getParam()
         const service = this.buildService(CVoteService)
