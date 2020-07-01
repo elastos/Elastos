5911052e855d2ce1ee9867f2ad43f176a72b397c
Billie Zhang <billiecoder@gmail.com>
2020-07-01 14:28:04 +0800

    refactor milestone signature callback

--- CR.CyberRepublic/back-end/src/service/MilestoneService.ts
+++ CR.CyberRepublic/back-end/src/service/MilestoneService.ts
@@ -172,21 +172,23 @@ export default class extends Base {
                   message: 'There is no this payment application.'
                 }
               }
-              await this.model.update(
-                { proposalHash, 'withdrawalHistory.messageHash': messageHash },
-                {
-                  $set: {
-                    'withdrawalHistory.$.signature': decoded.data
-                  }
-                }
-              )
-              await this.model.update(
-                {
-                  proposalHash,
-                  'budget.milestoneKey': history.milestoneKey
-                },
-                { $set: { 'budget.$.status': WAITING_FOR_APPROVAL } }
-              )
+
+              await Promise.all([
+                this.model.update(
+                  {
+                    proposalHash,
+                    'withdrawalHistory.messageHash': messageHash
+                  },
+                  { 'withdrawalHistory.$.signature': decoded.data }
+                ),
+                this.model.update(
+                  {
+                    proposalHash,
+                    'budget.milestoneKey': history.milestoneKey
+                  },
+                  { 'budget.$.status': WAITING_FOR_APPROVAL }
+                )
+              ])
 
               this.notifySecretaries(this.updateMailTemplate(proposal.vid))
               return { code: 200, success: true, message: 'Ok' }
