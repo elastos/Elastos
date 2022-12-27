08d573fa7cd58fb92cb85308142420b0c27c4c74
LiFayi <lifayi@trinity-tech.io>
2022-12-27 02:15:51 +0000

    Cleanup

--- .idea/fileTemplates/internal/Go File.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2017-${YEAR} The Elastos Foundation
-// Use of this source code is governed by an MIT
-// license that can be found in the LICENSE file.
-//
-
-package ${GO_PACKAGE_NAME}
